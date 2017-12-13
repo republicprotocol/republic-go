01e16c240e87d1180343fdea39c9906ee38b54f8
Loongy <benjamin.jl.wang@gmail.com>
2017-12-13 11:13:32 +1100

    Initial commit

---
+++ crypto_test.go
@@ -0,0 +1,12 @@
+package crypto_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/republicprotocol/go-crypto"
+)
+
+var _ = Describe("GoCrypto", func() {
+
+})
