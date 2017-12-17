9606522f99b6dc2457bdfd43d160f0a46da07455
Loongy <benjamin.jl.wang@gmail.com>
2017-12-18 09:59:04 +1100

    Specify to It

--- multiaddress_test.go
+++ multiaddress_test.go
@@ -11,11 +11,11 @@ var _ = Describe("MultiAddresses with support for Republic Protocol", func() {
 
 	Context("after importing the identity package", func() {
 
-		It("there should be a protocol called republic", func() {
+		It("should expose a protocol called republic", func() {
 			Ω(ProtocolWithName("republic").Name).Should(Equal("republic"))
 		})
 
-		Specify("the Republic Protocol code should be defined with the correct constants", func() {
+		It("should expose the Republic Protocol with the correct constants", func() {
 			Ω(ProtocolWithCode(RepublicCode).Name).Should(Equal("republic"))
 			Ω(ProtocolWithCode(RepublicCode).Code).Should(Equal(RepublicCode))
 		})
