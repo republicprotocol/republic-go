b6d179ed9356fa6a1fa2ac45dc367cab938bb26c
Loongy <benjamin.jl.wang@gmail.com>
2018-01-08 15:03:41 +1100

    Added test for full bucket error

--- dht_test.go
+++ dht_test.go
@@ -3,10 +3,31 @@ package dht_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/republicprotocol/go-identity"
 
 	. "github.com/republicprotocol/go-dht"
 )
 
-var _ = Describe("DHT", func() {
+var _ = Describe("Distributed Hash Table", func() {
+
+	Context("updates", func() {
+
+		It("should error when the bucket is full", func() {
+			address, _, err := identity.NewAddress()
+			Ω(err).ShouldNot(HaveOccurred())
+			dht := NewDHT(address)
+			for i := 0; i < MaxDHTSize; i++ {
+				address, _, e := identity.NewAddress()
+				Ω(e).ShouldNot(HaveOccurred())
+				multi, e := address.MultiAddress()
+				Ω(e).ShouldNot(HaveOccurred())
+				e = dht.Update(multi)
+				if err == nil {
+					err = e
+				}
+			}
+			Ω(err).Should(HaveOccurred())
+		})
+	})
 
 })
