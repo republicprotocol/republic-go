1d5f34e952a06369e68b86180669f4dd5b71bf33
tok-kkk <sacrifise000@gmail.com>
2018-01-09 14:20:42 +1100

    add errors for updating dht itself

--- errors.go
+++ errors.go
@@ -4,4 +4,7 @@ import "fmt"
 
 // ErrFullBucket is used when a peer is inserted into a Bucket that already has
 // the maximum number of Entries.
-var ErrFullBucket = fmt.Errorf("cannot add entry to a full bucket")
+var(
+	ErrFullBucket = fmt.Errorf("cannot add entry to a full bucket")
+	ErrUpdateSelf = fmt.Errorf("cannot update the dht itself")
+)
