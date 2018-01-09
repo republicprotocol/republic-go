ff9cf5994f8736c7890b4f4c64158608344b49c3
Loongy <benjamin.jl.wang@gmail.com>
2018-01-10 10:11:56 +1100

    Changed the send proto to app-specific send order fragment

---
+++ x_suite_test.go
@@ -0,0 +1,13 @@
+package swarm_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestGoXNetwork(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "X Network Suite")
+}
