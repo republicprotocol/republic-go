43444caa0bc25cc71db525d524c3a6bbb47ff8b4
Loongy <benjamin.jl.wang@gmail.com>
2018-01-15 11:13:11 +1100

    Implemented a parallel X sort

--- hash.go
+++ hash.go
@@ -1,9 +1,21 @@
 package x
 
+import identity "github.com/republicprotocol/go-identity"
+
 // Hash is an alias for a slice of bytes. It represents a Keccak256 hash.
 type Hash []byte
 
-// LessThen returns true if the left hand hash is less than the right hand
+// LessThan returns true if the left hand hash is less than the right hand
 // hash, otherwise false.
-func (lhs Hash) LessThen(rhs Hash) bool {
+func (lhs Hash) LessThan(rhs Hash) bool {
+	return false
+}
+
+// A Miner represents a miner in the X Network. They are identified by an
+// identity.ID and organized by combining their commitment hash with the Epoch
+// hash to produce an X hash.
+type Miner struct {
+	identity.ID
+	Commitment Hash
+	X          Hash
 }
