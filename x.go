66a5fd002ff8b5c280e68e76fda018152dbf7ae2
Loongy <benjamin.jl.wang@gmail.com>
2018-01-15 12:36:31 +1100

    Performance improvements through pub priv function separation

--- x.go
+++ x.go
@@ -9,29 +9,23 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
+// AssignXOverlay iterates throught all Miners in the list and assigns them an
+// X Hash, a class, and an M Network. The list of Miners will be sorted by
+// their X Hashes.
+func AssignXOverlay(miners []Miner, epoch Hash, numberOfMNetworks int) {
+	AssignXHash(miners, epoch)
+	assignClass(miners, numberOfMNetworks)
+	assignMNetwork(miners, numberOfMNetworks)
+}
+
 // AssignXHash assigns an X Hash to all Miners. For each Miner, the epoch Hash
 // and the commitment Hash are combined and hashed to produce the X Hash. All
 // hashing is done using the Keccak256 hashing function. The list of miners
 // will be sorted by their X Hashes.
-func AssignXHash(epoch Hash, miners []Miner) {
-
-	// Calculate workload size per CPU.
-	numCPUs := runtime.NumCPU()
-	numHashesPerCPU := (len(miners) / numCPUs) + 1
-
-	// Calculate list of output hashes in parallel.
-	var wg sync.WaitGroup
-	wg.Add(numCPUs)
-	for i := 0; i < len(miners); i += numHashesPerCPU {
-		go func(i int) {
-			defer wg.Done()
-			for j := i; j < i+numHashesPerCPU && j < len(miners); j++ {
-				miners[j].X = crypto.Keccak256([]byte(epoch), []byte(miners[j].Commitment))
-			}
-		}(i)
-	}
-	wg.Wait()
-
+func AssignXHash(miners []Miner, epoch Hash) {
+	forAll(miners, func(k int) {
+		miners[k].X = crypto.Keccak256([]byte(epoch), []byte(miners[k].Commitment))
+	})
 	// Sort the list of output hashes.
 	sort.Slice(miners, func(i, j int) bool {
 		return miners[i].X.LessThan(miners[j].X)
@@ -46,36 +40,18 @@ func AssignClass(numberOfMNetworks int, miners []Miner) {
 	if !RequireXHashes(miners) {
 		return
 	}
-
-	// Calculate workload size per CPU.
-	numCPUs := runtime.NumCPU()
-	numHashesPerCPU := (len(miners) / numCPUs) + 1
-
-	// Calculate list of output hashes in parallel.
-	var wg sync.WaitGroup
-	wg.Add(numCPUs)
-	for i := 0; i < len(miners); i += numHashesPerCPU {
-		go func(i int) {
-			defer wg.Done()
-			for j := i; j < i+numHashesPerCPU && j < len(miners); j++ {
-				miners[j].Class = j/numberOfMNetworks + 1
-			}
-		}(i)
-	}
-	wg.Wait()
+	assignClass(miners, numberOfMNetworks)
 }
 
 // AssignMNetwork will assigned an M Network to each miner. This function must
 // not be called before the assignXHash function. If any miner in the list does
 // not have an X Hash, this function will do nothing. If the miners are not
 // sorted then this function will do nothing.
-func AssignMNetwork(numberOfMNetworks int, miners []Miner) {
+func AssignMNetwork(miners []Miner, numberOfMNetworks int) {
 	if !RequireXHashes(miners) {
 		return
 	}
-	forAll(miners, func(k int) {
-		miners[k].MNetwork = k % numberOfMNetworks
-	})
+	assignMNetwork(miners, numberOfMNetworks)
 }
 
 // RequireXHashes checks that every Miner in the list has a valid X Hash. It
@@ -100,6 +76,18 @@ func RequireXHashes(miners []Miner) bool {
 	return true
 }
 
+func assignClass(miners []Miner, numberOfMNetworks int) {
+	forAll(miners, func(k int) {
+		miners[k].Class = k/numberOfMNetworks + 1
+	})
+}
+
+func assignMNetwork(miners []Miner, numberOfMNetworks int) {
+	forAll(miners, func(k int) {
+		miners[k].MNetwork = k % numberOfMNetworks
+	})
+}
+
 func forAll(data interface{}, f func(i int)) {
 	switch reflect.TypeOf(data).Kind() {
 	case reflect.Array, reflect.Map, reflect.Slice:
@@ -107,8 +95,7 @@ func forAll(data interface{}, f func(i int)) {
 		length := reflect.ValueOf(data).Len()
 		numCPUs := runtime.NumCPU()
 		numIterationsPerCPU := (length / numCPUs) + 1
-
-		// Calculate list of output hashes in parallel.
+		// Apply the function in parallel over the data.
 		var wg sync.WaitGroup
 		wg.Add(numCPUs)
 		for offset := 0; offset < length; offset += numIterationsPerCPU {
