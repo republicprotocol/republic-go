9e19e461ed54c798af9519d0858ec14e4203d8d0
Loongy <benjamin.jl.wang@gmail.com>
2018-01-15 11:40:55 +1100

    Updated Sort to use the new Miner struct

--- x.go
+++ x.go
@@ -8,33 +8,38 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-// Sort the list of miners. The epoch hash, and each miner hash, is combined
-// and hashed to produce an X hash for each miner. The miner are sorted by
-// comparing each their X hashes against each other.
-func Sort(epochHash Hash, minerHashes []Hash) []Hash {
+// Sort the list of Miners. The epoch Hash, and each Miner commitment Hash, is
+// combined and hashed to produce an X Hash for each Miner. The miner are sorted by
+// comparing each their X hashes against each other. All hashing is done using
+// the Keccak256 hashing function.
+func Sort(epoch Hash, miners []Miner) []Miner {
 
 	// Calculate workload size per CPU.
-	xHashes := make([]Hash, len(minerHashes))
-	numHashesPerCPU := (len(minerHashes) / runtime.NumCPU()) + 1
+	xs := make([]Miner, len(miners))
+	numCPUs := runtime.NumCPU()
+	numHashesPerCPU := (len(miners) / numCPUs) + 1
 
 	// Calculate list of output hashes in parallel.
 	var wg sync.WaitGroup
-	wg.Add(len(minerHashes))
-	for i := 0; i < len(minerHashes); i += numHashesPerCPU {
+	wg.Add(numCPUs)
+	for i := 0; i < len(miners); i += numHashesPerCPU {
 		go func(i int) {
 			defer wg.Done()
-			for j := i; j < i+numHashesPerCPU && j < len(minerHashes); j++ {
-				hash := crypto.Keccak256([]byte(epochHash), []byte(minerHashes[j]))
-				xHashes[j] = hash
+			for j := i; j < i+numHashesPerCPU && j < len(miners); j++ {
+				xs[j] = Miner{
+					ID:         miners[j].ID,
+					Commitment: miners[j].Commitment,
+					X:          crypto.Keccak256([]byte(epoch), []byte(miners[j].Commitment)),
+				}
 			}
 		}(i)
 	}
 	wg.Wait()
 
 	// Sort the list of output hashes.
-	sort.Slice(minerHashes, func(i, j int) bool {
-		return xHashes[i].LessThan(xHashes[j])
+	sort.Slice(xs, func(i, j int) bool {
+		return xs[i].X.LessThan(xs[j].X)
 	})
 
-	return minerHashes
+	return xs
 }
