5daa5dc75be83c269a6451a5984caf7d0780dada
Loongy <benjamin.jl.wang@gmail.com>
2018-01-17 16:27:59 +1100

    Added computations and reconstructions for order matching

--- miner.go
+++ miner.go
@@ -1,22 +1,19 @@
 package miner
 
 import (
-	"math/big"
-	"sync"
+	"log"
+	"runtime"
 
+	"github.com/republicprotocol/go-do"
 	"github.com/republicprotocol/go-identity"
 	"github.com/republicprotocol/go-network"
-	"github.com/republicprotocol/go-order"
+	"github.com/republicprotocol/go-order-compute"
 )
 
 type Miner struct {
 	network.Node
-
-	orderFragmentsMu          *sync.Mutex
-	orderFragments            map[string]*order.Fragment
-	orderFragmentComputations map[string]map[string]*order.Fragment
-
-	done chan struct{}
+	compute.ComputationMatrix
+	compute.ReconstructionMatrix
 }
 
 func NewMiner(config Config) Miner {
@@ -25,49 +22,77 @@ func NewMiner(config Config) Miner {
 	return miner
 }
 
-func (miner Miner) Start() {
+func (miner Miner) OnPingReceived(peer identity.MultiAddress) {
+}
+
+func (miner Miner) OnOrderFragmentReceived(orderFragment compute.OrderFragment) {
+	miner.ComputationMatrix.FillComputations(&orderFragment)
+}
+
+func (miner Miner) OnComputedOrderFragmentReceived(orderFragment compute.OrderFragment) {
+}
+
+func (miner Miner) Mine(quit chan struct{}) {
 	go func() {
-		for {
-			select {
-			case <-miner.done:
-				return
-			default:
-				miner.Mine()
-			}
+		if err := miner.Serve(); err != nil {
+			// TODO: Do something other than die.
+			log.Fatal(err)
 		}
 	}()
-}
-
-func (miner Miner) Stop() {
-	miner.done <- struct{}{}
-}
-
-func (miner Miner) Mine() {
-	for lhs := range miner.orderFragments {
-		for rhs := range miner.orderFragments {
-			if lhs == rhs {
-				continue
-			}
-			if result := miner.orderFragmentComputations[lhs][rhs]; result == nil {
-				result, err := miner.orderFragments[lhs].Add(miner.orderFragments[rhs], big.NewInt(2))
-				if err != nil {
-					continue
-				}
-				miner.orderFragmentComputations[lhs][rhs] = result
-			}
+	for {
+		select {
+		case <-quit:
+			miner.Stop()
+			return
+		default:
+			do.CoBegin(
+				func() do.Option {
+					miner.ComputeAll()
+					return do.Ok(nil)
+				},
+				func() do.Option {
+					miner.ReconstructAll()
+					return do.Ok(nil)
+				},
+			)
 		}
 	}
 }
 
-func (miner Miner) OnPingReceived(peer identity.MultiAddress) {
+func (miner Miner) ComputeAll() {
+	numberOfCPUs := runtime.NumCPU()
+	computations := miner.ComputationMatrix.WaitForComputations(numberOfCPUs)
+	do.CoForAll(computations, func(i int) {
+		miner.Compute(computations[i])
+	})
 }
 
-func (miner Miner) OnOrderFragmentReceived(orderFragment order.Fragment) {
-	miner.addOrderFragment(orderFragment)
+// Compute the required computation on two OrderFragments and send the result
+// to all Miners in the M Network.
+// TODO: Send computed order fragments to the M Network instead of all peers.
+func (miner Miner) Compute(com *Computation) {
+	com.Sub()
+	miner.JoinMatrix.FillJoins(com.Out)
+	for _, multi := range miner.DHT.MultiAddresses() {
+		network.RPCSendComputedOrderFragment(multi, com.Out)
+	}
 }
 
-func (miner Miner) addOrderFragment(orderFragment order.Fragment) {
-	miner.orderFragmentsMu.Lock()
-	defer miner.orderFragmentsMu.Unlock()
-	miner.orderFragments[string(orderFragment.ID)] = orderFragment
+func (miner Miner) ReconstructAll() {
+	numberOfCPUs := runtime.NumCPU()
+	reconstructables := miner.ReconstructionMatrix.WaitForJoins(numberOfCPUs)
+	do.CoForAll(joins, func(i int) {
+		miner.Reconstruct(reconstructables[i])
+	})
+}
+
+func (miner Miner) Reconstruct(orderFragments []*compute.OrderFragment) {
+	match, err := compute.IsMatch(orderFragments)
+	if err != nil {
+		return
+	}
+	if match {
+		// TODO: Do something other than logging to Stdout.
+		log.Println("match", orderFragments[0].OrderIDs)
+	}
 }
