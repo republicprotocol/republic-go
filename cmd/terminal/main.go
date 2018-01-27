6f10a594998fcda6f0806966426012dd16072692
Loongy <benjamin.jl.wang@gmail.com>
2018-01-28 10:39:55 +1100

    Arbitrary orders

--- cmd/terminal/main.go
+++ cmd/terminal/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/jbenet/go-base58"
 	"github.com/republicprotocol/go-identity"
@@ -11,67 +10,50 @@ import (
 	"github.com/republicprotocol/go-order-compute"
 )
 
+const orders = []*compute.Order{
+	compute.NewOrder(
+		compute.OrderTypeLimit,
+		compute.OrderBuy,
+		compute.CurrencyCodeETH,
+		compute.CurrencyCodeREN,
+		17200,
+		1000,
+		100,
+		0,
+	),
+	compute.NewOrder(
+		compute.OrderTypeLimit,
+		compute.OrderSell,
+		compute.CurrencyCodeETH,
+		compute.CurrencyCodeREN,
+		17200,
+		1000,
+		100,
+		0,
+	),
+	compute.NewOrder(
+		compute.OrderTypeLimit,
+		compute.OrderBuy,
+		compute.CurrencyCodeBTC,
+		compute.CurrencyCodeETH,
+		11,
+		2000,
+		2000,
+		0,
+	),
+	compute.NewOrder(
+		compute.OrderTypeLimit,
+		compute.OrderSell,
+		compute.CurrencyCodeETH,
+		compute.CurrencyCodeBTC,
+		9,
+		200,
+		200,
+		0,
+	),
+}
+
 func main() {
-
-	buyOrder := compute.NewOrder(
-		compute.OrderTypeLimit,
-		compute.OrderBuy,
-		compute.CurrencyCodeETH,
-		compute.CurrencyCodeREN,
-		17200,
-		1000,
-		100,
-		0,
-	)
-	buyOrderFragments, err := buyOrder.Split(miner.N, miner.K, miner.Prime)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	badBuyOrder := compute.NewOrder(
-		compute.OrderTypeLimit,
-		compute.OrderBuy,
-		compute.CurrencyCodeBTC,
-		compute.CurrencyCodeREN,
-		17200,
-		1000,
-		100,
-		0,
-	)
-	badBuyOrderFragments, err := badBuyOrder.Split(miner.N, miner.K, miner.Prime)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sellOrder := compute.NewOrder(
-		compute.OrderTypeLimit,
-		compute.OrderSell,
-		compute.CurrencyCodeETH,
-		compute.CurrencyCodeREN,
-		17200,
-		1000,
-		100,
-		0,
-	)
-	sellOrderFragments, err := sellOrder.Split(miner.N, miner.K, miner.Prime)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	badSellOrder := compute.NewOrder(
-		compute.OrderTypeLimit,
-		compute.OrderSell,
-		compute.CurrencyCodeBTC,
-		compute.CurrencyCodeETH,
-		17200,
-		1000,
-		100,
-		0,
-	)
-	badSellOrderFragments, err := badSellOrder.Split(miner.N, miner.K, miner.Prime)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	multiAddress1, err := identity.NewMultiAddressFromString("/ip4/127.0.0.1/tcp/3000/republic/8MGg76n7RfC6tuw23PYf85VFyM8Zto")
 	if err != nil {
@@ -86,61 +68,21 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Println("placing bad buy order", base58.Encode(badBuyOrder.ID))
-	if _, err := network.SendOrderFragment(multiAddress1, badBuyOrderFragments[0]); err != nil {
-		log.Fatal(err)
+	for _, order := range orders {
+		shares, err := order.Split(miner.N, miner.K, miner.Prime)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("placing order %s...", base58.Encode(order.ID))
+		if _, err := network.SendOrderFragment(multiAddress1, shares[0]); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := network.SendOrderFragment(multiAddress2, shares[1]); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := network.SendOrderFragment(multiAddress3, shares[2]); err != nil {
+			log.Fatal(err)
+		}
+		log.Printf(" done.")
 	}
-	log.Println("  sent", base58.Encode(badBuyOrderFragments[0].ID))
-	if _, err := network.SendOrderFragment(multiAddress2, badBuyOrderFragments[1]); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("  sent", base58.Encode(badBuyOrderFragments[1].ID))
-	if _, err := network.SendOrderFragment(multiAddress3, badBuyOrderFragments[2]); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("  sent", base58.Encode(badBuyOrderFragments[2].ID))
-
-	log.Println("placing bad sell order", base58.Encode(badSellOrder.ID))
-	if _, err := network.SendOrderFragment(multiAddress1, badSellOrderFragments[0]); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("  sent", base58.Encode(badSellOrderFragments[0].ID))
-	if _, err := network.SendOrderFragment(multiAddress2, badSellOrderFragments[1]); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("  sent", base58.Encode(badSellOrderFragments[1].ID))
-	if _, err := network.SendOrderFragment(multiAddress3, badSellOrderFragments[2]); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("  sent", base58.Encode(badSellOrderFragments[2].ID))
-
-	time.Sleep(4 * time.Second)
-
-	log.Println("placing good buy order", base58.Encode(buyOrder.ID))
-	if _, err := network.SendOrderFragment(multiAddress1, buyOrderFragments[0]); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("  sent", base58.Encode(buyOrderFragments[0].ID))
-	if _, err := network.SendOrderFragment(multiAddress2, buyOrderFragments[1]); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("  sent", base58.Encode(buyOrderFragments[1].ID))
-	if _, err := network.SendOrderFragment(multiAddress3, buyOrderFragments[2]); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("  sent", base58.Encode(buyOrderFragments[2].ID))
-
-	log.Println("placing good sell order", base58.Encode(sellOrder.ID))
-	if _, err := network.SendOrderFragment(multiAddress1, sellOrderFragments[0]); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("  sent", base58.Encode(sellOrderFragments[0].ID))
-	if _, err := network.SendOrderFragment(multiAddress2, sellOrderFragments[1]); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("  sent", base58.Encode(sellOrderFragments[1].ID))
-	if _, err := network.SendOrderFragment(multiAddress3, sellOrderFragments[2]); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("  sent", base58.Encode(sellOrderFragments[2].ID))
 }
