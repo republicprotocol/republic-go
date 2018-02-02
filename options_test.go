1307e694bd945bdbb70799bb2573a041b1f6a58b
Loongy <benjamin.jl.wang@gmail.com>
2018-02-02 17:39:28 +1100

    Simplified the test configuration after finishing large scale tests

--- options_test.go
+++ options_test.go
@@ -10,8 +10,8 @@ const (
 	DefaultOptionsDebug           = network.DebugOff
 	DefaultOptionsAlpha           = 3
 	DefaultOptionsMaxBucketLength = 20
-	DefaultOptionsTimeout         = 2 * time.Second
-	DefaultOptionsTimeoutStep     = 0 * time.Second
+	DefaultOptionsTimeout         = 30 * time.Second
+	DefaultOptionsTimeoutStep     = 30 * time.Second
 	DefaultOptionsTimeoutRetries  = 1
 	DefaultOptionsConcurrent      = false
 )
