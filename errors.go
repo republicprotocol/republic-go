b6429b561f7554e9a84b344c346e4facb6aa742b
susruth <susruth.praker@gmail.com>
2018-02-06 12:02:12 +1100

    Add Expire, Validate and Retrieve Secret Key for BTC atomic swap

---
+++ errors.go
@@ -0,0 +1,10 @@
+package btc
+
+import "fmt"
+
+// ErrFullBucket is used when a peer is inserted into a Bucket that already has
+// the maximum number of Entries.
+var (
+	ErrFullBucket = fmt.Errorf("cannot add entry to a full bucket")
+	ErrDHTAddress = fmt.Errorf("cannot add entry for the DHT address")
+)
