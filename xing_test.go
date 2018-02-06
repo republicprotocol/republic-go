fbc5b84c7d18eb64198d5769d6e848b8db360ba3
Loongy <benjamin.jl.wang@gmail.com>
2018-02-06 15:04:23 +1100

    Fixed test compilation

--- xing_test.go
+++ xing_test.go
@@ -9,14 +9,15 @@ import (
 
 	. "github.com/onsi/gomega"
 	"github.com/republicprotocol/go-identity"
+	xing "github.com/republicprotocol/go-xing"
 )
 
 var _ = Describe("X", func() {
 
 	Context("when assigning the X overlay", func() {
 
-		var miners []x.Miner
-		var overlayMiners []x.Miner
+		var miners []xing.Miner
+		var overlayMiners []xing.Miner
 
 		BeforeEach(func() {
 			epoch, err := generateEpoch()
@@ -27,19 +28,19 @@ var _ = Describe("X", func() {
 			Ω(err).ShouldNot(HaveOccurred())
 
 			// Compute the X Overlay.
-			numberOfMNetworks := x.NumberOfMNetworks(len(overlayMiners))
-			x.AssignXOverlay(overlayMiners, epoch, numberOfMNetworks)
+			numberOfMNetworks := xing.NumberOfMNetworks(len(overlayMiners))
+			xing.AssignXOverlay(overlayMiners, epoch, numberOfMNetworks)
 
 			// Clone miners into a clean slice.
-			miners = make([]x.Miner, len(overlayMiners))
+			miners = make([]xing.Miner, len(overlayMiners))
 			for i := range overlayMiners {
-				miners[i] = x.NewMiner(overlayMiners[i].ID, overlayMiners[i].Commitment)
+				miners[i] = xing.NewMiner(overlayMiners[i].ID, overlayMiners[i].Commitment)
 			}
 
 			// Compute the individual components of the Miners.
-			x.AssignXHash(miners, epoch)
-			x.AssignClass(miners, numberOfMNetworks)
-			x.AssignMNetwork(miners, numberOfMNetworks)
+			xing.AssignXHash(miners, epoch)
+			xing.AssignClass(miners, numberOfMNetworks)
+			xing.AssignMNetwork(miners, numberOfMNetworks)
 		})
 
 		It("should assign X hashes", func() {
@@ -65,7 +66,7 @@ var _ = Describe("X", func() {
 			Ω(err).ShouldNot(HaveOccurred())
 			miners, err := generateMiners()
 			Ω(err).ShouldNot(HaveOccurred())
-			x.AssignXHash(miners, epoch)
+			xing.AssignXHash(miners, epoch)
 			for _, miner := range miners {
 				Ω(bytes.Equal(miner.X, crypto.Keccak256(epoch[:], miner.Commitment[:]))).Should(Equal(true))
 			}
@@ -76,15 +77,15 @@ var _ = Describe("X", func() {
 			Ω(err).ShouldNot(HaveOccurred())
 			miners, err := generateMiners()
 			Ω(err).ShouldNot(HaveOccurred())
-			x.AssignXHash(miners, epoch)
-			Ω(x.RequireXHashes(miners)).Should(Equal(true))
+			xing.AssignXHash(miners, epoch)
+			Ω(xing.RequireXHashes(miners)).Should(Equal(true))
 		})
 	})
 
 	Context("when calculating the number of classes", func() {
 		It("should always be odd", func() {
 			for n := 7; n < 1000; n++ {
-				c := x.NumberOfClasses(n)
+				c := xing.NumberOfClasses(n)
 				Ω(c%2 == 1).Should(Equal(true))
 			}
 		})
@@ -93,28 +94,28 @@ var _ = Describe("X", func() {
 	Context("when assigning classes", func() {
 		It("should generate the correct classes", func() {
 			numberOfMiners := 1000
-			numberOfMNetworks := x.NumberOfMNetworks(numberOfMiners)
+			numberOfMNetworks := xing.NumberOfMNetworks(numberOfMiners)
 			epoch, err := generateEpoch()
 			Ω(err).ShouldNot(HaveOccurred())
 			miners, err := generateMiners()
 			Ω(err).ShouldNot(HaveOccurred())
-			x.AssignXHash(miners, epoch)
-			x.AssignClass(miners, numberOfMNetworks)
+			xing.AssignXHash(miners, epoch)
+			xing.AssignClass(miners, numberOfMNetworks)
 		})
 		It("should sort the miners", func() {
 			numberOfMiners := 1000
-			numberOfMNetworks := x.NumberOfMNetworks(numberOfMiners)
+			numberOfMNetworks := xing.NumberOfMNetworks(numberOfMiners)
 			epoch, err := generateEpoch()
 			Ω(err).ShouldNot(HaveOccurred())
 			miners, err := generateMiners()
 			Ω(err).ShouldNot(HaveOccurred())
-			x.AssignXHash(miners, epoch)
+			xing.AssignXHash(miners, epoch)
 			// Purposefully unsort the list.
 			m := miners[0]
 			miners[0] = miners[len(miners)-1]
 			miners[len(miners)-1] = m
 			// Calculate classes and ensure it is still sorted.
-			x.AssignClass(miners, numberOfMNetworks)
+			xing.AssignClass(miners, numberOfMNetworks)
 			Ω(sort.SliceIsSorted(miners, func(i, j int) bool {
 				return miners[i].X.LessThan(miners[j].X)
 			})).Should(Equal(true))
@@ -124,28 +125,28 @@ var _ = Describe("X", func() {
 	Context("when assigning M networks", func() {
 		It("should generate the correct M networks", func() {
 			numberOfMiners := 1000
-			numberOfMNetworks := x.NumberOfMNetworks(numberOfMiners)
+			numberOfMNetworks := xing.NumberOfMNetworks(numberOfMiners)
 			epoch, err := generateEpoch()
 			Ω(err).ShouldNot(HaveOccurred())
 			miners, err := generateMiners()
 			Ω(err).ShouldNot(HaveOccurred())
-			x.AssignXHash(miners, epoch)
-			x.AssignMNetwork(miners, numberOfMNetworks)
+			xing.AssignXHash(miners, epoch)
+			xing.AssignMNetwork(miners, numberOfMNetworks)
 		})
 		It("should sort the miners", func() {
 			numberOfMiners := 1000
-			numberOfMNetworks := x.NumberOfMNetworks(numberOfMiners)
+			numberOfMNetworks := xing.NumberOfMNetworks(numberOfMiners)
 			epoch, err := generateEpoch()
 			Ω(err).ShouldNot(HaveOccurred())
 			miners, err := generateMiners()
 			Ω(err).ShouldNot(HaveOccurred())
-			x.AssignXHash(miners, epoch)
+			xing.AssignXHash(miners, epoch)
 			// Purposefully unsort the list.
 			m := miners[0]
 			miners[0] = miners[len(miners)-1]
 			miners[len(miners)-1] = m
 			// Calculate classes and ensure it is still sorted.
-			x.AssignMNetwork(miners, numberOfMNetworks)
+			xing.AssignMNetwork(miners, numberOfMNetworks)
 			Ω(sort.SliceIsSorted(miners, func(i, j int) bool {
 				return miners[i].X.LessThan(miners[j].X)
 			})).Should(Equal(true))
@@ -153,7 +154,7 @@ var _ = Describe("X", func() {
 	})
 })
 
-func generateEpoch() (x.Hash, error) {
+func generateEpoch() (xing.Hash, error) {
 	id, _, err := identity.NewID()
 	if err != nil {
 		return nil, err
@@ -161,14 +162,14 @@ func generateEpoch() (x.Hash, error) {
 	return crypto.Keccak256([]byte(id.String())), nil
 }
 
-func generateMiners() ([]x.Miner, error) {
-	miners := make([]x.Miner, 100)
+func generateMiners() ([]xing.Miner, error) {
+	miners := make([]xing.Miner, 100)
 	for i := 0; i < len(miners); i++ {
 		id, _, err := identity.NewID()
 		if err != nil {
 			return nil, err
 		}
-		miners[i] = x.NewMiner(id, crypto.Keccak256([]byte(id.String())))
+		miners[i] = xing.NewMiner(id, crypto.Keccak256([]byte(id.String())))
 	}
 	return miners, nil
 }
