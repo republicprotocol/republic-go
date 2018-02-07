5bd888016d71d2b41c45cd56d01aac5700ebc656
susruth <susruth.praker@gmail.com>
2018-02-07 14:24:49 +1100

    Add validation checking for BTC atomic swapping

---
+++ btc_validate.go
@@ -0,0 +1,98 @@
+package btc
+
+type validateCmd struct {
+	contract   []byte
+	contractTx *wire.MsgTx
+}
+
+func Validate(contractString string, contractTransaction string, chain string, rpcuser string, rpcpass string) (Error error, showUsage bool) {
+	var chainParams *chaincfg.Params ;
+	if (chain == "testnet"){
+		chainParams = &chaincfg.TestNet3Params
+	} else {
+		chainParams = &chaincfg.MainNetParams
+	}
+
+	contract, err := hex.DecodeString(contractString)
+		if err != nil {
+			return fmt.Errorf("failed to decode contract: %v", err), true
+		}
+
+		contractTxBytes, err := hex.DecodeString(contractTransaction)
+		if err != nil {
+			return fmt.Errorf("failed to decode contract transaction: %v", err), true
+		}
+		var contractTx wire.MsgTx
+		err = contractTx.Deserialize(bytes.NewReader(contractTxBytes))
+		if err != nil {
+			return fmt.Errorf("failed to decode contract transaction: %v", err), true
+		}
+
+		cmd := &validateCmd{contract: contract, contractTx: &contractTx}
+		err = cmd.runCommand(chainParams)
+		return err, false
+}
+
+func (cmd *validateCmd) runCommand(chainParams *chaincfg.Params) error {
+	contractHash160 := btcutil.Hash160(cmd.contract)
+	contractOut := -1
+	for i, out := range cmd.contractTx.TxOut {
+		sc, addrs, _, err := txscript.ExtractPkScriptAddrs(out.PkScript, chainParams)
+		if err != nil || sc != txscript.ScriptHashTy {
+			continue
+		}
+		if bytes.Equal(addrs[0].(*btcutil.AddressScriptHash).Hash160()[:], contractHash160) {
+			contractOut = i
+			break
+		}
+	}
+	if contractOut == -1 {
+		return errors.New("transaction does not contain the contract output")
+	}
+
+	pushes, err := txscript.ExtractAtomicSwapDataPushes(cmd.contract)
+	if err != nil {
+		return err
+	}
+	if pushes == nil {
+		return errors.New("contract is not an atomic swap script recognized by this tool")
+	}
+
+	contractAddr, err := btcutil.NewAddressScriptHash(cmd.contract, chainParams)
+	if err != nil {
+		return err
+	}
+	recipientAddr, err := btcutil.NewAddressPubKeyHash(pushes.RecipientHash160[:],
+		chainParams)
+	if err != nil {
+		return err
+	}
+	refundAddr, err := btcutil.NewAddressPubKeyHash(pushes.RefundHash160[:],
+		chainParams)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Contract address:        %v\n", contractAddr)
+	fmt.Printf("Contract value:          %v\n", btcutil.Amount(cmd.contractTx.TxOut[contractOut].Value))
+	fmt.Printf("Recipient address:       %v\n", recipientAddr)
+	fmt.Printf("Author's refund address: %v\n\n", refundAddr)
+
+	fmt.Printf("Secret hash: %x\n\n", pushes.SecretHash[:])
+
+	if pushes.LockTime >= int64(txscript.LockTimeThreshold) {
+		t := time.Unix(pushes.LockTime, 0)
+		fmt.Printf("Locktime: %v\n", t.UTC())
+		reachedAt := time.Until(t).Truncate(time.Second)
+		if reachedAt > 0 {
+			fmt.Printf("Locktime reached in %v\n", reachedAt)
+		} else {
+			fmt.Printf("Contract refund time lock has expired\n")
+		}
+	} else {
+		fmt.Printf("Locktime: block %v\n", pushes.LockTime)
+	}
+
+	return nil
+}
+
