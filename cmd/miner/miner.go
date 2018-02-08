ca401defb44ae2a6b895553f0bdda9cb5045f9ab
Loongy <benjamin.jl.wang@gmail.com>
2018-02-08 14:50:11 +1100

    Renamed main to miner

---
+++ cmd/miner/miner.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"log"
+
+	"github.com/republicprotocol/go-miner"
+)
+
+var config *miner.Config
+
+func main() {
+	// Parse command line arguments and fill the miner.Config.
+	if err := parseCommandLineFlags(); err != nil {
+		log.Println(err)
+		flag.Usage()
+		return
+	}
+
+	// Create a new miner.Miner.
+	miner, err := miner.NewMiner(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	miner.Start()
+
+	// Establish connections to bootstrap swarm.Nodes.
+	go func() {
+		miner.EstablishConnections()
+	}()
+}
+
+func parseCommandLineFlags() error {
+	confFilename := flag.String("config", "", "Path to the JSON configuration file")
+
+	flag.Parse()
+
+	if *confFilename == "" {
+		return errors.New("no config file given")
+	}
+
+	conf, err := miner.LoadConfig(*confFilename)
+	if err != nil {
+		return err
+	}
+	config = conf
+
+	return nil
+}
