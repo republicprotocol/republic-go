9fb1b81576f9c30832fecaecebece34881d4b314
Loongy <benjamin.jl.wang@gmail.com>
2018-02-08 14:19:07 +1100

    Terminal loads orders from file

--- cmd/terminal/main.go
+++ cmd/terminal/main.go
@@ -1,116 +1,86 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
+	"flag"
 	"log"
+	"os"
 	"time"
+
+	"github.com/republicprotocol/go-order-compute"
 
 	"github.com/jbenet/go-base58"
 	"github.com/republicprotocol/go-identity"
 	"github.com/republicprotocol/go-miner"
-	"github.com/republicprotocol/go-order-compute"
-	rpc "github.com/republicprotocol/go-rpc"
+	"github.com/republicprotocol/go-rpc"
 )
 
-var orders = []*compute.Order{
-	// Buy ETH for REN
-	compute.NewOrder(
-		compute.OrderTypeLimit,
-		compute.OrderParityBuy,
-		compute.CurrencyCodeETH,
-		compute.CurrencyCodeREN,
-		17200,
-		1000,
-		1000,
-		0,
-	),
-	// Sell ETH for REN
-	compute.NewOrder(
-		compute.OrderTypeLimit,
-		compute.OrderParitySell,
-		compute.CurrencyCodeETH,
-		compute.CurrencyCodeREN,
-		17200,
-		1000,
-		1000,
-		0,
-	),
-
-	// Buy BTC for ETH
-	compute.NewOrder(
-		compute.OrderTypeLimit,
-		compute.OrderParityBuy,
-		compute.CurrencyCodeBTC,
-		compute.CurrencyCodeETH,
-		12,
-		2000,
-		200,
-		0,
-	),
-	// Sell BTC for ETH
-	compute.NewOrder(
-		compute.OrderTypeLimit,
-		compute.OrderParitySell,
-		compute.CurrencyCodeBTC,
-		compute.CurrencyCodeETH,
-		8,
-		200,
-		100,
-		0,
-	),
-
-	// Buy BTC for REN
-	compute.NewOrder(
-		compute.OrderTypeLimit,
-		compute.OrderParityBuy,
-		compute.CurrencyCodeBTC,
-		compute.CurrencyCodeREN,
-		172000,
-		1000,
-		1000,
-		0,
-	),
-	// Sell REN for BTC
-	compute.NewOrder(
-		compute.OrderTypeLimit,
-		compute.OrderParitySell,
-		compute.CurrencyCodeREN,
-		compute.CurrencyCodeBTC,
-		172000,
-		1000,
-		1000,
-		0,
-	),
-}
+var config map[string]interface{}
+var order *compute.Order
 
 func main() {
 
-	traderMultiAddress, err := identity.NewMultiAddressFromString("/ip4/127.0.0.1/tcp/4000/republic/8MJgvgEeiM4jRnsYM2v4Vm1sL7udGi")
+	// Parse command line arguments and fill the miner.Config.
+	if err := parseCommandLineFlags(); err != nil {
+		log.Println(err)
+		flag.Usage()
+		return
+	}
+
+	traderMultiAddress, err := identity.NewMultiAddressFromString(config["multi_address"].(string))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	minerMultiAddressString := []string{"8MGg76n7RfC6tuw23PYf85VFyM8Zto", "8MHarRJdvWd7SsTJE8vRVfj2jb5cWS", "8MKZ8JwCU9m9affPWHZ9rxp2azXNnE"}
-	minerMultiAddresses := make(identity.MultiAddresses, miner.N)
-	for i := range minerMultiAddresses {
-		multiAddress, err := identity.NewMultiAddressFromString(fmt.Sprintf("/ip4/127.0.0.1/tcp/%d/republic/%s", 3000+i, minerMultiAddressString[i]))
+	nodeMultiAddressStrings := config["node_multi_addresses"].([]string)
+	nodeMultiAddresses := make(identity.MultiAddresses, len(nodeMultiAddressStrings))
+	for i := range nodeMultiAddresses {
+		multiAddress, err := identity.NewMultiAddressFromString(nodeMultiAddressStrings[i])
 		if err != nil {
 			log.Fatal(err)
 		}
-		minerMultiAddresses[i] = multiAddress
+		nodeMultiAddresses[i] = multiAddress
 	}
 
-	for _, order := range orders {
-		shares, err := order.Split(miner.N, miner.K, miner.Prime)
-		if err != nil {
+	shares, err := order.Split(miner.N, miner.K, miner.Prime)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%v fragmented and distributed\n", base58.Encode(order.ID))
+
+	for i := range shares {
+		log.Printf("  %v sent to %v\n", base58.Encode(shares[i].ID), nodeMultiAddresses[i].Address())
+		if err := rpc.SendOrderFragmentToTarget(nodeMultiAddresses[i], nodeMultiAddresses[i].Address(), traderMultiAddress, shares[i], 5*time.Second); err != nil {
 			log.Fatal(err)
 		}
-		log.Println("placing order", base58.Encode(order.ID))
-
-		for i := range shares {
-			if err := rpc.SendOrderFragmentToTarget(minerMultiAddresses[i], minerMultiAddresses[i].Address(), traderMultiAddress, shares[i], 5*time.Second); err != nil {
-				log.Fatal(err)
-			}
-		}
 	}
+}
+
+func parseCommandLineFlags() error {
+	confFilename := flag.String("config", "", "Path to the JSON configuration file")
+	orderFilename := flag.String("order", "", "Path to the JSON order file")
+
+	flag.Parse()
+
+	if *confFilename == "" {
+		return errors.New("no config file given")
+	}
+
+	file, err := os.Open(*confFilename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	if err := json.NewDecoder(file).Decode(&config); err != nil {
+		return err
+	}
+
+	file, err = os.Open(*orderFilename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	order = new(compute.Order)
+	return json.NewDecoder(file).Decode(order)
 }
