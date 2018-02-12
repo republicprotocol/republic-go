6d56365ac83bfbcf8228a7c592c76a029640de72
Loongy <benjamin.jl.wang@gmail.com>
2018-02-13 10:59:33 +1100

    Test stubs

---
+++ ethereum/ethereum_test.go
@@ -0,0 +1,12 @@
+package ethereum_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/republicprotocol/go-atom/ethereum"
+)
+
+var _ = Describe("Ethereum", func() {
+
+})
