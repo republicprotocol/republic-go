010598f25f81cde2411334fe3a8ac928cd8f5b07
Loongy <benjamin.jl.wang@gmail.com>
2018-02-13 10:59:33 +1100

    Test stubs

---
+++ bitcoin/bitcoin_test.go
@@ -0,0 +1,12 @@
+package bitcoin_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/republicprotocol/go-atom/bitcoin"
+)
+
+var _ = Describe("Bitcoin", func() {
+
+})
---
+++ go_atom_test.go
@@ -0,0 +1,12 @@
+package atom_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/republicprotocol/go-atom"
+)
+
+var _ = Describe("Atom", func() {
+
+})
