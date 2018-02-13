34a5750c2831367748d19ca0d7e834262842b5ca
susruth <susruth.praker@gmail.com>
2018-02-14 10:14:26 +1100

    Simplified bitcoin atomic swap open function

--- bitcoin/btc_open.go
+++ bitcoin/btc_open.go
@@ -1,24 +1,16 @@
-package main
+package bitcoin
 
 import (
-	"time"
 	"fmt"
 	"errors"
 	"bytes"
-	
-	"crypto/rand"
 
 	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
 	rpc "github.com/btcsuite/btcd/rpcclient"
 )
 
-type openCmd struct {
-	cp2Addr *btcutil.AddressPubKeyHash
-	amount  btcutil.Amount
-}
-
-func Open(participantAddress string, value float64, chain string, rpcuser string, rpcpass string) (err error, showUsage bool) {
+func Open(participantAddress string, value int64, chain string, rpcuser string, rpcpass string, hash []byte, lockTime int64) (err error, result openResult) {
 	var chainParams *chaincfg.Params ;
 	if (chain == "testnet"){
 		chainParams = &chaincfg.TestNet3Params
@@ -28,27 +20,20 @@ func Open(participantAddress string, value float64, chain string, rpcuser string
 
 	cp2Addr, err := btcutil.DecodeAddress(participantAddress, chainParams)
 	if err != nil {
-		return fmt.Errorf("failed to decode participant address: %v", err), true
+		return fmt.Errorf("failed to decode participant address: %v", err), openResult{}
 	}
 	if !cp2Addr.IsForNet(chainParams) {
 		return fmt.Errorf("participant address is not "+
-			"intended for use on %v", chainParams.Name), true
+			"intended for use on %v", chainParams.Name), openResult{}
 	}
 	cp2AddrP2PKH, ok := cp2Addr.(*btcutil.AddressPubKeyHash)
 	if !ok {
-		return errors.New("participant address is not P2PKH"), true
+		return errors.New("participant address is not P2PKH"), openResult{}
 	}
-
-	amount, err := btcutil.NewAmount(value)
-	if err != nil {
-		return err, true
-	}
-	cmd := &openCmd{cp2Addr: cp2AddrP2PKH, amount: amount}
-
 
 	connect, err := normalizeAddress("localhost", walletPort(chainParams))
 	if err != nil {
-		return fmt.Errorf("wallet server address: %v", err), true
+		return fmt.Errorf("wallet server address: %v", err), openResult{}
 	}
 
 	connConfig := &rpc.ConnConfig{
@@ -61,58 +46,41 @@ func Open(participantAddress string, value float64, chain string, rpcuser string
 
 	client, err := rpc.New(connConfig, nil)
 	if err != nil {
-		return fmt.Errorf("rpc connect: %v", err), false
+		return fmt.Errorf("rpc connect: %v", err), openResult{}
 	}
 	defer func() {
 		client.Shutdown()
 		client.WaitForShutdown()
 	}()
 
-	err = cmd.runCommand(client)
-	return err, false
-}
-
-func (cmd *openCmd) runCommand(c *rpc.Client) error {
-	var secret [secretSize]byte
-	_, err := rand.Read(secret[:])
+	b, err := buildContract(client, &contractArgs{
+		them:       cp2AddrP2PKH,
+		amount:     value,
+		locktime:   lockTime,
+		secretHash: hash,
+	}, chain)
 	if err != nil {
-		return err
-	}
-	secretHash := sha256Hash(secret[:])
-
-	// locktime after 500,000,000 (Tue Nov  5 00:53:20 1985 UTC) is interpreted
-	// as a unix time rather than a block height.
-	locktime := time.Now().Add(48 * time.Hour).Unix()
-
-	b, err := buildContract(c, &contractArgs{
-		them:       cmd.cp2Addr,
-		amount:     cmd.amount,
-		locktime:   locktime,
-		secretHash: secretHash,
-	},"testnet")
-	if err != nil {
-		return err
+		return err, openResult{}
 	}
 
-	refundTxHash := b.refundTx.TxHash()
-	contractFeePerKb := calcFeePerKb(b.contractFee, b.contractTx.SerializeSize())
-	refundFeePerKb := calcFeePerKb(b.refundFee, b.refundTx.SerializeSize())
-
-	fmt.Printf("Secret:      %x\n", secret)
-	fmt.Printf("Secret hash: %x\n\n", secretHash)
-	fmt.Printf("Contract fee: %v (%0.8f BTC/kB)\n", b.contractFee, contractFeePerKb)
-	fmt.Printf("Refund fee:   %v (%0.8f BTC/kB)\n\n", b.refundFee, refundFeePerKb)
-	fmt.Printf("Contract (%v):\n", b.contractP2SH)
-	fmt.Printf("%x\n\n", b.contract)
 	var contractBuf bytes.Buffer
 	contractBuf.Grow(b.contractTx.SerializeSize())
 	b.contractTx.Serialize(&contractBuf)
-	fmt.Printf("Contract transaction (%v):\n", b.contractTxHash)
-	fmt.Printf("%x\n\n", contractBuf.Bytes())
+
 	var refundBuf bytes.Buffer
 	refundBuf.Grow(b.refundTx.SerializeSize())
 	b.refundTx.Serialize(&refundBuf)
-	fmt.Printf("Refund transaction (%v):\n", &refundTxHash)
-	fmt.Printf("%x\n\n", refundBuf.Bytes())
-	return promptPublishTx(c, b.contractTx, "contract")
+
+	if err := promptPublishTx(c, b.contractTx, "contract"); err != nil {
+		return err, openResult{}
+	}
+
+	return nil, openResult{
+		contract: b.contract,
+		contractHash: b.contractP2SH.EncodeAddress(),
+		contractTx: contractBuf.Bytes(),
+		contractTxHash: b.contractTxHash,
+		refundTx: refundBuf.Bytes(),
+		refundTxHash: &b.refundTx.TxHash(),
+	}
 }
