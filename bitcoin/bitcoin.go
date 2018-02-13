996b72649339a75374dd3ca376a611c67c2f4308
susruth <susruth.praker@gmail.com>
2018-02-14 10:55:44 +1100

    Updated bitcoin atomic swap entry file

--- bitcoin/bitcoin.go
+++ bitcoin/bitcoin.go
@@ -1,28 +1,68 @@
 package bitcoin
 
 type BTCAtomContract struct {
+	rpcUser    string
+	rpcPass    string
+	chain      string
+	secretHash []byte
+	ledgerData BitcoinData
 }
 
-func NewBTCAtomContract() *BTCAtomContract {
-	return &BTCAtomContract{}
+type BitcoinData struct {
+	contractHash   string
+	contract       []byte
+	contractTxHash []byte
+	contractTx     []byte
+	refundTxHash   [32]byte
+	refundTx       []byte
+	redeemTxHash   [32]byte
+	redeemTx       []byte
+}
+
+func NewBTCAtomContract(rpcUser string, rpcPass string, chain string) *BTCAtomContract {
+	return &BTCAtomContract{
+		rpcUser: rpcUser,
+		rpcPass: rpcPass,
+		chain:   chain,
+	}
 }
 
 func (contract *BTCAtomContract) Initiate(hash, to, from []byte, value, expiry int64) (err error) {
-	panic("unimplemented")
+	err, result := Open(string(to), value, contract.chain, contract.rpcUser, contract.rpcPass, hash, expiry)
+	if err != nil {
+		return err
+	}
+	contract.ledgerData = result
+	contract.secretHash = hash
+	return nil
 }
 
 func (contract *BTCAtomContract) Read() (hash, to, from []byte, value, expiry int64, err error) {
-	panic("unimplemented")
+	err, result := Validate(contract.ledgerData.contract, contract.ledgerData.contractTx, contract.chain, contract.rpcUser, contract.rpcPass)
+	if err != nil {
+		return []byte{}, []byte{}, []byte{}, 0, 0, err
+	}
+	return result.secretHash, result.recipientAddress, result.recipientAddress, result.amount, result.lockTime, nil
+}
+
+func (contract *BTCAtomContract) Redeem(secret []byte) error {
+	err, result := Close(contract.ledgerData.contract, contract.ledgerData.contractTx, secret, contract.rpcUser, contract.rpcPass, contract.chain)
+	if err != nil {
+		return err
+	}
+	contract.ledgerData.redeemTx = result.redeemTx
+	contract.ledgerData.redeemTxHash = result.redeemTxHash
+	return nil
 }
 
 func (contract *BTCAtomContract) ReadSecret() (secret []byte, err error) {
-	panic("unimplemented")
-}
-
-func (contract *BTCAtomContract) Redeem() error {
-	panic("unimplemented")
+	err, result := ExtractSecret(contract.ledgerData.redeemTx, contract.secretHash, contract.rpcUser, contract.rpcPass)
+	if err != nil {
+		return []byte{}, err
+	}
+	return result, nil
 }
 
 func (contract *BTCAtomContract) Refund() error {
-	panic("unimplemented")
+	return Expire(contract.ledgerData.contract, contract.ledgerData.contractTx, contract.chain, contract.rpcUser, contract.rpcPass)
 }
