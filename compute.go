937e38a90f1932dd4a9e920476d05a88e1894347
Loongy <benjamin.jl.wang@gmail.com>
2018-02-14 14:55:32 +1100

    Computer uses guarded objects

--- compute.go
+++ compute.go
@@ -2,8 +2,8 @@ package compute
 
 import (
 	"math/big"
-	"sync"
-	"sync/atomic"
+
+	"github.com/republicprotocol/go-do"
 
 	"github.com/ethereum/go-ethereum/crypto"
 )
@@ -41,46 +41,41 @@ func (computation *Computation) Sub(prime *big.Int) (*ResultFragment, error) {
 }
 
 type Computer struct {
+	do.GuardedObject
+
 	orderFragments []*OrderFragment
 
-	computationsMu       *sync.Mutex
-	computationsLeftCond *sync.Cond
-	computations         []*Computation
-	computationsLeft     int64
-	computationsMarker   map[string]struct{}
-
-	resultsMu       *sync.Mutex
-	results         map[string]*Result
-	resultFragments map[string][]*ResultFragment
+	computations           []*Computation
+	computationsIsNotEmpty *do.Guard
 }
 
-func NewComputationMatrix() *Computer {
-	return &Computer{
-		orderFragments: []*OrderFragment{},
-
-		computationsMu:       new(sync.Mutex),
-		computationsLeftCond: sync.NewCond(new(sync.Mutex)),
-		computations:         []*Computation{},
-		computationsLeft:     0,
-		computationsMarker:   map[string]struct{}{},
-
-		resultsMu:       new(sync.Mutex),
-		results:         map[string]*Result{},
-		resultFragments: map[string][]*ResultFragment{},
-	}
+// NewComputer returns a new Computer with no OrderFragments, or Computations.
+func NewComputer() *Computer {
+	computer := new(Computer)
+	computer.GuardedObject = do.NewGuardedObject()
+	computer.orderFragments = make([]*OrderFragment, 0)
+	computer.computations = make([]*Computation, 0)
+	computer.computationsIsNotEmpty = computer.Guard(func() bool { return len(computer.computations) > 0 })
+	return computer
 }
 
-func (matrix *Computer) AddOrderFragment(orderFragment *OrderFragment) {
-	matrix.computationsMu.Lock()
-	defer matrix.computationsMu.Unlock()
+func (computer *Computer) AddOrderFragment(orderFragment *OrderFragment) {
+	computer.Enter(nil)
+	defer computer.Exit()
+	computer.addOrderFragment(orderFragment)
+}
 
-	for _, rhs := range matrix.orderFragments {
+func (computer *Computer) addOrderFragment(orderFragment *OrderFragment) {
+	// Check that the OrderFragment has not been added.
+	for _, rhs := range computer.orderFragments {
 		if orderFragment.ID.Equals(rhs.ID) {
 			return
 		}
 	}
 
-	for _, other := range matrix.orderFragments {
+	// For all other OrderFragment in the Computer, create a new Computation
+	// that needs to be processed.
+	for _, other := range computer.orderFragments {
 		if orderFragment.OrderID.Equals(other.OrderID) {
 			continue
 		}
@@ -91,77 +86,17 @@ func (matrix *Computer) AddOrderFragment(orderFragment *OrderFragment) {
 		if err != nil {
 			continue
 		}
-		matrix.computations = append(matrix.computations, computation)
-		atomic.AddInt64(&matrix.computationsLeft, 1)
+		computer.computations = append(computer.computations, computation)
 	}
-
-	matrix.orderFragments = append(matrix.orderFragments, orderFragment)
-	if atomic.LoadInt64(&matrix.computationsLeft) > 0 {
-		matrix.computationsLeftCond.Signal()
-	}
+	computer.orderFragments = append(computer.orderFragments, orderFragment)
 }
 
-func (matrix *Computer) WaitForComputations(max int) []*Computation {
-	matrix.computationsLeftCond.L.Lock()
-	defer matrix.computationsLeftCond.L.Unlock()
-	for atomic.LoadInt64(&matrix.computationsLeft) == 0 {
-		matrix.computationsLeftCond.Wait()
-	}
-
-	matrix.computationsMu.Lock()
-	defer matrix.computationsMu.Unlock()
-
-	computations := make([]*Computation, 0, max)
-	for _, computation := range matrix.computations {
-		if _, ok := matrix.computationsMarker[string(computation.ID)]; !ok {
-			matrix.computationsMarker[string(computation.ID)] = struct{}{}
-			computations = append(computations, computation)
-			if len(computations) == max {
-				break
-			}
-		}
-	}
-	atomic.AddInt64(&matrix.computationsLeft, -int64(len(computations)))
-	return computations
+func (computer *Computer) Compute() []*Result {
+	computer.Enter(computer.computationsIsNotEmpty)
+	defer computer.Exit()
+	return computer.compute()
 }
 
-func (matrix *Computer) AddResultFragments(resultFragments []*ResultFragment, k int64, prime *big.Int) ([]*Result, error) {
-	matrix.resultsMu.Lock()
-	defer matrix.resultsMu.Unlock()
-
-	results := make([]*Result, 0, len(resultFragments))
-	for _, resultFragment := range resultFragments {
-		resultID := ResultID(crypto.Keccak256(resultFragment.BuyOrderID[:], resultFragment.SellOrderID[:]))
-
-		// Check that this result fragment has not been collected yet.
-		resultFragmentIsUnique := true
-		for _, candidate := range matrix.resultFragments[string(resultID)] {
-			if candidate.ID.Equals(resultFragment.ID) {
-				resultFragmentIsUnique = false
-				break
-			}
-		}
-		if resultFragmentIsUnique {
-			matrix.resultFragments[string(resultID)] = append(matrix.resultFragments[string(resultID)], resultFragment)
-		}
-
-		if int64(len(matrix.resultFragments[string(resultID)])) >= k {
-			if result, ok := matrix.results[string(resultID)]; result != nil && ok {
-				// FIXME: At the moment we are only returning new results. Do
-				// we want to return results we have already found?
-				continue
-			}
-			result := NewResult(matrix.resultFragments[string(resultID)], prime)
-			matrix.results[string(resultID)] = result
-			results = append(results, result)
-		}
-	}
-	return results, nil
-}
-
-func (matrix *Computer) ComputationsLeft() int64 {
-	matrix.computationsLeftCond.L.Lock()
-	defer matrix.computationsLeftCond.L.Unlock()
-
-	return matrix.computationsLeft
+func (computer *Computer) compute() []*Result {
+	panic("unimplemented")
 }
