f27541e6af6eeb99c4f61a48a7106c9562612b7d
Loongy <benjamin.jl.wang@gmail.com>
2018-02-16 10:25:02 +1100

    Inbox uses a more efficient tracker for new results

--- inbox.go
+++ inbox.go
@@ -8,17 +8,17 @@ import (
 type Inbox struct {
 	do.GuardedObject
 
+	r            int
 	results      []*compute.Result
-	newResults   []*compute.Result
 	hasNewResult *do.Guard
 }
 
 func NewInbox() *Inbox {
 	inbox := new(Inbox)
 	inbox.GuardedObject = do.NewGuardedObject()
+	inbox.r = 0
 	inbox.results = make([]*compute.Result, 0)
-	inbox.newResults = make([]*compute.Result, 0)
-	inbox.hasNewResult = inbox.Guard(func() bool { return len(inbox.newResults) > 0 })
+	inbox.hasNewResult = inbox.Guard(func() bool { return r < len(inbox.results) })
 	return inbox
 }
 
@@ -26,23 +26,22 @@ func (inbox *Inbox) AddNewResult(result *compute.Result) {
 	inbox.Enter(nil)
 	defer inbox.Exit()
 	inbox.results = append(inbox.results, result)
-	inbox.newResults = append(inbox.newResults, result)
 }
 
 func (inbox *Inbox) GetAllNewResults() []*compute.Result {
 	inbox.Enter(inbox.hasNewResult)
 	defer inbox.Exit()
-	ret := make([]*compute.Result, 0, len(inbox.newResults))
-	ret = append(ret, inbox.newResults...)
-	inbox.newResults = inbox.newResults[:0]
+	ret := make([]*compute.Result, 0, len(inbox.results)-r)
+	ret = append(ret, inbox.results[r:]...)
+	inbox.r = len(inbox.results)
 	return ret
 }
 
-func (inbox *Inbox) GetAllResults() []*compute.Result{
+func (inbox *Inbox) GetAllResults() []*compute.Result {
 	inbox.Enter(nil)
 	defer inbox.Exit()
 	ret := make([]*compute.Result, 0, len(inbox.results))
 	ret = append(ret, inbox.results...)
-	inbox.newResults = inbox.newResults[:0]
+	inbox.r = len(inbox.results)
 	return ret
 }
