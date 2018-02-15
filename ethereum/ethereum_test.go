c2b970d496c9c23f2f6a119dbad3c295adf92ae3
Noah <noahingham@protonmail.com>
2018-02-15 17:20:10 +1100

    Added support for running tests on both ropsten and simulated backend

--- ethereum/ethereum_test.go
+++ ethereum/ethereum_test.go
@@ -18,40 +18,50 @@ import (
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/core/types"
 )
 
 var ether = big.NewInt(1000000000000000000)
-var contractAddress = common.HexToAddress("0xbd59e72598737a08a68fe192df04c773adbbfa53")
 
 const key1 = `{"version":3,"id":"7844982f-abe7-4690-8c15-34f75f847c66","address":"db205ea9d35d8c01652263d58351af75cfbcbf07","Crypto":{"ciphertext":"378dce3c1279b36b071e1c7e2540ac1271581bff0bbe36b94f919cb73c491d3a","cipherparams":{"iv":"2eb92da55cc2aa62b7ffddba891f5d35"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"80d3341678f83a14024ba9c3edab072e6bd2eea6aa0fbc9e0a33bae27ffa3d6d","n":8192,"r":8,"p":1},"mac":"3d07502ea6cd6b96a508138d8b8cd2e46c3966240ff276ce288059ba4235cb0d"}}`
 const key2 = `{"version":3,"id":"1bc823af-210a-4143-8eb4-306c19485622","address":"d95bd5b44a1290c91a31af1114e49b961e56b03b","Crypto":{"ciphertext":"0eb788eee71b9796390d6b3495c25d87746a7c2ddd98a641b90e7271231f6df0","cipherparams":{"iv":"35decc930518c37116e8fcf1f9933948"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"a3b2f03dc27ee3c89b6d21b1e0d1973bb130524e7570bd0bf4da531313df9730","n":8192,"r":8,"p":1},"mac":"ef73d708b39832e6be65db73d088ab60a2c7711189a330324b410bc64c6bfe7a"}}`
 
-func loadAccounts(client *ethclient.Client) (*bind.TransactOpts, *bind.TransactOpts) {
+func loadClient() (ethereum.Client, *bind.TransactOpts, *bind.TransactOpts, common.Address) {
+
 	auth1, err := bind.NewTransactor(strings.NewReader(key1), "password1")
 	Ω(err).Should(BeNil())
 
 	auth2, err := bind.NewTransactor(strings.NewReader(key2), "password2")
 	Ω(err).Should(BeNil())
 
-	// Ensure that the bigger acount is sending to the smaller account
+	sim := true
+	var client ethereum.Client
+	var address common.Address
+	if sim {
+		client = ethereum.Simulated(auth1, auth2)
+		var tx *types.Transaction
+		tx, address = ethereum.DeployETH(client, auth1)
+		ethereum.PatchedWaitDeployed(context.Background(), client, tx)
+	} else {
+		// Connect to Infura (or use local node at 13.54.129.55:8180)
+		client = ethereum.Ropsten("https://ropsten.infura.io/")
+		address = common.HexToAddress("0xbd59e72598737a08a68fe192df04c773adbbfa53")
+	}
+
 	bal1, _ := client.BalanceAt(context.Background(), auth1.From, nil)
 	bal2, _ := client.BalanceAt(context.Background(), auth2.From, nil)
 	if bal1.Cmp(bal2) > 0 {
 		auth1, auth2 = auth2, auth1
 	}
 
-	return auth1, auth2
+	return client, auth1, auth2, address
 }
 
 var _ = Describe("Ethereum", func() {
 
 	It("can swap with an arbitrary ledger", func() {
-		// Connect to Infura (or use local node at 13.54.129.55:8180)
-		client := ethereum.Ropsten("https://ropsten.infura.io/")
-		// Load accounts
-		auth1, auth2 := loadAccounts(client)
-		// Contract address
+		// Load client and accounts
+		client, auth1, auth2, contractAddress := loadClient()
 
 		// ALICE has locked up tokens on another ledger with the following secret
 		secret := []byte("this is the secret")
@@ -84,8 +94,8 @@ var _ = Describe("Ethereum", func() {
 	})
 
 	It("can't redeem with a wrong password", func() {
-		client := ethereum.Ropsten("https://ropsten.infura.io/")
-		auth1, auth2 := loadAccounts(client)
+		// Load client and accounts
+		client, auth1, auth2, contractAddress := loadClient()
 
 		secret := []byte("this is the secret")
 		secretHash := sha256.Sum256(secret)
@@ -107,8 +117,8 @@ var _ = Describe("Ethereum", func() {
 	})
 
 	It("can't expire before timeout", func() {
-		client := ethereum.Ropsten("https://ropsten.infura.io/")
-		auth1, auth2 := loadAccounts(client)
+		// Load client and accounts
+		client, auth1, auth2, contractAddress := loadClient()
 
 		secret := []byte("this is the secret")
 		secretHash := sha256.Sum256(secret)
@@ -129,8 +139,8 @@ var _ = Describe("Ethereum", func() {
 	})
 
 	It("can expire after timeout", func() {
-		client := ethereum.Ropsten("https://ropsten.infura.io/")
-		auth1, auth2 := loadAccounts(client)
+		// Load client and accounts
+		client, auth1, auth2, contractAddress := loadClient()
 
 		secret := []byte("this is the secret")
 		secretHash := sha256.Sum256(secret)
@@ -150,11 +160,12 @@ var _ = Describe("Ethereum", func() {
 	})
 
 	It("can't expire an expired order", func() {
-		hash, err := hex.DecodeString("1d2f007c416ddec089ba60cd052e64e25f8fe7dde85625e2d3354b8faf3ef4ae")
+		hash, err := hex.DecodeString("bdcf34f235c01c96c6110b30ea4cec8b78e90304271db5cef8b1b1eb34f0f71b")
 		Ω(err).Should(BeNil())
 
-		client := ethereum.Ropsten("https://ropsten.infura.io/")
-		auth, _ := loadAccounts(client)
+		// Load client and accounts
+		client, auth, _, contractAddress := loadClient()
+
 		connection, _ := ethereum.NewETHAtomContract(context.Background(), client, auth, contractAddress, hash)
 
 		// Should NOT be able to refund
