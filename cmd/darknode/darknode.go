fdb95c633989a3f26f6391e9dba6b179f549d5e5
tok-kkk <sacrifise000@gmail.com>
2018-02-23 16:06:29 +1100

    merge susruth work

--- cmd/darknode/darknode.go
+++ cmd/darknode/darknode.go
@@ -3,9 +3,12 @@ package main
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"log"
+	"os/exec"
 
 	"github.com/republicprotocol/go-dark-node"
+	"github.com/republicprotocol/go-identity"
 )
 
 var config *node.Config
@@ -29,19 +32,68 @@ func main() {
 }
 
 func parseCommandLineFlags() error {
-	confFilename := flag.String("config", "", "Path to the JSON configuration file")
+	confFilename := flag.String("config", "./default-config.json", "Path to the JSON configuration file")
 
 	flag.Parse()
 
-	if *confFilename == "" {
-		return errors.New("no config file given")
-	}
-
 	conf, err := node.LoadConfig(*confFilename)
 	if err != nil {
-		return err
+		conf, err = LoadDefaultConfig();
+		if err != nil {
+			return err;
+		}
+		config = conf
+		return nil;
 	}
 	config = conf
-
 	return nil
+}
+
+func LoadDefaultConfig() (*node.Config, error) {
+	address, keypair, err := identity.NewAddress()
+	if err != nil {
+		return &node.Config{}, err
+	}
+	out, err := exec.Command("curl https://ipinfo.io/ip").Output()
+	if err != nil {
+		return &node.Config{}, err
+	}
+	if err != nil {
+		return &node.Config{}, err
+	}
+	multiAddress, err := identity.NewMultiAddressFromString(fmt.Sprintf("/ip4/%s/tcp/18514/republic/%s", out, address))
+	if err != nil {
+		return &node.Config{}, err
+	}
+	bootstrap1, err := identity.NewMultiAddressFromString("/ip4/52.78.129.188/tcp/18514/republic/8MKZ8JwCU9m9affPWHZ9rxp2azXNnE")
+	if err != nil {
+		return &node.Config{}, err
+	}
+	bootstrap2, err := identity.NewMultiAddressFromString("/ip4/54.179.189.157/tcp/18514/republic/8MHarRJdvWd7SsTJE8vRVfj2jb5cWS")
+	if err != nil {
+		return &node.Config{}, err
+	}
+	bootstrap3, err := identity.NewMultiAddressFromString("/ip4/52.59.192.207/tcp/18514/republic/8MKDGUTgKtkymyKTH28xeMxiCnJ9xy")
+	if err != nil {
+		return &node.Config{}, err
+	}
+	bootstrap4, err := identity.NewMultiAddressFromString("/ip4/54.88.24.57/tcp/18514/republic/8MGg76n7RfC6tuw23PYf85VFyM8Zto")
+	if err != nil {
+		return &node.Config{}, err
+	}
+	bootstrap5, err := identity.NewMultiAddressFromString("/ip4/34.217.114.249/tcp/18514/republic/8MJ38m8Nzknh3gVj7QiMjuejmHBMSf")
+	if err != nil {
+		return &node.Config{}, err
+	}
+
+	return &node.Config{
+		Host:            "0.0.0.0",
+		Port:            "18514",
+		RepublicKeyPair: keypair,
+		MultiAddress:    multiAddress,
+		BootstrapMultiAddresses: identity.MultiAddresses{
+			bootstrap1, bootstrap2, bootstrap3, bootstrap4, bootstrap5,
+		},
+	}, nil
+
 }
