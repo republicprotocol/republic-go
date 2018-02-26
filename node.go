edd89ba3d9bade336f4e1de7e5071dcfc21d0c2c
tok-kkk <sacrifise000@gmail.com>
2018-02-26 11:11:45 +1100

    intialize the test

--- node.go
+++ node.go
@@ -127,7 +127,7 @@ func (node *DarkNode) OnDeltaFragmentReceived(peer identity.MultiAddress, fragme
 // Start mining for compute.Orders that are matched. It establishes connections
 // to other peers in the swarm network by bootstrapping against a set of
 // bootstrap swarm.Nodes.
-func (node *DarkNode) Start() {
+func (node *DarkNode) Start() error {
 	// Start both gRPC servers.
 	go func() {
 		log.Printf("Listening on %s:%s\n", node.Configuration.Host, node.Configuration.Port)
@@ -142,22 +142,22 @@ func (node *DarkNode) Start() {
 		}
 	}()
 
-	// registered, err := isRegistered(node.Configuration.MultiAddress.ID())
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if !registered {
-	// 	panic("dark node hasn't been registered")
-	// }
+	 registered, err := isRegistered(node.Configuration.MultiAddress.ID())
+	 if err != nil {
+	 	log.Fatal(err)
+	 }
+	 if !registered {
+	 	panic("dark node hasn't been registered")
+	 }
 
-	// darkPool, err := getDarkPoolConfig()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	 darkPool, err := getDarkPoolConfig()
+	 if err != nil {
+	 	log.Fatal(err)
+	 }
 
-	// // Wait for the server to start and bootstrap the connections in the swarm.
-	// time.Sleep(time.Second)
-	// node.Swarm.Bootstrap()
+	 // Wait for the server to start and bootstrap the connections in the swarm.
+	 time.Sleep(time.Second)
+	 node.Swarm.Bootstrap()
 
 	// //  Ping all nodes in the dark pool
 	// for _, id := range darkPool {
@@ -421,21 +421,23 @@ func (node *DarkNode) Compute(shard compute.Shard) {
 }
 
 func isRegistered(id identity.ID) (bool, error) {
-	// todo: need to get key from the ethereum private key
-	key := `{"version":3,"id":"7844982f-abe7-4690-8c15-34f75f847c66","address":"db205ea9d35d8c01652263d58351af75cfbcbf07","Crypto":{"ciphertext":"378dce3c1279b36b071e1c7e2540ac1271581bff0bbe36b94f919cb73c491d3a","cipherparams":{"iv":"2eb92da55cc2aa62b7ffddba891f5d35"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"80d3341678f83a14024ba9c3edab072e6bd2eea6aa0fbc9e0a33bae27ffa3d6d","n":8192,"r":8,"p":1},"mac":"3d07502ea6cd6b96a508138d8b8cd2e46c3966240ff276ce288059ba4235cb0d"}}`
-	auth, err := bind.NewTransactor(strings.NewReader(key), "password1")
-	if err != nil {
-		return false, err
-	}
-	client := ethereum.Ropsten("https://ropsten.infura.io/")
-	contractAddress := common.HexToAddress("0x32Dad9E9Fe2A3eA2C2c643675A7d2A56814F554f")
-	userConnection := dnr.NewDarkNodeRegistrar(context.Background(), client, auth, &bind.CallOpts{}, contractAddress, nil)
-	idInBytes := [20]byte{}
-	copy(idInBytes[:], id)
-	return userConnection.IsDarkNodeRegistered(idInBytes)
+	//// todo: need to get key from the ethereum private key
+	//key := `{"version":3,"id":"7844982f-abe7-4690-8c15-34f75f847c66","address":"db205ea9d35d8c01652263d58351af75cfbcbf07","Crypto":{"ciphertext":"378dce3c1279b36b071e1c7e2540ac1271581bff0bbe36b94f919cb73c491d3a","cipherparams":{"iv":"2eb92da55cc2aa62b7ffddba891f5d35"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"80d3341678f83a14024ba9c3edab072e6bd2eea6aa0fbc9e0a33bae27ffa3d6d","n":8192,"r":8,"p":1},"mac":"3d07502ea6cd6b96a508138d8b8cd2e46c3966240ff276ce288059ba4235cb0d"}}`
+	//auth, err := bind.NewTransactor(strings.NewReader(key), "password1")
+	//if err != nil {
+	//	return false, err
+	//}
+	//client := ethereum.Ropsten("https://ropsten.infura.io/")
+	//contractAddress := common.HexToAddress("0x32Dad9E9Fe2A3eA2C2c643675A7d2A56814F554f")
+	//userConnection := dnr.NewDarkNodeRegistrar(context.Background(), client, auth, &bind.CallOpts{}, contractAddress, nil)
+	//idInBytes := [20]byte{}
+	//copy(idInBytes[:], id)
+	//return userConnection.IsDarkNodeRegistered(idInBytes)
+	return true, nil
 }
 
-func getDarkPoolConfig() ([]identity.ID, error) {
+func getDarkPoolConfig() ([]identity.ID, error){
+	return
 	// todo: need to get key from the ethereum private key
 	key := `{"version":3,"id":"7844982f-abe7-4690-8c15-34f75f847c66","address":"db205ea9d35d8c01652263d58351af75cfbcbf07","Crypto":{"ciphertext":"378dce3c1279b36b071e1c7e2540ac1271581bff0bbe36b94f919cb73c491d3a","cipherparams":{"iv":"2eb92da55cc2aa62b7ffddba891f5d35"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"80d3341678f83a14024ba9c3edab072e6bd2eea6aa0fbc9e0a33bae27ffa3d6d","n":8192,"r":8,"p":1},"mac":"3d07502ea6cd6b96a508138d8b8cd2e46c3966240ff276ce288059ba4235cb0d"}}`
 	auth, err := bind.NewTransactor(strings.NewReader(key), "password1")
--- node_test.go
+++ node_test.go
@@ -1,12 +1,132 @@
 package node_test
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
+	"github.com/republicprotocol/go-dark-node"
+	"github.com/republicprotocol/go-do"
+	"github.com/republicprotocol/go-identity"
 )
 
+const Configs_Path = "./test_configs/"
+
 var _ = Describe("Dark nodes", func() {
-	// It("should fail", func() {
-	// 	Ω(true).Should(Equal(false))
-	// })
+
+	start := func(nodes []*node.DarkNode){
+		do.ForAll(nodes, func(i int) {
+			nodes[i].Start()
+		})
+	}
+
+	stop := func(nodes []*node.DarkNode){
+		do.ForAll(nodes, func(i int) {
+			nodes[i].Stop()
+		})
+	}
+
+	Context("startup", func() {
+
+		It("should read from the config file and create a new dark node.", func() {
+			config, err := node.LoadConfig("test_configs/test_config.json")
+			Ω(err).ShouldNot(HaveOccurred())
+			node, err := node.NewDarkNode(config)
+			Ω(err).ShouldNot(HaveOccurred())
+			go func() {
+				defer GinkgoRecover()
+				node.Start()
+			}()
+		})
+
+		It("should register itself on startup", func() {
+
+		})
+	})
+
+	Context("dark pool specification", func() {
+
+		It("should get dark pool assignments from the registrar", func() {
+
+		})
+
+		It("Ping all dark nodes in the relevant dark pool", func() {
+
+		})
+	})
+
+	Context("Order computation", func() {
+		const Boostrap_Nodes_Port = 3000
+		const Test_Nodes_Port = 4000
+
+
+		BeforeEach(func() {
+			err := generateConfigs(10 , Boostrap_Nodes_Port)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			err  = generateConfigs(10 , Test_Nodes_Port)
+			Ω(err).ShouldNot(HaveOccurred())
+		})
+
+		It("should receive order fragment and discover (mis)matches ", func() {
+		})
+	})
 })
+
+// Generate config files in the test_configs folder
+func generateConfigs(numberOfNodes , port int) error {
+	var configs []node.Config
+	for i := 0; i < numberOfNodes; i++ {
+		address, keyPair, err := identity.NewAddress()
+		if err != nil {
+			return err
+		}
+		multi, err := identity.NewMultiAddressFromString(fmt.Sprintf("/ip4/127.0.0.1/tcp/%d/republic/%s", port+i, address.String()))
+		if err != nil {
+			return err
+		}
+
+		config := node.Config{
+			Host:                    "127.0.0.1",
+			Port:                    fmt.Sprintf("%d", port+i),
+			RepublicKeyPair:         keyPair,
+			MultiAddress:            multi,
+			BootstrapMultiAddresses: identity.MultiAddresses{},
+		}
+		configs = append(configs, config)
+	}
+
+	for i := 0; i < numberOfNodes; i++ {
+		data, err := json.Marshal(configs[i])
+		if err != nil {
+			return err
+		}
+		d1 := []byte(data)
+		err = ioutil.WriteFile(fmt.Sprintf("./test_configs/config-%d.json", i), d1, 0644)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Generate nodes from the config files
+func generateNodes(numberOfNodes int )([]*node.DarkNode, error ) {
+	nodes := make ([]*node.DarkNode, numberOfNodes)
+
+	for i:=0; i < numberOfNodes; i++{
+		config, err  := node.LoadConfig(fmt.Sprintf("./test_configs/config-%d.json", i))
+		if err != nil {
+			return nil, err
+		}
+		node,err  := node.NewDarkNode(config)
+		if err != nil {
+			return nil, err
+		}
+		nodes[i] = node
+	}
+	return nodes, nil
+}
+
