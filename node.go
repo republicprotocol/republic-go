81f7663bcaf30d387a0370fc0a501a90e3559841
Loongy <benjamin.jl.wang@gmail.com>
2018-02-28 11:25:20 +1100

    Matches being found successfully

--- node.go
+++ node.go
@@ -76,7 +76,7 @@ func NewDarkNode(config *Config) (*DarkNode, error) {
 	swarmOptions := swarm.Options{
 		MultiAddress:            config.MultiAddress,
 		BootstrapMultiAddresses: config.BootstrapMultiAddresses,
-		Debug:           swarm.DebugOff,
+		Debug:           swarm.DebugHigh,
 		Alpha:           3,
 		MaxBucketLength: 20,
 		Timeout:         30 * time.Second,
@@ -89,7 +89,7 @@ func NewDarkNode(config *Config) (*DarkNode, error) {
 
 	darkOptions := dark.Options{
 		Address:        config.MultiAddress.Address(),
-		Debug:          dark.DebugOff,
+		Debug:          dark.DebugHigh,
 		Timeout:        30 * time.Second,
 		TimeoutStep:    30 * time.Second,
 		TimeoutRetries: 3,
