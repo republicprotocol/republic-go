e6a43d3d855fd4316f340b816da72428d6d0341f
Loongy <benjamin.jl.wang@gmail.com>
2018-03-01 08:58:14 +1100

    Delegate method are non-blocking

--- delegate.go
+++ delegate.go
@@ -39,47 +39,51 @@ func (node *DarkNode) OnSync(from identity.MultiAddress) chan do.Option {
 // by the underlying dark.Node whenever it receives a compute.OrderFragment
 // that it must process.
 func (node *DarkNode) OnOrderFragmentReceived(from identity.MultiAddress, orderFragment *compute.OrderFragment) {
-	deltaFragments, err := node.DeltaFragmentMatrix.InsertOrderFragment(orderFragment)
-	if err != nil {
-		log.Println(err)
-	}
-	for _, deltaFragment := range deltaFragments {
-		node.DeltaBuilder.InsertDeltaFragment(deltaFragment)
-	}
-	for _, multiAddress := range node.DarkPool {
-		client, err := rpc.NewClient(multiAddress, node.Swarm.MultiAddress())
+	go func() {
+		deltaFragments, err := node.DeltaFragmentMatrix.InsertOrderFragment(orderFragment)
 		if err != nil {
 			log.Println(err)
-			continue
 		}
 		for _, deltaFragment := range deltaFragments {
-			_, err := client.BroadcastDeltaFragment(deltaFragment)
+			node.DeltaBuilder.InsertDeltaFragment(deltaFragment)
+		}
+		for _, multiAddress := range node.DarkPool {
+			client, err := rpc.NewClient(multiAddress, node.Swarm.MultiAddress())
 			if err != nil {
 				log.Println(err)
+				continue
+			}
+			for _, deltaFragment := range deltaFragments {
+				_, err := client.BroadcastDeltaFragment(deltaFragment)
+				if err != nil {
+					log.Println(err)
+				}
 			}
 		}
-	}
+	}()
 }
 
 // OnBroadcastDeltaFragment implements the dark.Delegate interface. It is
 // called by the underlying dark.Node whenever it receives a
 // compute.DeltaFragment that it must process.
 func (node *DarkNode) OnBroadcastDeltaFragment(from identity.MultiAddress, deltaFragment *compute.DeltaFragment) {
-	delta, err := node.DeltaBuilder.InsertDeltaFragment(deltaFragment)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	if delta == nil {
-		return
-	}
-	if delta.IsMatch(node.Configuration.Prime) {
-		log.Printf("%v compared (%v, %v) <- MATCH\n", node.Swarm.Address(), base58.Encode(deltaFragment.BuyOrderID), base58.Encode(deltaFragment.SellOrderID))
-		// TODO: Attempt to get consensus on the match and then mark the orders
-		// handled if the consensus is won. If the consensus is not won take
-		// either the buy, or sell (or both), orders and mark them as completed
-		// (this depends on which ones conflicted).
-	} else {
-		log.Printf("%v compared (%v, %v)\n", node.Swarm.Address(), base58.Encode(deltaFragment.BuyOrderID), base58.Encode(deltaFragment.SellOrderID))
-	}
+	go func() {
+		delta, err := node.DeltaBuilder.InsertDeltaFragment(deltaFragment)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if delta == nil {
+			return
+		}
+		if delta.IsMatch(node.Configuration.Prime) {
+			log.Printf("%v compared (%v, %v) <- MATCH\n", node.Swarm.Address(), base58.Encode(deltaFragment.BuyOrderID), base58.Encode(deltaFragment.SellOrderID))
+			// TODO: Attempt to get consensus on the match and then mark the orders
+			// handled if the consensus is won. If the consensus is not won take
+			// either the buy, or sell (or both), orders and mark them as completed
+			// (this depends on which ones conflicted).
+		} else {
+			log.Printf("%v compared (%v, %v)\n", node.Swarm.Address(), base58.Encode(deltaFragment.BuyOrderID), base58.Encode(deltaFragment.SellOrderID))
+		}
+	}()
 }
