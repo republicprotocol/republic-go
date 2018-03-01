4eaec9f9463eba52f41dacfc6524e2e1aef66afa
tok-kkk <sacrifise000@gmail.com>
2018-03-02 10:04:54 +1100

    format config json

--- test/republic.go
+++ test/republic.go
@@ -1,1 +1,37 @@
 package test
+
+import (
+	"flag"
+
+	"github.com/republicprotocol/go-dark-node"
+)
+
+func main() {
+
+	numberOfNodes := flag.Int("nodes", 20, "number of nodes")
+
+	nodes := generateNodes(numberOfNodes)
+
+	err := deployNodes(nodes)
+
+	sendingOrders()
+
+	collectLogs()
+
+}
+
+func generateNodes(numberOfNodes int) []*node.DarkNode {
+	return nil
+}
+
+func deployNodes(nodes []*node.DarkNode) error {
+	return nil
+}
+
+func sendingOrders() {
+
+}
+
+func collectLogs() {
+
+}
--- test/republic_test.go
+++ test/republic_test.go
@@ -3,8 +3,6 @@ package test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "github.com/republicprotocol/go-dark-node"
 )
 
 var _ = Describe("Republic Protocol Test", func() {
