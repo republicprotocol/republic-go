5c1d8d802fab31c55d9ac86314bab4607d26f119
Loongy <benjamin.jl.wang@gmail.com>
2018-03-06 11:14:03 +1100

    Impl for client pool caching and lru

--- network/rpc/client_pool_test.go
+++ network/rpc/client_pool_test.go
@@ -1,16 +1,22 @@
 package rpc_test
 
 import (
+	"time"
+
 	"github.com/republicprotocol/go-do"
 	"github.com/republicprotocol/republic-go/identity"
 )
+
+type ClientCacheEntry struct {
+	*Client
+	Timestamp time.Time
+}
 
 type ClientPool struct {
 	do.GuardedObject
 
 	from       identity.Multiaddress
-	cache      map[string]*Client
-	cacheLRU   []*Client
+	cache      map[string]ClientCacheEntry
 	cacheLimit int
 }
 
@@ -28,12 +34,29 @@ func (pool *ClientPool) FindOrCreateClientConnection(to identity.Multiaddress) (
 }
 
 func (pool *ClientPool) findOrCreateClientConnection(to identity.Multiaddress) (*Client, error) {
-	client, ok := pool.cache[to.String()]
-	if !ok {
-		client, err := NewClient(to, from)
-		if err != nil {
-			return client, err
-		}
+	clientCacheEntry, ok := pool.cache[to.String()]
+	if ok {
+		clientCacheEntry.Timestamp = time.Now()
+		return clientCacheEntry.Client, nil
 	}
+
+	client, err := NewClient(to, from)
+	if err != nil {
+		return client, err
+	}
+
+	if len(pool.cache) >= pool.cacheLimit {
+		var k string
+		var kClientCacheEntry *ClientCacheEntry
+		for multiaddress := range pool.cache {
+			if kClientCacheEntry == nil || kClientCacheEntry.Timestamp.After(pool.cache[multiaddress]) {
+				k = multiaddress
+				kClientCacheEntry = pool.cache[multiaddress]
+			}
+		}
+		delete(pool.cache, k)
+	}
+
+	pool.cache[to.String()] = ClientCacheEntry{Client: client, Timestamp: time.Now()}
 	return client, nil
 }
