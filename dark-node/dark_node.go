6fb4ac719a1bd51e5604ec1b6f04faf64d636da5
Jaz Gulati <jaskaran.g15@gmail.com>
2018-03-09 16:54:39 +1100

    Changed endpoint

--- dark-node/dark_node.go
+++ dark-node/dark_node.go
@@ -196,7 +196,7 @@ func (node *DarkNode) Start() {
 
 func (node *DarkNode) ServeUI()  {
 	fs := http.FileServer(http.Dir("darknode-ui"))
-	http.Handle("/ui", fs)
+	http.Handle("/", fs)
 	node.Info(logger.TagNetwork, "Serving the Dark Node UI")
 	err :=http.ListenAndServe("0.0.0.0:3000", nil)
 	if err != nil {
