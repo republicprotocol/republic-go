81bb8b3e6247f8add0ab3a332e725f8e04e4cca7
Loong <loong@republicprotocol.com>
2018-03-12 13:34:49 +1100

    Finished updating the dark ocean logic

--- dark/dark_ocean.go
+++ dark/dark_ocean.go
@@ -1,71 +1,45 @@
 package dark
 
 import (
-	"errors"
+	"bytes"
+	"fmt"
 	"math/big"
 	"time"
 
 	"github.com/republicprotocol/go-do"
+
 	"github.com/republicprotocol/republic-go/contracts/dnr"
 	"github.com/republicprotocol/republic-go/identity"
+	"github.com/republicprotocol/republic-go/logger"
 )
 
 // Ocean of Pools.
 type Ocean struct {
-	Pools             Pools
-	DarkNodeRegistrar dnr.DarkNodeRegistrar
+	do.GuardedObject
+
+	logger            *logger.Logger
+	pools             Pools
+	darkNodeRegistrar dnr.DarkNodeRegistrar
 }
 
 // NewOcean uses a DarkNodeRegistrar to read all registered nodes and sort them
 // into Pools.
-func NewOcean(darkNodeRegistrar dnr.DarkNodeRegistrar) (*Ocean, error) {
-	allNodes, err := darkNodeRegistrar.GetAllNodes()
-	if err != nil {
-		return &Ocean{}, err
+func NewOcean(logger *logger.Logger, darkNodeRegistrar dnr.DarkNodeRegistrar) (*Ocean, error) {
+	ocean := &Ocean{
+		GuardedObject:     do.NewGuardedObject(),
+		logger:            logger,
+		pools:             Pools{},
+		darkNodeRegistrar: darkNodeRegistrar,
 	}
-
-	blockhash := /* TODO: Get from contract */ big.NewInt(1234567)
-	poolsize := /* TODO: Get from contract? */ 72
-
-	// Find the prime smaller or equal to the number of registered nodes
-	// Start at +2 because it has to greater than the maximum (x+1)
-	previousPrime := big.NewInt(int64(len(allNodes) + 2))
-	// https://golang.org/src/math/big/prime.go
-	// ProbablyPrime is 100% accurate for inputs less than 2^64.
-	for !previousPrime.ProbablyPrime(0) {
-		previousPrime = previousPrime.Sub(previousPrime, big.NewInt(1))
-	}
-
-	inverse := blockhash.ModInverse(blockhash, previousPrime)
-
-	// Integer division
-	numberOfPools := big.NewInt(0).Div(previousPrime, big.NewInt(int64(poolsize)))
-	if numberOfPools.Int64() == 0 {
-		numberOfPools = big.NewInt(1)
-	}
-
-	pools := make(Pools, numberOfPools.Int64())
-	for x := range allNodes {
-		// Add one so that
-		xPlusOne := big.NewInt(int64(x + 1))
-		i := big.NewInt(0).Mod(big.NewInt(0).Mul(xPlusOne, inverse), previousPrime)
-
-		pool := big.NewInt(0).Mod(i, numberOfPools).Int64()
-		pools[pool].Append(Node{
-			ID:           allNodes[x],
-			MultiAddress: nil,
-		})
-	}
-
-	return &Ocean{
-		Pools: pools,
-	}, err
+	return ocean, ocean.update()
 }
 
 // FindPool with the given node ID. Returns the Pool, or nil if no Pool can be
 // found.
 func (ocean *Ocean) FindPool(id identity.ID) *Pool {
-	for _, pool := range ocean.Pools {
+	ocean.EnterReadOnly(nil)
+	defer ocean.ExitReadOnly()
+	for _, pool := range ocean.pools {
 		if pool.Has(id) != nil {
 			return pool
 		}
@@ -73,32 +47,83 @@ func (ocean *Ocean) FindPool(id identity.ID) *Pool {
 	return nil
 }
 
-// WatchForDarkOceanChanges returns a channel through which it will send an update every epoch
-// Will check if a new epoch has been triggered and then sleep for 5 minutes
-// Blocking function
-func WatchForDarkOceanChanges(registrar dnr.DarkNodeRegistrarInterface, channel chan do.Option) {
+func (ocean *Ocean) Update() error {
+	ocean.Enter(nil)
+	defer ocean.Exit()
+	return ocean.update()
+}
 
-	// This function runs until the channel is closed
+func (ocean *Ocean) update() error {
+	// TODO: Get these details from the smart contract.
+	blockhash := big.NewInt(1234567)
+	poolsize := 72
+
+	nodes, err := ocean.darkNodeRegistrar.GetAllNodes()
+	if err != nil {
+		return err
+	}
+
+	// Find the prime smaller or equal to the number of registered nodes
+	// Start at +2 because it has to greater than the maximum (x+1)
+	previousPrime := big.NewInt(int64(len(nodes) + 2))
+
+	// ProbablyPrime is 100% accurate for inputs less than 2^64.
+	// https://golang.org/src/math/big/prime.go
+	for !previousPrime.ProbablyPrime(0) {
+		previousPrime = previousPrime.Sub(previousPrime, big.NewInt(1))
+	}
+
+	// Integer division
+	numberOfPools := big.NewInt(0).Div(previousPrime, big.NewInt(int64(poolsize)))
+	if numberOfPools.Int64() == 0 {
+		numberOfPools = big.NewInt(1)
+	}
+	pools := make(Pools, numberOfPools.Int64())
+
+	// Calcualte the pool assignment for each node
+	inverse := blockhash.ModInverse(blockhash, previousPrime)
+	for x := range nodes {
+		xPlusOne := big.NewInt(int64(x + 1))
+		i := big.NewInt(0).Mod(big.NewInt(0).Mul(xPlusOne, inverse), previousPrime)
+
+		pool := big.NewInt(0).Mod(i, numberOfPools).Int64()
+		pools[pool].Append(Node{
+			ID:           nodes[x],
+			MultiAddress: nil,
+		})
+	}
+
+	ocean.pools = pools
+	return nil
+}
+
+// Watch for changes to the Ocean. This function is a blocking function that
+// sleeps and wakes once per period to check for a change in epoch. It accepts
+// a channel that is pinged whenever the Ocean changes.
+func (ocean *Ocean) Watch(period time.Duration, changes chan struct{}) {
+	// Recover from writing to a closed channel
 	defer func() { recover() }()
 
 	var currentBlockhash [32]byte
-
-	// TODO loop until an epoch, call calculateDarkOcean()
 	for {
-		epoch, err := registrar.CurrentEpoch()
-		if err != nil {
-			channel <- do.Err(errors.New("Couldn't retrieve current epoch"))
-		}
+		func() {
+			ocean.Enter(nil)
+			defer ocean.Exit()
 
-		if epoch.Blockhash != currentBlockhash {
-			currentBlockhash = epoch.Blockhash
-			darkOceanOverlay, err := GetDarkPools(registrar)
+			epoch, err := ocean.darkNodeRegistrar.CurrentEpoch()
 			if err != nil {
-				channel <- do.Err(errors.New("Couldn't retrieve dark ocean overlay"))
-			} else {
-				channel <- do.Ok(darkOceanOverlay)
+				ocean.logger.Error(logger.TagEthereum, fmt.Sprintf("cannot update epoch: %s", err.Error()))
+				return
 			}
-		}
-		time.Sleep(5 * 60 * time.Second)
+			if !bytes.Equal(currentBlockhash[:], epoch.Blockhash[:]) {
+				currentBlockhash = epoch.Blockhash
+				if err := ocean.update(); err != nil {
+					ocean.logger.Error(logger.TagEthereum, fmt.Sprintf("cannot update dark ocean: %s", err.Error()))
+					return
+				}
+				changes <- struct{}{}
+			}
+		}()
+		time.Sleep(period)
 	}
 }
