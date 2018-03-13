bc1a7519a2035c157c847074af1966acb7c4b86e
Noah <noahingham@protonmail.com>
2018-03-13 19:26:47 +1100

    Removed int1024_internal file

--- stackint/int1024_bitwise.go
+++ stackint/int1024_bitwise.go
@@ -9,6 +9,23 @@ func (x *Int1024) ShiftLeft(n int) Int1024 {
 	return z
 }
 
+// shiftLeftByOne shifts to the left x by one
+func (x *Int1024) overwritingShiftLeftByOne() {
+	overflow := Word(0)
+	for i := INT1024WORDS - 1; i >= 0; i-- {
+		// Calculate if word overflows into next word
+		newOverflow := (x.words[i] >> (WORDSIZE - 1)) & 1
+		// Shift word to the right
+		// If previous word overflowed, add 1
+		x.words[i] = (x.words[i] << 1) | overflow
+		overflow = newOverflow
+	}
+
+	if overflow == 1 {
+		// WARNING: Overflow occured (not important for Shift)
+	}
+}
+
 // ShiftRight returns x>>n
 func (x *Int1024) ShiftRight(n int) Int1024 {
 	z := x.Clone()
@@ -16,6 +33,23 @@ func (x *Int1024) ShiftRight(n int) Int1024 {
 		z.overwritingShiftRightByOne()
 	}
 	return z
+}
+
+// shiftRightByOne shifts to the right x by one
+func (x *Int1024) overwritingShiftRightByOne() {
+	overflow := Word(0)
+	for i := 0; i < INT1024WORDS; i++ {
+		// Calculate if word overflows into next word
+		newOverflow := (x.words[i] & 1) << (WORDSIZE - 1)
+		// Shift word to the right
+		// If previous word overflowed, add 1
+		x.words[i] = (x.words[i] >> 1) | overflow
+		overflow = newOverflow
+	}
+
+	if overflow == 1 {
+		// WARNING: Overflow occured (not important for Shift)
+	}
 }
 
 // AND returns x&y
