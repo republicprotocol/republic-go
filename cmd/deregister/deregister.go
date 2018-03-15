d95f59f91aea13a780b75a9d89812ec3b7f47aa1
susruth <susruth@republicprotocol.com>
2018-03-15 14:20:10 +1100

    fised deregister cmd script

--- cmd/deregister/deregister.go
+++ cmd/deregister/deregister.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 
 	base58 "github.com/jbenet/go-base58"
 	"github.com/republicprotocol/go-do"
-
-	"github.com/republicprotocol/go-dark-node"
+	"github.com/republicprotocol/republic-go/contracts/connection"
+	"github.com/republicprotocol/republic-go/contracts/dnr"
+	node "github.com/republicprotocol/republic-go/dark-node"
 )
-
-var config *node.Config
 
 func main() {
 
@@ -40,13 +43,19 @@ func DeregisterAll(configs []*node.Config) {
 
 		keypair := configs[i].RepublicKeyPair
 
-		ethereumKeyPair, err := configs[i].EthereumKeyPair()
+		clientDetails, err := connection.FromURI("https://ropsten.infura.io/", "ropsten")
 		if err != nil {
-			log.Printf("[%v] %sCouldn't load Ethereum key pair%s: %v\n", base58.Encode(keypair.ID()), red, reset, err)
-			return
+			// TODO: Handler err
+			panic(err)
 		}
 
-		registrar, err := node.ConnectToRegistrar(ethereumKeyPair)
+		key := `{"version":3,"id":"7844982f-abe7-4690-8c15-34f75f847c66","address":"db205ea9d35d8c01652263d58351af75cfbcbf07","Crypto":{"ciphertext":"378dce3c1279b36b071e1c7e2540ac1271581bff0bbe36b94f919cb73c491d3a","cipherparams":{"iv":"2eb92da55cc2aa62b7ffddba891f5d35"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"80d3341678f83a14024ba9c3edab072e6bd2eea6aa0fbc9e0a33bae27ffa3d6d","n":8192,"r":8,"p":1},"mac":"3d07502ea6cd6b96a508138d8b8cd2e46c3966240ff276ce288059ba4235cb0d"}}`
+		auth, err := bind.NewTransactor(strings.NewReader(key), "password1")
+		if err != nil {
+			panic(err)
+		}
+
+		registrar, err := dnr.NewEthereumDarkNodeRegistrar(context.Background(), &clientDetails, auth, &bind.CallOpts{})
 		if err != nil {
 			log.Printf("[%v] %sCouldn't connect to registrar%s: %v\n", base58.Encode(keypair.ID()), red, reset, err)
 			return
