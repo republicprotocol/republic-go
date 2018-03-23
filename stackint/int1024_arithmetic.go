197995d3d5031d5ba0db768824fb889b19b3e741
Noah <noahingham@protonmail.com>
2018-03-23 13:07:18 +1100

    Reduced precision back to 1024 bits

--- stackint/int1024_arithmetic.go
+++ stackint/int1024_arithmetic.go
@@ -12,63 +12,38 @@ func (x *Int1024) Add(y *Int1024) Int1024 {
 // Inc sets x to x+y (used for multiplication)
 func (x *Int1024) Inc(y *Int1024) {
 
-	// expected := big.NewInt(0).Add(x.ToBigInt(), y.ToBigInt())
+	if y.IsZero() {
+		return
+	} else if x.IsZero() {
+		var i uint16
+		for i = 0; i < y.length; i++ {
+			x.words[i] = y.words[i]
+		}
+		x.length = y.length
+		return
+	}
 
-	// a, b := x, y
-
-	var a, b *Int1024
-	// var aLen, bLen int
-	if x.length > y.length {
-		a, b = x, y
-		// aLen, bLen = xLen, yLen
-	} else {
+	a, b := x, y
+	if x.length < y.length {
 		a, b = y, x
-		// aLen, bLen = yLen, xLen
 	}
 
-	var firstPositive uint16
-	var overflow uint64
-	overflow = 0
-	var i uint16
-	for i = 0; i < b.length; i++ {
-		previousOverflow := overflow
-		if a.words[i] > WORDMAX-b.words[i] || a.words[i] > WORDMAX-b.words[i]-previousOverflow || b.words[i] > WORDMAX-previousOverflow {
-			overflow = 1
+	m := a.length
+	n := b.length
+
+	c := addVV_g(x.words[0:n], a.words[:], b.words[:])
+	if m > n {
+		c = addVW_g(x.words[n:m], a.words[n:], c)
+	}
+	x.length = m
+	if c > 0 {
+		if m < INT1024WORDS {
+			x.words[x.length] = c
+			x.length++
 		} else {
-			overflow = 0
-		}
-		x.words[i] = a.words[i] + b.words[i] + previousOverflow
-		if x.words[i] != 0 {
-			firstPositive = i
+			x.setLength()
 		}
 	}
-	for i = b.length; i < a.length; i++ {
-		previousOverflow := overflow
-		if a.words[i] > WORDMAX || a.words[i] > WORDMAX-previousOverflow {
-			overflow = 1
-		} else {
-			overflow = 0
-		}
-		x.words[i] = a.words[i] + previousOverflow
-		if x.words[i] != 0 {
-			firstPositive = i
-		}
-	}
-
-	x.length = a.length
-
-	if overflow == 1 && x.length < INT1024WORDS {
-		x.length++
-		x.words[x.length-1] = 1
-		firstPositive = x.length - 1
-		// WARNING: Overflow occured
-	}
-
-	x.length = firstPositive + 1
-
-	// if expected.Cmp(x.ToBigInt()) != 0 && expected.BitLen() <= SIZE {
-	// 	panic("Addition failed")
-	// }
 }
 
 // Sub returns x-y
@@ -149,7 +124,7 @@ func (x *Int1024) Dec(y *Int1024) {
 	// }
 }
 
-func (x *Int1024) BasicMulBig(y *Int1024) Int1024 {
+func (x *Int1024) BasicMulBig(y *Int1024) [INT1024WORDS * 2]uint64 {
 
 	var words [INT1024WORDS * 2]uint64
 	var i uint16
@@ -171,17 +146,8 @@ func (x *Int1024) BasicMulBig(y *Int1024) Int1024 {
 			words[l+i] = c
 		}
 	}
-	var words2 [INT1024WORDS]uint64
-	var highest uint16
-	for i = 0; i < INT1024WORDS; i++ {
-		words2[i] = words[i]
-		if words2[i] > 0 {
-			highest = i
-		}
-	}
-	return Int1024{
-		words2, highest + 1,
-	}
+
+	return words
 }
 
 // BasicMul returns x*y using the shift and add method
@@ -229,7 +195,17 @@ func (x *Int1024) Mul(y *Int1024) Int1024 {
 	if x.length+y.length <= INT1024WORDS {
 		return x.BasicMul(y)
 	}
-	return x.BasicMulBig(y)
+	words := x.BasicMulBig(y)
+	var words2 [INT1024WORDS]uint64
+	var highest uint16
+	var i uint16
+	for i = 0; i < INT1024WORDS; i++ {
+		words2[i] = words[i]
+		if words2[i] > 0 {
+			highest = i
+		}
+	}
+	return Int1024{words2, highest + 1}
 }
 
 // Div returns the quotient of x/y. If y is 0, a run-time panic occurs.
@@ -316,49 +292,50 @@ func (x *Int1024) AddModulo(y, n *Int1024) Int1024 {
 	return X.Add(&Y)
 }
 
-// // MulModuloSlow returns (x*y) % n
+// MulModulo returns (x*y) % n
 func (x *Int1024) MulModulo(y, n *Int1024) Int1024 {
+	if x.length+y.length > INT1024WORDS {
+		return x.MulModuloBig(y, n)
+	}
 	z := x.Mul(y)
 	return z.Mod(n)
-	// return x.peasant(y, n)
 }
 
 // ModInverse sets z to the multiplicative inverse of g in the ring ℤ/nℤ
 // and returns z. If g and n are not relatively prime, the result is undefined.
 // Code adapted from https://www.di-mgt.com.au/euclidean.html
 func (x *Int1024) ModInverse(n *Int1024) Int1024 {
-	v := n.Clone()
-	// unsigned int inv, u1, u3, v1, v3, t1, t3, q;
-	// int iter;
 	/* Step X1. Initialise */
-	u1 := FromUint64(1)
-	u3 := *x
-	v1 := Zero()
-	v3 := v
+	lastX := FromUint64(1)
+	A := *x
+	X := Zero()
+	N := n.Clone()
+	B := N
 	/* Remember odd/even iterations */
 	iter := 1
-	/* Step X2. Loop while v3 != 0 */
-	for !v3.IsZero() {
+	/* Step X2. Loop while B != 0 */
+
+	for !B.IsZero() {
 		/* Step X3. Divide and "Subtract" */
-		q, t3 := u3.DivMod(&v3)
-		tmp := q.Mul(&v1)
-		u1.Inc(&tmp)
+		q, r := A.DivMod(&B)
+		tmp := q.Mul(&X)
+		lastX.Inc(&tmp)
 		/* Swap */
-		u1, v1, u3, v3 = v1, u1, v3, t3
-		iter = -iter
+		lastX, X, A, B = X, lastX, B, r
+		iter ^= 1
 	}
 
-	/* Make sure u3 = gcd(u,v) == 1 */
-	if !u3.EqualsUint64(1) {
+	/* Make sure A = gcd(u,v) == 1 */
+	if !A.EqualsUint64(1) {
 		// return zero() /* Error: No inverse exists */
 		panic("not relatively prime")
 	}
 	/* Ensure a positive result */
 
-	inv := u1
-	if iter < 0 {
-		v.Dec(&inv)
-		return v
+	inv := lastX
+	if iter == 0 {
+		N.Dec(&inv)
+		return N
 		// inv = v.Sub(&inv)
 	}
 	return inv
