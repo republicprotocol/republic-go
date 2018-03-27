54b38ddbc1ad15e92f14c352a2d96aa744ac83fd
tok-kkk <yunshi@republicprotocol.com>
2018-03-28 10:08:23 +1100

    comment out the implementation instead of the whole file

--- cmd/deploy-contracts/deploy_contracts.go
+++ cmd/deploy-contracts/deploy_contracts.go
@@ -2,14 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
-	"math/big"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/jbenet/go-base58"
-	"github.com/republicprotocol/republic-go/contracts/connection"
 	"github.com/republicprotocol/republic-go/dark-node"
 )
 
@@ -19,30 +12,30 @@ const reset = "\x1b[0m"
 const green = "\x1b[32;1m"
 
 func main() {
-	err := parseCommandLineFlags()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	ethereumKeyPair := config.EthereumKey
-	auth := bind.NewKeyedTransactor(ethereumKeyPair.PrivateKey)
-
-	client, err := connection.FromURI("https://ropsten.infura.io/", connection.ChainRopsten)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// REPLACE REN ADDRESS HERE
-	renContract := common.HexToAddress("0x889debfe1478971bcff387f652559ae1e0b6d34a")
-	address, _ , _, err := bind.DeployContract(auth, client, renContract, big.NewInt(1000), big.NewInt(60))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	//_, err = dnr.PatchedWaitDeployed(context.Background(), client, tx)
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	fmt.Printf("[%v] Contract deployed at %s%v%s\n", base58.Encode(config.KeyPair.ID()), green, address.Hex(), reset)
+	//	err := parseCommandLineFlags()
+	//	if err != nil {
+	//		log.Fatalln(err)
+	//	}
+	//
+	//	ethereumKeyPair := config.EthereumKey
+	//	auth := bind.NewKeyedTransactor(ethereumKeyPair.PrivateKey)
+	//
+	//	client, err := connection.FromURI("https://ropsten.infura.io/", connection.ChainRopsten)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//
+	//	// REPLACE REN ADDRESS HERE
+	//	renContract := common.HexToAddress("0x889debfe1478971bcff387f652559ae1e0b6d34a")
+	//	address, _ , _, err := bind.DeployContract(auth, client, renContract, big.NewInt(1000), big.NewInt(60))
+	//	if err != nil {
+	//		log.Fatalln(err)
+	//	}
+	//	//_, err = dnr.PatchedWaitDeployed(context.Background(), client, tx)
+	//	//if err != nil {
+	//	//	log.Fatalln(err)
+	//	//}
+	//	fmt.Printf("[%v] Contract deployed at %s%v%s\n", base58.Encode(config.KeyPair.ID()), green, address.Hex(), reset)
 }
 
 func parseCommandLineFlags() error {
