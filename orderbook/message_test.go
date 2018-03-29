ea846065adabc4da7f1e430c5a07054f0ed6ce1b
tok-kkk <yunshi@republicprotocol.com>
2018-03-29 12:49:29 +1100

    add stub tests for orderbook.message

---
+++ orderbook/message_test.go
@@ -0,0 +1,12 @@
+package orderbook_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("message", func() {
+	Context("to be done", func() {
+		Ω(true).Should(BeTrue())
+	})
+})
