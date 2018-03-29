c4c61d9a4a561a0b398f8bbd4b7859f151391f4e
Jaz Gulati <jaskaran.g15@gmail.com>
2018-03-29 12:26:13 +1100

    Updated URL handling

--- dark-node/dark_node.go
+++ dark-node/dark_node.go
@@ -155,7 +155,6 @@ func (node *DarkNode) StartServices() {
 }
 
 func (node *DarkNode) StartUI() {
-
 	host, err := node.NetworkOptions.MultiAddress.ValueForProtocol(identity.IP4Code)
 	if err != nil {
 		node.Logger.Network(logger.Error, "UI host unknown: "+err.Error())
@@ -191,7 +190,7 @@ func (node *DarkNode) StartUI() {
 	})))
 
 	path := node.Config.Path
-	http.Handle("/", http.FileServer(http.Dir(path+"/ui")))
+	http.Handle("*", http.FileServer(http.Dir(path+"/ui")))
 	if err := http.ListenAndServe("0.0.0.0:3000", nil); err != nil {
 		node.Logger.Error(err.Error())
 	}
