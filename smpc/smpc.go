766eff230d012444bc0bfd01f83f7cbdae9f8880
Loong <loong@republicprotocol.com>
2018-03-31 22:34:28 +1100

    Replaced workers with pipeline-like funcs

--- smpc/smpc.go
+++ smpc/smpc.go
@@ -1,1 +1,13 @@
 package smpc
+
+type ComputeEngine struct {
+	workers             Workers
+	broadcasters        Broadcasters
+	deltaFragmentMatrix DeltaFragmentMatrix
+	deltaBuilder        DeltaBuilder
+	deltaOutput         chan Delta
+}
+
+func NewComputeEngine() ComputeEngine {
+	return ComputeEngine{}
+}
