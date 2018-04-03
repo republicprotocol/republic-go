b277b78b0c92860d385752d026b365f8f0304d58
susruth <susruth@republicprotocol.com>
2018-04-03 16:53:47 +1000

    Add go files for a block in hyperdrive

---
+++ hyperdrive/block.go
@@ -0,0 +1,46 @@
+package hyper
+
+import (
+	"context"
+)
+
+type Block struct {
+	tuples    Tuples
+	signature Signature
+}
+
+func ConsumeBlock(ctx context.Context, blockChIn chan Block, sharedBlocks *SharedBlocks) chan error {
+	errCh := make(chan error)
+	go func() {
+		defer close(errCh)
+
+		for {
+			select {
+			case <-ctx.Done():
+				errCh <- ctx.Err()
+				return
+			case block, ok := <-blockChIn:
+				if !ok {
+					return
+				}
+				sharedBlocks.AddBlock(block)
+			}
+		}
+	}()
+
+	return errCh
+}
+
+func validateBlock(b Block, sb SharedBlocks) bool {
+	for _, tuple := range b.tuples {
+		if !sb.ValidateTuple(tuple) {
+			return false
+		}
+	}
+	// TODO: Replace the validate signature with actual logic
+	return validateSignature()
+}
+
+func validateSignature() bool {
+	return true
+}
