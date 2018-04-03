57290e50c22cedcf951a39cf646f6d74f424ae50
susruth <susruth@republicprotocol.com>
2018-04-03 17:45:33 +1000

    Add go code for prepare in hyperdrive

---
+++ hyperdrive/prepare.go
@@ -0,0 +1,60 @@
+package hyper
+
+import "context"
+
+const THRESHOLD uint8 = 72
+
+type Prepare struct {
+	Signature
+	Block
+	Rank
+	Height
+}
+
+type Prepares map[BlockHash]uint8
+
+func ProcessPreparation(ctx context.Context, prepareChIn chan Prepare, sharedBlocks SharedBlocks) (chan Prepare, chan Commit, chan Fault, chan error) {
+	prepareCh := make(chan Prepare)
+	commitCh := make(chan Commit)
+	faultCh := make(chan Fault)
+	errCh := make(chan error)
+	prepares := make(Prepares)
+
+	go func() {
+		defer close(prepareCh)
+		defer close(commitCh)
+		defer close(faultCh)
+		defer close(errCh)
+
+		for {
+			select {
+			case <-ctx.Done():
+				errCh <- ctx.Err()
+				return
+			case prepare := <-prepareChIn:
+				b := getBlockHash(prepare.Block)
+				if prepares[b] >= THRESHOLD {
+					commitCh <- Commit{}
+				} else {
+					if validatePrepare(prepare, sharedBlocks) {
+						prepareCh <- Prepare{}
+						prepares[b]++
+					} else {
+						faultCh <- Fault{}
+					}
+				}
+			}
+		}
+	}()
+
+	return prepareCh, commitCh, faultCh, errCh
+}
+
+func validatePrepare(prepare Prepare, sharedBlocks SharedBlocks) bool {
+	valid := validateBlock(prepare.Block, sharedBlocks)
+	return valid
+}
+
+func signPrepare(prepare Prepare) Signature {
+	return Signature{}
+}
