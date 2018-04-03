4cf35f739596db320b197c8c677dc50545638682
susruth <susruth@republicprotocol.com>
2018-04-03 17:05:40 +1000

    Updated proposal go file for hyperdrive

--- hyperdrive/proposal.go
+++ hyperdrive/proposal.go
@@ -2,10 +2,18 @@ package hyper
 
 import "context"
 
-type Proposal struct {
+type Signer interface {
+	Sign([]byte) (Signature, error)
 }
 
-func ProcessProposal(ctx context.Context, proposalChIn chan Proposal) (chan Prepare, chan Fault, chan error) {
+type Proposal struct {
+	Signature
+	Block
+	Rank
+	Height
+}
+
+func ProcessProposal(ctx context.Context, proposalChIn chan Proposal, signer Signer, sharedBlocks SharedBlocks) (chan Prepare, chan Fault, chan error) {
 	prepareCh := make(chan Prepare)
 	faultCh := make(chan Fault)
 	errCh := make(chan error)
@@ -20,11 +28,35 @@ func ProcessProposal(ctx context.Context, proposalChIn chan Proposal) (chan Prep
 			case <-ctx.Done():
 				errCh <- ctx.Err()
 				return
-			case _ = <-proposalChIn:
-				// TODO: process the proposal
+			case proposal, ok := <-proposalChIn:
+				if !ok {
+					return
+				}
+				valid := validateProposal(proposal, sharedBlocks)
+				if valid {
+					prepareCh <- Prepare{
+						proposal.Signature,
+						proposal.Block,
+						proposal.Rank,
+						proposal.Height,
+					}
+				} else {
+					faultCh <- Fault{
+						proposal.Rank,
+						proposal.Height,
+					}
+				}
 			}
 		}
 	}()
 
 	return prepareCh, faultCh, errCh
+}
+
+func validateProposal(p Proposal, sb SharedBlocks) bool {
+	valid := validateBlock(p.Block, sb)
+	// TODO: check whether the signer inside block, and signer in the proposal are same
+	// TODO: validate rank, make sure that the current rank and the rank of the proposer is same
+	// TODO: validate height, make sure that the height is correct
+	return valid
 }
