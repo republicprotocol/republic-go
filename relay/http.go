1a7368fb8ba28c73c1e75307c95013ae5cb7ff30
Divya <u6479594@anu.edu.au>
2018-04-09 15:08:29 +1000

    Added stubs for POST, GET and DELETE in http.go

--- relay/http.go
+++ relay/http.go
@@ -1,1 +1,36 @@
 package relay
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/republicprotocol/republic-go/order"
+)
+
+// handleHTTPRequests will handle POST, DELETE and GET requests
+func handleHTTPRequests(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+		// TODO: Handle GET requests here
+	case "POST":
+		ord := order.Order{}
+		err := json.NewDecoder(r.Body).Decode(&ord)
+		if err != nil {
+			fmt.Fprintf("cannot decode json: %v", err)
+			return
+		}
+
+		SendOrderToDarkOcean(ord)
+	case "DELETE":
+		// Handle cancel requests here
+
+	default:
+		fmt.Fprintf(w, "Only GET, POST and DELETE methods are supported.")
+	}
+}
--- relay/relay.go
+++ relay/relay.go
@@ -2,3 +2,9 @@ package relay
 
 type Relay struct {
 }
+
+func SendOrderToDarkOcean (order order.Order) error {
+
+	fmt.Println(string(order.Type))
+
+}
