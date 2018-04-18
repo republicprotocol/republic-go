91ba39f0723b32b06e491f45b767162d900b33f5
susruth <susruth@republicprotocol.com>
2018-04-18 11:57:20 +1000

    Add Basic functions to rsa crypto package

--- crypto/rsa.go
+++ crypto/rsa.go
@@ -1,27 +1,45 @@
 package crypto
 
-import "crypto/rsa"
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/json"
+)
 
 type RsaKeyPair struct {
 	*rsa.PublicKey
 	*rsa.PrivateKey
 }
 
-func NewRsaKeyPair() RsaKeyPair {
-	privateKey, err = rsa.GenerateKey()
-
-}
-
-func KeyPairFromPublicKey(pubKey *rsa.PublicKey) RsaKeyPair {
-	return RsaKeyPair{
-		PublicKey: pubKey,
+func NewRsaKeyPair() (RsaKeyPair, error) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return RsaKeyPair{}, err
 	}
+	publicKey := privateKey.PublicKey
+	return RsaKeyPair{
+		PublicKey:  &publicKey,
+		PrivateKey: privateKey,
+	}, nil
 }
 
-func (kp *RsaKeyPair) Encrypt(msg []byte) ([]byte, error) {
-
+func Encrypt(pubKey *rsa.PublicKey, msg []byte) ([]byte, error) {
+	return rsa.EncryptPKCS1v15(rand.Reader, pubKey, msg)
 }
 
-func (kp *RsaKeyPair) Decrypt(cipherText []byte) ([]byte, error) {
+func Decrypt(privKey *rsa.PrivateKey, cipherText []byte) ([]byte, error) {
+	return rsa.DecryptPKCS1v15(rand.Reader, privKey, cipherText)
+}
 
+func PublicKeyToBytes(publicKey *rsa.PublicKey) ([]byte, error) {
+	return json.Marshal(publicKey)
+}
+
+func BytesToPublicKey(publicKey []byte) (*rsa.PublicKey, error) {
+	var pubKey rsa.PublicKey
+	err := json.Unmarshal(publicKey, &pubKey)
+	if err != nil {
+		return &rsa.PublicKey{}, err
+	}
+	return &pubKey, nil
 }
