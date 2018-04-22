f23dafbfca45c55d50a72be424329ff0766cb3ce
Loong <loong@republicprotocol.com>
2018-04-22 20:26:24 +1000

    Stubs for the different RPC subpackages

---
+++ rpc/smpc/smpc_suite_test.go
@@ -0,0 +1,13 @@
+package smpc_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestRpc(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "SMPC Suite")
+}
