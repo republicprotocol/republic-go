ad6c6b0aab8d052cbf6e9ff81d0321338750236f
tok-kkk <yunshi@republicprotocol.com>
2018-04-23 11:04:25 +1000

    generate bindings for hyperdrive contract

--- ethereum/bindings/generate.sh
+++ ethereum/bindings/generate.sh
@@ -2,4 +2,7 @@
 #abigen --sol ./republic-sol/contracts/DarkNodeRegistry.sol -pkg bindings --out dnr.go
 
 # Hyperdrive registry
-abigen --sol ./republic-sol/contracts/HyperdriveEpoch.sol -pkg bindings --out hde.go
+#abigen --sol ./republic-sol/contracts/HyperdriveEpoch.sol -pkg bindings --out hde.go
+
+# Hyperdrive contract
+abigen --sol ./republic-sol/contracts/Hyperdrive.sol -pkg bindings --out hyperdrive.go
