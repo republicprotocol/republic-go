9e0d477eb76d082fc98c2c9c2b5fbb4da16733dd
Jaz Gulati <jaskaran.g15@gmail.com>
2018-05-03 12:12:38 +1000

    Checking response status codes and added documentation

---
+++ docs/relay-sdk.md
@@ -0,0 +1,80 @@
+# Relay SDK
+
+A software development kit (SDK) for providing an abstraction over the Relay RESTful API. This includes creating orders, canceling orders, getting an order, and getting updates on the status of an order.
+
+## Using the SDK
+
+### Import the Relay package
+
+```go
+import github.com/republicprotocol/republic-go/relay
+```
+
+### Construct a new Relay API object
+
+```go
+api := relay.NewAPI(url, token)
+```
+
+`url` – The address your Relay is serving from.
+`token` – The authentication token used to verify your Relay.
+> Note: If your Relay does not require an authentication token use `relay.Insecure()`.
+
+### Interact with the Relay API
+
+**Opening orders**
+
+```go
+orderID, err := api.OpenOrder(ty, parity, expiry, fstCode, sndCode, price, maxVolume, minVolume)
+if err != nil {
+    // Handle error
+}
+```
+
+> Note: For information regarding the parameters, please refer to [...]
+
+**Closing orders**
+
+```go
+orderID := "vrZhWU3VV9LRIriRvuzT9CbVc57wQhbQyV6ryi1wDSM"
+err := api.CloseOrder(orderID)
+if err != nil {
+    // Handle error
+}
+```
+
+**Getting an order**
+
+```go
+orderID := "vrZhWU3VV9LRIriRvuzT9CbVc57wQhbQyV6ryi1wDSM"
+order, err := api.GetOrder(orderID)
+if err != nil {
+    // Handle error
+}
+```
+
+**Getting updates on an order**
+
+```go
+filter := relay.Filter{
+    ID: "vrZhWU3VV9LRIriRvuzT9CbVc57wQhbQyV6ryi1wDSM",
+    Status: "confirmed, settled"
+}
+updates, errs := GetOrderbookUpdates(filter)
+go func() {
+    for {
+        select {
+            case entry, ok := <-updates:
+                if !ok {
+                    return
+                }
+                status := entry.Status
+                // ...
+    		case <-errs:
+    			// Handle errors
+    	}
+    }
+}()
+```
+
+> Note: If you wish to receive updates regarding all statuses, use `relay.StatusAll()`.
--- relay/api.go
+++ relay/api.go
@@ -7,7 +7,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
+
+	"github.com/republicprotocol/republic-go/orderbook"
 
 	"github.com/gorilla/websocket"
 	"github.com/republicprotocol/republic-go/order"
@@ -22,6 +25,7 @@ type API struct {
 
 // Filter objects are constructed to filter updates when retrieving data
 // regarding an order using the GetOrders function.
+// TODO: Check if this is necessary
 type Filter struct {
 	ID     string
 	Status string
@@ -35,11 +39,12 @@ func NewAPI(url string, token string) API {
 	}
 }
 
-// OpenOrder opens a new order using the HTTP API.
+// OpenOrder opens a new order using the Relay API.
+// TODO: Simplify parameter types?
+// TODO: Accept int64 instead of uint
 func (api *API) OpenOrder(ty order.Type, parity order.Parity, expiry time.Time, fstCode, sndCode order.CurrencyCode, price, maxVolume, minVolume uint) (order.ID, error) {
 	// Construct order request using parameters
-	nonce := stackint.FromUint(0) // TODO: Set nonce
-	// TODO: Accept int64 instead of uint?
+	nonce := stackint.Zero() // TODO: Set nonce
 	ord := order.NewOrder(ty, parity, expiry, fstCode, sndCode, stackint.FromUint(price), stackint.FromUint(maxVolume), stackint.FromUint(minVolume), nonce)
 	orderRequest := OpenOrderRequest{
 		Order:          *ord,
@@ -58,76 +63,64 @@ func (api *API) OpenOrder(ty order.Type, parity order.Parity, expiry time.Time, 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("cannot complete request: %s", err)
+		return nil, fmt.Errorf("cannot send request: %s", err)
 	}
 	defer resp.Body.Close()
 
-	// Read response body
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("cannot read response body: %s", err)
+	// Verify response
+	if resp.StatusCode != http.StatusCreated {
+		return nil, fmt.Errorf(fmt.Sprintf("invalid response status code: %d", resp.StatusCode))
 	}
-
-	// TODO: Handle response
-	fmt.Println("Status: ", resp.Status)
-	fmt.Println("Headers: ", resp.Header)
-	fmt.Println("Body: ", string(body))
 
 	return ord.ID, nil
 }
 
-// CancelOrder cancels an existing order using the HTTP API.
-func (api *API) CancelOrder(orderID order.ID) error {
+// CancelOrder cancels an existing order using the Relay API.
+func (api *API) CancelOrder(orderID string) error {
 	// Create a request and set the authorization header
-	req, err := http.NewRequest("DELETE", api.URL+"/orders/"+orderID.String(), nil)
+	req, err := http.NewRequest("DELETE", api.URL+"/orders/"+orderID, nil)
 	req.Header.Set("Authorization", "Bearer "+api.Token)
 
 	// Create a client and fetch request
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("cannot complete request: %s", err)
+		return fmt.Errorf("cannot send request: %s", err)
 	}
 	defer resp.Body.Close()
 
-	// Read response body
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("cannot read response body: %s", err)
+	// Verify response
+	if resp.StatusCode != http.StatusGone {
+		return fmt.Errorf(fmt.Sprintf("invalid response status code: %d", resp.StatusCode))
 	}
-
-	// TODO: Handle response
-	fmt.Println("Status: ", resp.Status)
-	fmt.Println("Headers: ", resp.Header)
-	fmt.Println("Body: ", string(body))
 
 	return nil
 }
 
-// GetOrder gets an existing order using the HTTP API.
-func (api *API) GetOrder(orderID order.ID) (order.Order, error) {
+// GetOrder gets an existing order using the Relay API.
+func (api *API) GetOrder(orderID string) (order.Order, error) {
 	// Create a request and set the authorization header
-	req, err := http.NewRequest("GET", api.URL+"/orders/"+orderID.String(), nil)
+	req, err := http.NewRequest("GET", api.URL+"/orders/"+orderID, nil)
 	req.Header.Set("Authorization", "Bearer "+api.Token)
 
 	// Create a client and fetch request
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return order.Order{}, fmt.Errorf("cannot complete request: %s", err)
+		return order.Order{}, fmt.Errorf("cannot send request: %s", err)
 	}
 	defer resp.Body.Close()
+
+	// Verify response
+	if resp.StatusCode != http.StatusOK {
+		return order.Order{}, fmt.Errorf(fmt.Sprintf("invalid response status code: %d", resp.StatusCode))
+	}
 
 	// Read response body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return order.Order{}, fmt.Errorf("cannot read response body: %s", err)
 	}
-
-	// TODO: Handle response
-	fmt.Println("Status: ", resp.Status)
-	fmt.Println("Headers: ", resp.Header)
-	fmt.Println("Body: ", string(body))
 
 	var ord order.Order
 	if err := json.Unmarshal(body, ord); err != nil {
@@ -137,18 +130,18 @@ func (api *API) GetOrder(orderID order.ID) (order.Order, error) {
 	return ord, nil
 }
 
-// GetOrders retrieves updates to an existing order using the HTTP API. This
+// GetOrderbookUpdates retrieves updates to an existing order using the Relay API. This
 // function returns a channel which will contain all the updates that are
 // received.
-func (api *API) GetOrders(filter Filter) (<-chan order.Order, <-chan error) {
-	orders := make(chan order.Order)
+func (api *API) GetOrderbookUpdates(filter Filter) (<-chan orderbook.Entry, <-chan error) {
+	entries := make(chan orderbook.Entry)
 	errs := make(chan error)
 
 	// Construct WebSocket URL using filters
 	u := url.URL{Scheme: "ws", Host: api.URL, Path: "/orders"}
 	query := u.String() + "?id=" + filter.ID
 	if filter.Status != "" {
-		query = query + "&status=" + filter.Status
+		query = query + "&status=" + strings.Replace(filter.Status, " ", "", -1)
 	}
 
 	// Set authorization header and connect to WebSocket
@@ -158,31 +151,37 @@ func (api *API) GetOrders(filter Filter) (<-chan order.Order, <-chan error) {
 	if err != nil {
 		errs <- err
 		close(errs) // TODO: Check this
-		return orders, errs
+		return entries, errs
 	}
 	defer c.Close()
 
-	// Write any WebSocket messages to channel
+	// Write WebSocket messages to the channels
 	go func() {
-		defer close(orders)
+		defer close(entries)
 		defer close(errs)
 		for {
-			var ord order.Order
+			var entry orderbook.Entry
 			_, message, err := c.ReadMessage()
 			if err != nil {
 				errs <- err
 				continue // TODO: Confirm we want to continue after a read error
 			}
-			json.Unmarshal(message, ord)
-			orders <- ord
+			json.Unmarshal(message, entry)
+			entries <- entry
 		}
 	}()
 
-	return orders, errs
+	return entries, errs
 }
 
-// Insecure returns an empty string used for when when instantiating a new API
+// Insecure returns an empty string used for when instantiating a new API
 // object without providing a token.
 func Insecure() string {
+	return ""
+}
+
+// StatusAll returns an empty string used for when the user wishes to receive
+// all updates to an order.
+func StatusAll() string {
 	return ""
 }
