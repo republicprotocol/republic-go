a24fd9a25fb700bc0f2a361e8122e939fb84d616
tok-kkk <yunshi@republicprotocol.com>
2018-05-08 16:57:33 +1000

    add tests for inserting orders into the orderbook

--- orderbook/orderbook.go
+++ orderbook/orderbook.go
@@ -1,7 +1,10 @@
 package orderbook
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
 	"path"
 
@@ -15,16 +18,16 @@ import (
 // Orderbook is made after a call to Orderbook.Close.
 var ErrWriteToClosedOrderbook = errors.New("write to closed orderbook")
 
-type Syncer interface {
-	Open(order order.Order) error
-	Match(order order.Order) error
-	Confirm(order order.Order) error
-	Release(order order.Order) error
-	Settle(order order.Order) error
-	Cancel(order order.Order) error
-	Status(id order.ID) (order.Status, error)
-	Clear()
-}
+//type Syncer interface {
+//	Open(order order.Order) error
+//	Match(order order.Order) error
+//	Confirm(order order.Order) error
+//	Release(order order.Order) error
+//	Settle(order order.Order) error
+//	Cancel(order order.Order) error
+//	Status(id order.ID) (order.Status, error)
+//	Clear()
+//}
 
 // An Orderbook is responsible for store the historical orders.
 // It also broadcasts the newly received orders to its subscribers.
@@ -51,11 +54,11 @@ func NewOrderbook() (Orderbook, error) {
 	if err != nil {
 		return Orderbook{}, err
 	}
-	order, err := leveldb.OpenFile(path.Join(dbPath, "orders"), nil)
+	orders, err := leveldb.OpenFile(path.Join(dbPath, "orders"), nil)
 	if err != nil {
 		return Orderbook{}, err
 	}
-	atom, err := leveldb.OpenFile(path.Join(dbPath, "atoms"), nil)
+	atoms, err := leveldb.OpenFile(path.Join(dbPath, "atoms"), nil)
 	if err != nil {
 		return Orderbook{}, err
 	}
@@ -64,7 +67,7 @@ func NewOrderbook() (Orderbook, error) {
 		return Orderbook{}, err
 	}
 
-	broadcaster := dispatch.NewBroadcaster()
+	broadcaster := dispatch.NewBroadcaster(dispatch.MaxListeners)
 	broadcasterChDone := make(chan struct{})
 	broadcasterCh := make(chan interface{})
 	go broadcaster.Broadcast(broadcasterChDone, broadcasterCh)
@@ -72,8 +75,8 @@ func NewOrderbook() (Orderbook, error) {
 	return Orderbook{
 		status:         status,
 		orderFragments: orderFragments,
-		orders:         order,
-		atoms:          atom,
+		orders:         orders,
+		atoms:          atoms,
 		matches:        matches,
 
 		broadcaster:       broadcaster,
@@ -99,8 +102,8 @@ func (orderbook *Orderbook) Close() {
 
 // Listen to the orderbook for updates. Calls to Orderbook.Listen are
 // non-blocking, and the background worker is terminated when the done
-// channel is closed. A read-only channel of entries is returned, and will be
-// closed when no more data will be written to it.
+// channel is closed. A read-only channel of updates is returned, and
+// will be closed when no more data will be written to it.
 func (orderbook *Orderbook) Listen(done <-chan struct{}) <-chan Update {
 	listener := orderbook.broadcaster.Listen(done)
 	subscriber := make(chan Update)
@@ -130,18 +133,24 @@ func (orderbook *Orderbook) Listen(done <-chan struct{}) <-chan Update {
 				}
 			}
 		}, func() {
-
-			//Fixme:
-			blocks := orderbook.cache.Blocks()
-			for _, block := range blocks {
+			// Stream historical data
+			iter := orderbook.status.NewIterator(nil, nil)
+			for iter.Next() {
+				key, value := iter.Key(), iter.Value()
+				if len(value) != 1 {
+					return
+				}
+				update := NewUpdate(order.ID(key), value[0])
 				select {
 				case <-done:
 					return
 				case <-orderbook.broadcasterChDone:
 					return
-				case subscriber <- block:
+				case subscriber <- update:
 				}
 			}
+
+			iter.Release()
 		})
 	}()
 
@@ -151,15 +160,24 @@ func (orderbook *Orderbook) Listen(done <-chan struct{}) <-chan Update {
 // Open is called when we first receive the order fragment.
 func (orderbook *Orderbook) Open(fragment order.Fragment) error {
 	key := fragment.OrderID
-	// todo : serialize and deserialize orderFragment
-	if err := orderbook.orderFragments.Put([]byte(key), []byte("value"), nil); err != nil {
+
+	// Check existence of the order
+	_ , err := orderbook.status.Get(key, nil )
+	if err != leveldb.ErrNotFound{
+		return err
+	}
+
+	encoded, err := json.Marshal(fragment)
+	if err != nil {
+		return err
+	}
+	if err := orderbook.orderFragments.Put([]byte(key), encoded, nil); err != nil {
 		return err
 	}
 	if err := orderbook.status.Put([]byte(key), []byte{order.Open}, nil); err != nil {
 		return err
 	}
-	
-	
+
 	update := NewUpdate(fragment.OrderID, order.Open)
 	select {
 	case <-orderbook.broadcasterChDone:
@@ -172,7 +190,17 @@ func (orderbook *Orderbook) Open(fragment order.Fragment) error {
 // Match is called when we discover a match for the order.
 func (orderbook *Orderbook) Match(dlt delta.Delta) error {
 	// Save buy order in the orderbook
-	key := dlt.BuyOrderID
+	buyKey := dlt.BuyOrderID
+
+	// Check previous status of the order
+	buyStatus , err := orderbook.status.Get(buyKey, nil )
+	if err != nil {
+		return err
+	}
+	if bytes.Compare(buyStatus , []byte{order.Open}) != 0{
+		return fmt.Errorf("cannot matched order with status %v", buyStatus)
+	}
+
 	//todo : do we care about the expiry and nonce ( can get from the fragments table)
 	fstCode, err := dlt.FstCode.ToUint()
 	if err != nil {
@@ -192,19 +220,35 @@ func (orderbook *Orderbook) Match(dlt delta.Delta) error {
 		MaxVolume: dlt.MaxVolume,
 		MinVolume: dlt.MinVolume,
 	}
-	// todo : serialize and deserialize orderFragment
-	if err := orderbook.orders.Put([]byte(key), buyOrder.Hash(), nil); err != nil {
+	encodedBuy, err := json.Marshal(buyOrder)
+	if err != nil {
 		return err
 	}
-	if err := orderbook.status.Put([]byte(key), []byte{order.Unconfirmed}, nil); err != nil {
+	if err := orderbook.orders.Put([]byte(buyKey), encodedBuy, nil); err != nil {
 		return err
 	}
-	if err := orderbook.matches.Put([]byte(key), dlt.SellOrderID, nil); err != nil {
+	if err := orderbook.status.Put([]byte(buyKey), []byte{order.Unconfirmed}, nil); err != nil {
+		return err
+	}
+	buyMatches, err := json.Marshal([]order.ID{dlt.SellOrderID})
+	if err != nil {
+		return err
+	}
+	if err := orderbook.matches.Put([]byte(buyStatus), buyMatches, nil); err != nil {
 		return err
 	}
 
 	// Save sell order in the orderbook
-	key = dlt.SellOrderID
+	sellKey := dlt.SellOrderID
+
+	// Check previous status of the order
+	sellStatus , err := orderbook.status.Get(sellKey, nil )
+	if err != nil {
+		return err
+	}
+	if bytes.Compare(sellStatus , []byte{order.Open}) != 0{
+		return fmt.Errorf("cannot matched order with status %v", sellStatus)
+	}
 	//todo : do we care about the expiry and nonce ( can get from the fragments table)
 	fstCode, err = dlt.FstCode.ToUint()
 	if err != nil {
@@ -224,14 +268,18 @@ func (orderbook *Orderbook) Match(dlt delta.Delta) error {
 		MaxVolume: dlt.MaxVolume,
 		MinVolume: dlt.MinVolume,
 	}
-	// todo : serialize and deserialize orderFragment
-	if err := orderbook.orders.Put([]byte(key), sellOrder.Hash(), nil); err != nil {
+	encodedSell, err := json.Marshal(sellOrder)
+	if err := orderbook.orders.Put([]byte(sellKey), encodedSell, nil); err != nil {
 		return err
 	}
-	if err := orderbook.status.Put([]byte(key), []byte{order.Unconfirmed}, nil); err != nil {
+	if err := orderbook.status.Put([]byte(sellKey), []byte{order.Unconfirmed}, nil); err != nil {
 		return err
 	}
-	if err := orderbook.matches.Put([]byte(key), dlt.BuyOrderID, nil); err != nil {
+	sellMatches, err := json.Marshal([]order.ID{dlt.SellOrderID})
+	if err != nil {
+		return err
+	}
+	if err := orderbook.matches.Put([]byte(sellKey), sellMatches, nil); err != nil {
 		return err
 	}
 
@@ -253,6 +301,15 @@ func (orderbook *Orderbook) Match(dlt delta.Delta) error {
 
 // Confirm is called when the order has been confirmed by the hyperdrive.
 func (orderbook *Orderbook) Confirm(id order.ID) error {
+	// Check existence of the order
+	status, err := orderbook.status.Get(id, nil )
+	if err != nil{
+		return err
+	}
+	if bytes.Compare(status , []byte{order.Unconfirmed}) != 0{
+		return fmt.Errorf("cannot confirm order with status %v", status)
+	}
+
 	if err := orderbook.status.Put(id, []byte{order.Confirmed}, nil); err != nil {
 		return err
 	}
@@ -268,6 +325,15 @@ func (orderbook *Orderbook) Confirm(id order.ID) error {
 
 // Release is called when the order has been denied by the hyperdrive.
 func (orderbook *Orderbook) Release(id order.ID) error {
+	// Check existence of the order
+	status, err := orderbook.status.Get(id, nil )
+	if err != nil{
+		return err
+	}
+	if bytes.Compare(status , []byte{order.Unconfirmed}) != 0{
+		return fmt.Errorf("cannot release order with status %v", status)
+	}
+
 	if err := orderbook.status.Put(id, []byte{order.Open}, nil); err != nil {
 		return err
 	}
@@ -286,6 +352,15 @@ func (orderbook *Orderbook) Release(id order.ID) error {
 
 // Settle is called when the order is settled.
 func (orderbook *Orderbook) Settle(id order.ID) error {
+	// Check previous status of the order
+	status, err := orderbook.status.Get(id, nil )
+	if err != nil{
+		return err
+	}
+	if bytes.Compare(status , []byte{order.Confirmed}) != 0{
+		return fmt.Errorf("cannot settle order with status %v", status)
+	}
+
 	if err := orderbook.status.Put(id, []byte{order.Settled}, nil); err != nil {
 		return err
 	}
@@ -301,6 +376,14 @@ func (orderbook *Orderbook) Settle(id order.ID) error {
 
 // Cancel is called when the order is canceled.
 func (orderbook *Orderbook) Cancel(id order.ID) error {
+	// Check previous status of the order
+	status, err := orderbook.status.Get(id, nil )
+	if err != nil{
+		return err
+	}
+	if bytes.Compare(status , []byte{order.Open}) != 0 || bytes.Compare(status , []byte{order.Unconfirmed}) != 0{
+		return fmt.Errorf("too late too cancel the order")
+	}
 	if err := orderbook.status.Put(id, []byte{order.Canceled}, nil); err != nil {
 		return err
 	}
@@ -315,11 +398,36 @@ func (orderbook *Orderbook) Cancel(id order.ID) error {
 }
 
 // Order retrieves information regarding an order.
-func (orderbook *Orderbook) Order(id order.ID) {
-	// todo : whether we need this function
-	// what should we return if so.
+func (orderbook *Orderbook) Order(id order.ID) (order.Order, error) {
+	ord := order.Order{}
+	ordBytes, err := orderbook.orders.Get(id, nil)
+	if err != nil {
+		return order.Order{}, err
+	}
+	err = json.Unmarshal(ordBytes, ord)
+	if err != nil {
+		return order.Order{}, err
+	}
+
+	return ord, nil
 }
 
+// OrderFragment retrieves information regarding an orderFragment.
+func (orderbook *Orderbook) OrderFragment(id order.ID) (order.Fragment, error) {
+	fragment := order.Fragment{}
+	fragmentBytes, err := orderbook.orderFragments.Get(id, nil)
+	if err != nil {
+		return order.Fragment{}, err
+	}
+	err = json.Unmarshal(fragmentBytes, fragment)
+	if err != nil {
+		return order.Fragment{}, err
+	}
+
+	return fragment, nil
+}
+
+// Status retrieves status regarding an orderID.
 func (orderbook *Orderbook) Status(id order.ID) (order.Status, error) {
 	status, err := orderbook.status.Get(id, nil)
 	if err != nil {
@@ -330,4 +438,18 @@ func (orderbook *Orderbook) Status(id order.ID) (order.Status, error) {
 		return order.Nil, errors.New("internal error : wrong status length ")
 	}
 	return status[0], nil
+}
+
+// CounterOrders retrieves the matched orders regarding an orderID.
+func (orderbook *Orderbook) CounterOrders(id order.ID) ([]order.ID, error) {
+	var matches []order.ID
+	matchesBytes, err := orderbook.matches.Get(id, nil)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(matchesBytes, &matches); err != nil {
+		return nil, err
+	}
+
+	return matches, nil
 }
--- orderbook/orderbook_test.go
+++ orderbook/orderbook_test.go
@@ -1,33 +1,67 @@
 package orderbook_test
 
 import (
+	"os/exec"
+	"time"
+
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
+	"github.com/republicprotocol/republic-go/order"
 	"github.com/republicprotocol/republic-go/orderbook"
+	"github.com/republicprotocol/republic-go/smpc"
+	"github.com/republicprotocol/republic-go/stackint"
 )
 
-const maxConnections = 3
+const (
+	MaxConnections = 128
+	NumberOfOrders = 100
+	)
 
 var _ = Describe("order book", func() {
-	Context("creating new orderbook", func() {
+	Context("order status change event", func() {
 		var book orderbook.Orderbook
+		var orders [NumberOfOrders]order.ID
+		var err error
 
 		BeforeEach(func() {
-			book = orderbook.NewOrderbook()
+			book, err  = orderbook.NewOrderbook()
+			Expect(err).ShouldNot(HaveOccurred())
 		})
 
 		AfterEach(func() {
 			book.Close()
+
+			err:= ClearDB()
+			Expect(err).ShouldNot(HaveOccurred())
 		})
 
-		It("listen and stop listening", func() {
-			dones := [maxConnections]chan struct{}{}
-			for i := 0; i < maxConnections; i++ {
-				// stream := NewMockStream()
-				dones[i] = make(chan struct{})
-				defer close(dones[i])
-				book.Listen(dones[i])
+		It("should be able to open an order", func() {
+			for i:=0 ; i < NumberOfOrders; i ++ {
+				ord := NewOrder(order.ParityBuy, uint(i))
+				orders[i] = ord.ID
+				fragments, err  := ord.Split(1,1, &smpc.Prime)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				err = book.Open( *fragments[0])
+				Expect(err).ShouldNot(HaveOccurred())
+			}
+
+			for _, id := range orders{
+				status, err := book.Status(id)
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(status).Should(Equal(order.Open))
 			}
 		})
 	})
 })
+
+// ClearDB will delete the db files from previous tests.
+func ClearDB() error {
+	cmd := exec.Command("/bin/sh", "-c", "rm -rf $HOME/.darknode/orderbook")
+	return cmd.Run()
+}
+
+func NewOrder(parity order.Parity, i uint) *order.Order{
+	return order.NewOrder(order.TypeLimit, parity, time.Now(), order.CurrencyCodeREN, order.CurrencyCodeBTC, stackint.FromUint(i),stackint.FromUint(i),stackint.FromUint(i),stackint.FromUint(i) )
+}
+
