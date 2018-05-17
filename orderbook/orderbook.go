4d76b41a967f2c2c42c0f2b16c3198d7621b171e
tok-kkk <yunshi@republicprotocol.com>
2018-05-17 11:14:04 +1000

    orderbook: move syncer and storer from ome to the orderbook

--- orderbook/orderbook.go
+++ orderbook/orderbook.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/pkg/errors"
-	"github.com/republicprotocol/republic-go/cal"
 	"github.com/republicprotocol/republic-go/identity"
 	"github.com/republicprotocol/republic-go/order"
 )
@@ -13,7 +12,7 @@ var ErrOrderFragmentNotFound = errors.New("order fragment not found")
 
 type Orderbooker interface {
 	OpenOrder(context.Context, identity.MultiAddress, order.Fragment) error
-	Sync() ([]order.ID, []order.ID, error)
+	Sync() ([]OrderUpdate, error)
 	OrderFragment(order.ID) (order.Fragment, error)
 	Order(order.ID) (order.Order, error)
 }
@@ -28,13 +27,15 @@ type OrderbookServer interface {
 
 type orderbook struct {
 	client OrderbookClient
-	storer  Storer
+	storer Storer
+	syncer Syncer
 }
 
-func NewOrderbook(client OrderbookClient, ledger cal.RenLedger, storer Storer) orderbook{
+func NewOrderbook(client OrderbookClient, storer Storer, syncer Syncer) orderbook{
 	return orderbook{
 		client: client,
 		storer: storer,
+		syncer : syncer,
 	}
 }
 
@@ -42,6 +43,14 @@ func (book *orderbook) OpenOrder(ctx context.Context, multiAddr identity.MultiAd
 	return book.client.OpenOrder(ctx, multiAddr, orderFragment)
 }
 
-func (book *orderbook) Sync() ([]order.ID, []order.ID, error){
+func (book *orderbook) 	Sync() ([]OrderUpdate, error) {
+	return book.syncer.Sync()
+}
 
+func (book *orderbook) OrderFragment(id order.ID) (order.Fragment, error){
+	return book.storer.OrderFragment(id)
+}
+
+func (book *orderbook) Order(id order.ID) (order.Order, error){
+	return book.storer.Order(id)
 }
