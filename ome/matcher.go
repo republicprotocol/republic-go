b1e3bcf662b57b702c68a2ccaef13a6f02020e04
Loong <loong@republicprotocol.com>
2018-06-08 15:57:38 +1000

    ome: generic matching capability

--- ome/matcher.go
+++ ome/matcher.go
@@ -1,6 +1,7 @@
 package ome
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/republicprotocol/republic-go/logger"
@@ -9,9 +10,13 @@ import (
 	"github.com/republicprotocol/republic-go/smpc"
 )
 
+// ErrUnexpectedResolveStage is returned when a ResolveStage is not one of the
+// explicitly enumerated values.
+var ErrUnexpectedResolveStage = errors.New("unexpected resolve stage")
+
 // ResolveStage defines the various stages that resolving can be in for any
 // given Computation.
-type ResolveStage = byte
+type ResolveStage byte
 
 // Values for ResolveStage.
 const (
@@ -24,6 +29,29 @@ const (
 	ResolveStageSellVolumeCo
 	ResolveStageTokens
 )
+
+// String returns the human-readable representation of a ResolveStage.
+func (stage ResolveStage) String() string {
+	switch stage {
+	case ResolveStageNil:
+		return "nil"
+	case ResolveStagePriceExp:
+		return "priceExp"
+	case ResolveStagePriceCo:
+		return "priceCo"
+	case ResolveStageBuyVolumeExp:
+		return "buyVolumeExp"
+	case ResolveStageBuyVolumeCo:
+		return "buyVolumeCo"
+	case ResolveStageSellVolumeExp:
+		return "sellVolumeExp"
+	case ResolveStageSellVolumeCo:
+		return "sellVolumeCo"
+	case ResolveStageTokens:
+		return "tokens"
+	}
+	return ""
+}
 
 // A MatchCallback is called when a Computation is finished. The Computation
 // can then be inspected to determine if the result is a match.
@@ -57,251 +85,110 @@ func NewMatcher(storer Storer, smpcer smpc.Smpcer) Matcher {
 
 // Resolve implements the Matcher interface.
 func (matcher *matcher) Resolve(ξ [32]byte, com Computation, buyFragment, sellFragment order.Fragment, callback MatchCallback) {
-	matcher.resolvePriceExp(smpc.NetworkID(ξ), buyFragment, sellFragment, com, callback)
+	matcher.resolve(smpc.NetworkID(ξ), com, buyFragment, sellFragment, callback, ResolveStagePriceExp)
 }
 
-func (matcher *matcher) resolvePriceExp(networkID smpc.NetworkID, buyFragment, sellFragment order.Fragment, com Computation, callback MatchCallback) {
-	priceExpShare := buyFragment.Price.Exp.Sub(&sellFragment.Price.Exp)
-	priceExpJoin := smpc.Join{
-		ID:     smpc.JoinID(com.ID),
-		Index:  smpc.JoinIndex(priceExpShare.Index),
-		Shares: shamir.Shares{priceExpShare},
+func (matcher *matcher) resolve(networkID smpc.NetworkID, com Computation, buyFragment, sellFragment order.Fragment, callback MatchCallback, stage ResolveStage) {
+	join, err := buildJoin(com, buyFragment, sellFragment, stage)
+	if err != nil {
+		logger.Compute(logger.LevelError, fmt.Sprintf("cannot build %v join: %v", stage, err))
+		return
 	}
-	priceExpJoin.ID[31] = ResolveStagePriceExp
+	matcher.smpcer.Join(networkID, join, func(joinID smpc.JoinID, values []uint64) {
+		matcher.resolveValues(values, networkID, com, buyFragment, sellFragment, callback, stage)
+	})
+}
 
-	err := matcher.smpcer.Join(networkID, priceExpJoin, func(joinID smpc.JoinID, values []uint64) {
-		if len(values) != 1 {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot resolve priceExp: unexpected number of values: %v", len(values)))
-			return
-		}
+func (matcher *matcher) resolveValues(values []uint64, networkID smpc.NetworkID, com Computation, buyFragment, sellFragment order.Fragment, callback MatchCallback, stage ResolveStage) {
+	if len(values) != 1 {
+		logger.Compute(logger.LevelError, fmt.Sprintf("cannot resolve %v: unexpected number of values: %v", stage, len(values)))
+		return
+	}
+
+	switch stage {
+	case ResolveStagePriceExp, ResolveStageBuyVolumeExp, ResolveStageSellVolumeExp:
 		if isGreaterThanZero(values[0]) {
-			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ priceExp => buy = %v, sell = %v", com.Buy, com.Sell))
-			matcher.resolveBuyVolumeExp(networkID, buyFragment, sellFragment, com, callback)
+			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ %v => buy = %v, sell = %v", stage, com.Buy, com.Sell))
+			matcher.resolve(networkID, com, buyFragment, sellFragment, callback, stage+2)
 			return
 		}
 		if isEqualToZero(values[0]) {
-			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ priceExp => buy = %v, sell = %v", com.Buy, com.Sell))
-			matcher.resolvePriceCo(networkID, buyFragment, sellFragment, com, callback)
+			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ %v => buy = %v, sell = %v", stage, com.Buy, com.Sell))
+			matcher.resolve(networkID, com, buyFragment, sellFragment, callback, stage+1)
 			return
 		}
-		com.State = ComputationStateMismatched
-		com.Match = false
-		if err := matcher.storer.InsertComputation(com); err != nil {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot store mismatched computation buy = %v, sell = %v", com.Buy, com.Sell))
-		}
-		logger.Compute(logger.LevelDebugHigh, fmt.Sprintf("✗ priceExp => mismatch buy = %v, sell = %v", com.Buy, com.Sell))
-		callback(com)
-	})
-	if err != nil {
-		logger.Compute(logger.LevelError, fmt.Sprintf("cannot join priceExp: %v", err))
-	}
-}
 
-func (matcher *matcher) resolvePriceCo(networkID smpc.NetworkID, buyFragment, sellFragment order.Fragment, com Computation, callback MatchCallback) {
-	priceCoShare := buyFragment.Price.Co.Sub(&sellFragment.Price.Co)
-	priceCoJoin := smpc.Join{
-		ID:     smpc.JoinID(com.ID),
-		Index:  smpc.JoinIndex(priceCoShare.Index),
-		Shares: shamir.Shares{priceCoShare},
-	}
-	priceCoJoin.ID[31] = ResolveStagePriceCo
-
-	err := matcher.smpcer.Join(networkID, priceCoJoin, func(joinID smpc.JoinID, values []uint64) {
-		if len(values) != 1 {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot resolve priceCo: unexpected number of values: %v", len(values)))
-			return
-		}
+	case ResolveStagePriceCo, ResolveStageBuyVolumeCo, ResolveStageSellVolumeCo:
 		if isGreaterThanOrEqualToZero(values[0]) {
-			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ priceCo => buy = %v, sell = %v", com.Buy, com.Sell))
-			matcher.resolveBuyVolumeExp(networkID, buyFragment, sellFragment, com, callback)
+			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ %v => buy = %v, sell = %v", stage, com.Buy, com.Sell))
+			matcher.resolve(networkID, com, buyFragment, sellFragment, callback, stage+1)
 			return
 		}
-		com.State = ComputationStateMismatched
-		com.Match = false
-		if err := matcher.storer.InsertComputation(com); err != nil {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot store mismatched computation buy = %v, sell = %v", com.Buy, com.Sell))
-		}
-		logger.Compute(logger.LevelDebugHigh, fmt.Sprintf("✗ priceCo => mismatch buy = %v, sell = %v", com.Buy, com.Sell))
-		callback(com)
-	})
-	if err != nil {
-		logger.Compute(logger.LevelError, fmt.Sprintf("cannot join priceCo: %v", err))
-	}
-}
 
-func (matcher *matcher) resolveBuyVolumeExp(networkID smpc.NetworkID, buyFragment, sellFragment order.Fragment, com Computation, callback MatchCallback) {
-	buyVolumeExpShare := buyFragment.Volume.Exp.Sub(&sellFragment.MinimumVolume.Exp)
-	buyVolumeExpJoin := smpc.Join{
-		ID:     smpc.JoinID(com.ID),
-		Index:  smpc.JoinIndex(buyVolumeExpShare.Index),
-		Shares: shamir.Shares{buyVolumeExpShare},
-	}
-	buyVolumeExpJoin.ID[31] = ResolveStageBuyVolumeExp
-
-	err := matcher.smpcer.Join(networkID, buyVolumeExpJoin, func(joinID smpc.JoinID, values []uint64) {
-		if len(values) != 1 {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot resolve buyVolumeExp: unexpected number of values: %v", len(values)))
-			return
-		}
-		if isGreaterThanZero(values[0]) {
-			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ buyVolumeExp => buy = %v, sell = %v", com.Buy, com.Sell))
-			matcher.resolveSellVolumeExp(networkID, buyFragment, sellFragment, com, callback)
-			return
-		}
+	case ResolveStageTokens:
 		if isEqualToZero(values[0]) {
-			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ buyVolumeExp => buy = %v, sell = %v", com.Buy, com.Sell))
-			matcher.resolveBuyVolumeCo(networkID, buyFragment, sellFragment, com, callback)
-			return
-		}
-		com.State = ComputationStateMismatched
-		com.Match = false
-		if err := matcher.storer.InsertComputation(com); err != nil {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot store mismatched computation buy = %v, sell = %v", com.Buy, com.Sell))
-		}
-		logger.Compute(logger.LevelDebugHigh, fmt.Sprintf("✗ buyVolumeExp => mismatch buy = %v, sell = %v", com.Buy, com.Sell))
-		callback(com)
-	})
-	if err != nil {
-		logger.Compute(logger.LevelError, fmt.Sprintf("cannot join buyVolumeExp: %v", err))
-	}
-}
-
-func (matcher *matcher) resolveBuyVolumeCo(networkID smpc.NetworkID, buyFragment, sellFragment order.Fragment, com Computation, callback MatchCallback) {
-	buyVolumeCoShare := buyFragment.Volume.Co.Sub(&sellFragment.MinimumVolume.Co)
-	buyVolumeCoJoin := smpc.Join{
-		ID:     smpc.JoinID(com.ID),
-		Index:  smpc.JoinIndex(buyVolumeCoShare.Index),
-		Shares: shamir.Shares{buyVolumeCoShare},
-	}
-	buyVolumeCoJoin.ID[31] = ResolveStageBuyVolumeCo
-
-	err := matcher.smpcer.Join(networkID, buyVolumeCoJoin, func(joinID smpc.JoinID, values []uint64) {
-		if len(values) != 1 {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot resolve buyVolumeCo: unexpected number of values: %v", len(values)))
-			return
-		}
-		if isGreaterThanOrEqualToZero(values[0]) {
-			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ buyVolumeCo => buy = %v, sell = %v", com.Buy, com.Sell))
-			matcher.resolveSellVolumeExp(networkID, buyFragment, sellFragment, com, callback)
-			return
-		}
-		com.State = ComputationStateMismatched
-		com.Match = false
-		if err := matcher.storer.InsertComputation(com); err != nil {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot store mismatched computation buy = %v, sell = %v", com.Buy, com.Sell))
-		}
-		logger.Compute(logger.LevelDebugHigh, fmt.Sprintf("✗ buyVolumeCo => mismatch buy = %v, sell = %v", com.Buy, com.Sell))
-		callback(com)
-	})
-	if err != nil {
-		logger.Compute(logger.LevelError, fmt.Sprintf("cannot join buyVolumeCo: %v", err))
-	}
-}
-
-func (matcher *matcher) resolveSellVolumeExp(networkID smpc.NetworkID, buyFragment, sellFragment order.Fragment, com Computation, callback MatchCallback) {
-	sellVolumeExpShare := sellFragment.Volume.Exp.Sub(&buyFragment.MinimumVolume.Exp)
-	sellVolumeExpJoin := smpc.Join{
-		ID:     smpc.JoinID(com.ID),
-		Index:  smpc.JoinIndex(sellVolumeExpShare.Index),
-		Shares: shamir.Shares{sellVolumeExpShare},
-	}
-	sellVolumeExpJoin.ID[31] = ResolveStageSellVolumeExp
-
-	err := matcher.smpcer.Join(networkID, sellVolumeExpJoin, func(joinID smpc.JoinID, values []uint64) {
-		if len(values) != 1 {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot resolve sellVolumeExp: unexpected number of values: %v", len(values)))
-			return
-		}
-		if isGreaterThanZero(values[0]) {
-			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ sellVolumeExp => buy = %v, sell = %v", com.Buy, com.Sell))
-			matcher.resolveTokens(networkID, buyFragment, sellFragment, com, callback)
-			return
-		}
-		if isEqualToZero(values[0]) {
-			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ sellVolumeExp => buy = %v, sell = %v", com.Buy, com.Sell))
-			matcher.resolveSellVolumeCo(networkID, buyFragment, sellFragment, com, callback)
-			return
-		}
-		com.State = ComputationStateMismatched
-		com.Match = false
-		if err := matcher.storer.InsertComputation(com); err != nil {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot store mismatched computation buy = %v, sell = %v", com.Buy, com.Sell))
-		}
-		logger.Compute(logger.LevelDebugHigh, fmt.Sprintf("✗ sellVolumeExp => mismatch buy = %v, sell = %v", com.Buy, com.Sell))
-		callback(com)
-	})
-	if err != nil {
-		logger.Compute(logger.LevelError, fmt.Sprintf("cannot join sellVolumeExp: %v", err))
-	}
-}
-
-func (matcher *matcher) resolveSellVolumeCo(networkID smpc.NetworkID, buyFragment, sellFragment order.Fragment, com Computation, callback MatchCallback) {
-	sellVolumeCoShare := sellFragment.Volume.Co.Sub(&buyFragment.MinimumVolume.Co)
-	sellVolumeCoJoin := smpc.Join{
-		ID:     smpc.JoinID(com.ID),
-		Index:  smpc.JoinIndex(sellVolumeCoShare.Index),
-		Shares: shamir.Shares{sellVolumeCoShare},
-	}
-	sellVolumeCoJoin.ID[31] = ResolveStageSellVolumeCo
-
-	err := matcher.smpcer.Join(networkID, sellVolumeCoJoin, func(joinID smpc.JoinID, values []uint64) {
-		if len(values) != 1 {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot resolve sellVolumeCo: unexpected number of values: %v", len(values)))
-			return
-		}
-		if isGreaterThanOrEqualToZero(values[0]) {
-			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ sellVolumeCo => buy = %v, sell = %v", com.Buy, com.Sell))
-			matcher.resolveTokens(networkID, buyFragment, sellFragment, com, callback)
-			return
-		}
-		com.State = ComputationStateMismatched
-		com.Match = false
-		if err := matcher.storer.InsertComputation(com); err != nil {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot store mismatched computation buy = %v, sell = %v", com.Buy, com.Sell))
-		}
-		logger.Compute(logger.LevelDebugHigh, fmt.Sprintf("✗ sellVolumeCo => mismatch buy = %v, sell = %v", com.Buy, com.Sell))
-		callback(com)
-	})
-	if err != nil {
-		logger.Compute(logger.LevelError, fmt.Sprintf("cannot join sellVolumeCo: %v", err))
-	}
-}
-
-func (matcher *matcher) resolveTokens(networkID smpc.NetworkID, buyFragment, sellFragment order.Fragment, com Computation, callback MatchCallback) {
-	tokensShare := buyFragment.Tokens.Sub(&sellFragment.Tokens)
-	tokensJoin := smpc.Join{
-		ID:     smpc.JoinID(com.ID),
-		Index:  smpc.JoinIndex(tokensShare.Index),
-		Shares: shamir.Shares{tokensShare},
-	}
-	tokensJoin.ID[31] = ResolveStageTokens
-
-	err := matcher.smpcer.Join(networkID, tokensJoin, func(joinID smpc.JoinID, values []uint64) {
-		if len(values) != 1 {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot resolve tokens: unexpected number of values: %v", len(values)))
-			return
-		}
-		if isEqualToZero(values[0]) {
+			// Store the computation as a match
 			com.State = ComputationStateMatched
 			com.Match = true
 			if err := matcher.storer.InsertComputation(com); err != nil {
 				logger.Compute(logger.LevelError, fmt.Sprintf("cannot store matched computation buy = %v, sell = %v", com.Buy, com.Sell))
 			}
-			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ tokens => buy = %v, sell = %v", com.Buy, com.Sell))
+
+			// Trigger the callback with a match
+			logger.Compute(logger.LevelDebug, fmt.Sprintf("✔ %v => buy = %v, sell = %v", stage, com.Buy, com.Sell))
 			callback(com)
 			return
 		}
-		com.State = ComputationStateMismatched
-		com.Match = false
-		if err := matcher.storer.InsertComputation(com); err != nil {
-			logger.Compute(logger.LevelError, fmt.Sprintf("cannot store mismatched computation buy = %v, sell = %v", com.Buy, com.Sell))
-		}
-		logger.Compute(logger.LevelDebugHigh, fmt.Sprintf("✗ tokens => mismatch buy = %v, sell = %v", com.Buy, com.Sell))
-		callback(com)
-	})
-	if err != nil {
-		logger.Compute(logger.LevelError, fmt.Sprintf("cannot join tokens: %v", err))
+
+	default:
+		// If the stage is unknown it is always considered a mismatch
 	}
+
+	// Store the computation as a mismatch
+	com.State = ComputationStateMismatched
+	com.Match = false
+	if err := matcher.storer.InsertComputation(com); err != nil {
+		logger.Compute(logger.LevelError, fmt.Sprintf("cannot store mismatched computation buy = %v, sell = %v", com.Buy, com.Sell))
+	}
+
+	// Trigger the callback with a mismatch
+	logger.Compute(logger.LevelDebug, fmt.Sprintf("✗ %v => buy = %v, sell = %v", stage, com.Buy, com.Sell))
+	callback(com)
+}
+
+func buildJoin(com Computation, buyFragment, sellFragment order.Fragment, stage ResolveStage) (smpc.Join, error) {
+	var share shamir.Share
+	switch stage {
+	case ResolveStagePriceExp:
+		share = buyFragment.Price.Exp.Sub(&sellFragment.Price.Exp)
+
+	case ResolveStagePriceCo:
+		share = buyFragment.Price.Co.Sub(&sellFragment.Price.Co)
+
+	case ResolveStageBuyVolumeExp:
+		share = buyFragment.Volume.Exp.Sub(&sellFragment.MinimumVolume.Exp)
+
+	case ResolveStageBuyVolumeCo:
+		share = buyFragment.Volume.Co.Sub(&sellFragment.MinimumVolume.Co)
+
+	case ResolveStageSellVolumeExp:
+		share = sellFragment.Volume.Exp.Sub(&buyFragment.MinimumVolume.Exp)
+
+	case ResolveStageSellVolumeCo:
+		share = sellFragment.Volume.Co.Sub(&buyFragment.MinimumVolume.Co)
+
+	case ResolveStageTokens:
+		share = buyFragment.Tokens.Sub(&sellFragment.Tokens)
+	default:
+		return smpc.Join{}, ErrUnexpectedResolveStage
+	}
+	join := smpc.Join{
+		ID:     smpc.JoinID(com.ID),
+		Index:  smpc.JoinIndex(share.Index),
+		Shares: shamir.Shares{share},
+	}
+	join.ID[31] = byte(stage)
+	return join, nil
 }
 
 func isGreaterThanOrEqualToZero(value uint64) bool {
