0717837c3ffa026006964cc6d7ea910748c1cdfe
tok-kkk <yunshi@republicprotocol.com>
2018-06-12 11:34:31 +1000

    test: add mock implementation of darkpool accounts

--- ome/settler_test.go
+++ ome/settler_test.go
@@ -1,1 +1,36 @@
 package ome_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/republicprotocol/republic-go/ome"
+	"github.com/republicprotocol/republic-go/testutils"
+
+	"github.com/republicprotocol/republic-go/cal"
+	"github.com/republicprotocol/republic-go/smpc"
+)
+
+
+var _ = Describe("Settler", func() {
+	var storer Storer
+	var smpcer smpc.Smpcer
+	var account cal.DarkpoolAccounts
+
+
+	BeforeEach(func() {
+		storer = testutils.NewStorer()
+		smpcer = testutils.NewAlwaysMatchSmpc()
+		account = testutils.
+	})
+
+
+	Context("when a computation has been resolved to a match and been confirmed ", func() {
+		It("should be able to reconstruct the order and settle it.", func() {
+
+
+
+
+			Ω(true).Should(BeTrue())
+		})
+	})
+})
