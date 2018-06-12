1de907b8df728c978fa2f6251628d1c062e01435
tok-kkk <yunshi@republicprotocol.com>
2018-06-12 11:56:57 +1000

    grpc: uncomment the stream test

--- ome/settler_test.go
+++ ome/settler_test.go
@@ -4,31 +4,26 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/republicprotocol/republic-go/ome"
-	"github.com/republicprotocol/republic-go/testutils"
 
 	"github.com/republicprotocol/republic-go/cal"
 	"github.com/republicprotocol/republic-go/smpc"
+	"github.com/republicprotocol/republic-go/testutils"
 )
-
 
 var _ = Describe("Settler", func() {
 	var storer Storer
 	var smpcer smpc.Smpcer
-	var account cal.DarkpoolAccounts
-
+	var accounts cal.DarkpoolAccounts
 
 	BeforeEach(func() {
 		storer = testutils.NewStorer()
 		smpcer = testutils.NewAlwaysMatchSmpc()
-		account = testutils.
+		accounts = testutils.NewDarkpoolAccounts()
 	})
-
 
 	Context("when a computation has been resolved to a match and been confirmed ", func() {
 		It("should be able to reconstruct the order and settle it.", func() {
-
-
-
+			// todo
 
 			Ω(true).Should(BeTrue())
 		})
