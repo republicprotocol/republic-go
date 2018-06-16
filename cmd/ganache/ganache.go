7fa3ac1af38ace7ba3617aa9c4832ff3a5006199
Divya <u6479594@anu.edu.au>
2018-06-16 17:10:55 +1000

    cmd: commenting out ganache.go temporarily

--- cmd/ganache/ganache.go
+++ cmd/ganache/ganache.go
@@ -1,42 +1,42 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"os/signal"
+// 	"syscall"
+// 	"time"
 
-	"github.com/republicprotocol/republic-go/blockchain/test/ganache"
-)
+// 	"github.com/republicprotocol/republic-go/blockchain/test/ganache"
+// )
 
-const reset = "\x1b[0m"
-const green = "\x1b[32;1m"
-const yellow = "\x1b[33;1m"
+// const reset = "\x1b[0m"
+// const green = "\x1b[32;1m"
+// const yellow = "\x1b[33;1m"
 
-func main() {
-	argSleep := flag.Int("sleep", 10, "Seconds to sleep after starting ganache")
-	flag.Parse()
+// func main() {
+// 	argSleep := flag.Int("sleep", 10, "Seconds to sleep after starting ganache")
+// 	flag.Parse()
 
-	fmt.Printf("Ganache is listening on %shttp://localhost:8545%s...\n", green, reset)
+// 	fmt.Printf("Ganache is listening on %shttp://localhost:8545%s...\n", green, reset)
 
-	ganache.Start()
-	time.Sleep(time.Duration(*argSleep) * time.Second)
+// 	ganache.Start()
+// 	time.Sleep(time.Duration(*argSleep) * time.Second)
 
-	conn, err := ganache.Connect("http://localhost:8545")
-	if err != nil {
-		log.Fatalf("cannot connect to ganache: %v", err)
-	}
+// 	conn, err := ganache.Connect("http://localhost:8545")
+// 	if err != nil {
+// 		log.Fatalf("cannot connect to ganache: %v", err)
+// 	}
 
-	if err := ganache.DeployContracts(conn); err != nil {
-		log.Fatalf("cannot deploy contracts to ganache: %v", err)
-	}
+// 	if err := ganache.DeployContracts(conn); err != nil {
+// 		log.Fatalf("cannot deploy contracts to ganache: %v", err)
+// 	}
 
-	signals := make(chan os.Signal, 1)
-	defer close(signals)
+// 	signals := make(chan os.Signal, 1)
+// 	defer close(signals)
 
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
-	<-signals
-}
+// 	signal.Notify(signals, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
+// 	<-signals
+// }
