926d03537530b200b311decaf040264b887891de
Loong <loong@republicprotocol.com>
2018-06-17 17:57:41 +1000

    blockchain: mutex added to accounts to reduce contention

--- blockchain/ethereum/accounts/accounts.go
+++ blockchain/ethereum/accounts/accounts.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"log"
 	"math/big"
+	"sync"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
@@ -14,6 +15,8 @@ import (
 
 // RenExAccounts implements the cal.DarkpoolAccounts interface
 type RenExAccounts struct {
+	mu *sync.RWMutex
+
 	network      ethereum.Network
 	context      context.Context
 	conn         ethereum.Conn
@@ -31,6 +34,8 @@ func NewRenExAccounts(ctx context.Context, conn ethereum.Conn, transactOpts *bin
 	}
 
 	return RenExAccounts{
+		mu: new(sync.RWMutex),
+
 		network:      conn.Config.Network,
 		context:      ctx,
 		conn:         conn,
@@ -43,18 +48,49 @@ func NewRenExAccounts(ctx context.Context, conn ethereum.Conn, transactOpts *bin
 
 // SubmitOrder to the RenEx accounts
 func (accounts *RenExAccounts) SubmitOrder(ord order.Order) error {
-	nonceHash := big.NewInt(0).SetBytes(ord.BytesFromNonce())
-	log.Printf("[submit order] id: %v,tokens:%d, priceCo:%v, priceExp:%v, volumeCo:%v, volumeExp:%v, minVol:%v, minVolExp:%v", base64.StdEncoding.EncodeToString(ord.ID[:]), uint64(ord.Tokens), uint16(ord.Price.Co), uint16(ord.Price.Exp), uint16(ord.Volume.Co), uint16(ord.Volume.Exp), uint16(ord.MinimumVolume.Co), uint16(ord.MinimumVolume.Exp))
-	tx, err := accounts.binding.SubmitOrder(accounts.transactOpts, uint8(ord.Type), uint8(ord.Parity), uint64(ord.Expiry.Unix()), uint64(ord.Tokens), uint16(ord.Price.Co), uint16(ord.Price.Exp), uint16(ord.Volume.Co), uint16(ord.Volume.Exp), uint16(ord.MinimumVolume.Co), uint16(ord.MinimumVolume.Exp), nonceHash)
-	if err != nil {
-		return err
-	}
-	_, err = accounts.conn.PatchedWaitMined(accounts.context, tx)
-	return err
+	accounts.mu.Lock()
+	defer accounts.mu.Unlock()
+
+	return accounts.submitOrder(ord)
 }
 
 // SubmitMatch will submit a matched order pair to the RenEx accounts
 func (accounts *RenExAccounts) SubmitMatch(buy, sell order.ID) error {
+	accounts.mu.Lock()
+	defer accounts.mu.Unlock()
+
+	return accounts.submitMatch(buy, sell)
+}
+
+// Settle implements the cal.DarkpoolAccounts interface
+func (accounts *RenExAccounts) Settle(buy order.Order, sell order.Order) error {
+	accounts.mu.Lock()
+	defer accounts.mu.Unlock()
+
+	err := accounts.submitOrder(buy)
+	if err != nil {
+		return err
+	}
+	err = accounts.submitOrder(sell)
+	if err != nil {
+		return err
+	}
+
+	return accounts.submitMatch(buy.ID, sell.ID)
+}
+
+func (accounts *RenExAccounts) SettlementDetail(buy, sell order.ID) (*big.Int, *big.Int, *big.Int, *big.Int, *big.Int, error) {
+	accounts.mu.RLock()
+	defer accounts.mu.RUnlock()
+
+	price, lowVolume, highVolume, lowFee, highFee, err := accounts.binding.GetSettlementDetails(accounts.callOpts, buy, sell)
+	if err != nil {
+		return nil, nil, nil, nil, nil, err
+	}
+	return price, lowVolume, highVolume, lowFee, highFee, nil
+}
+
+func (accounts *RenExAccounts) submitMatch(buy, sell order.ID) error {
 	accounts.transactOpts.GasLimit = 500000
 	tx, err := accounts.binding.SubmitMatch(accounts.transactOpts, buy, sell)
 	if err != nil {
@@ -64,24 +100,13 @@ func (accounts *RenExAccounts) SubmitMatch(buy, sell order.ID) error {
 	return err
 }
 
-// Settle implements the cal.DarkpoolAccounts interface
-func (accounts *RenExAccounts) Settle(buy order.Order, sell order.Order) error {
-	err := accounts.SubmitOrder(buy)
+func (accounts *RenExAccounts) submitOrder(ord order.Order) error {
+	nonceHash := big.NewInt(0).SetBytes(ord.BytesFromNonce())
+	log.Printf("[submit order] id: %v,tokens:%d, priceCo:%v, priceExp:%v, volumeCo:%v, volumeExp:%v, minVol:%v, minVolExp:%v", base64.StdEncoding.EncodeToString(ord.ID[:]), uint64(ord.Tokens), uint16(ord.Price.Co), uint16(ord.Price.Exp), uint16(ord.Volume.Co), uint16(ord.Volume.Exp), uint16(ord.MinimumVolume.Co), uint16(ord.MinimumVolume.Exp))
+	tx, err := accounts.binding.SubmitOrder(accounts.transactOpts, uint8(ord.Type), uint8(ord.Parity), uint64(ord.Expiry.Unix()), uint64(ord.Tokens), uint16(ord.Price.Co), uint16(ord.Price.Exp), uint16(ord.Volume.Co), uint16(ord.Volume.Exp), uint16(ord.MinimumVolume.Co), uint16(ord.MinimumVolume.Exp), nonceHash)
 	if err != nil {
 		return err
 	}
-	err = accounts.SubmitOrder(sell)
-	if err != nil {
-		return err
-	}
-
-	return accounts.SubmitMatch(buy.ID, sell.ID)
-}
-
-func (accounts *RenExAccounts) SettlementDetail(buy, sell order.ID) (*big.Int, *big.Int, *big.Int, *big.Int, *big.Int, error) {
-	price, lowVolume, highVolume, lowFee, highFee, err := accounts.binding.GetSettlementDetails(accounts.callOpts, buy, sell)
-	if err != nil {
-		return nil, nil, nil, nil, nil, err
-	}
-	return price, lowVolume, highVolume, lowFee, highFee, nil
+	_, err = accounts.conn.PatchedWaitMined(accounts.context, tx)
+	return err
 }
