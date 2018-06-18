4eeff1242f3d271c8fb15929870fce0225874d6e
Loong <loong@republicprotocol.com>
2018-06-18 10:59:39 +1000

    orderbook: reverted accidental copy/paste

--- orderbook/sync.go
+++ orderbook/sync.go
@@ -101,7 +101,7 @@ func (syncer *syncer) Sync() (ChangeSet, error) {
 
 	buyOrderIDs, buyErr := syncer.renLedger.BuyOrders(syncer.syncBuyPointer, syncer.renLedgerLimit)
 	if buyErr == nil {
-		for i, ord := range buyOrderIDs {
+		for _, ord := range buyOrderIDs {
 			status, err := syncer.renLedger.Status(ord)
 			if err != nil {
 				log.Println("cannot sync order status", err)
@@ -131,7 +131,7 @@ func (syncer *syncer) Sync() (ChangeSet, error) {
 	// Get new sell orders from the ledger
 	sellOrderIDs, sellErr := syncer.renLedger.SellOrders(syncer.syncSellPointer, syncer.renLedgerLimit)
 	if sellErr == nil {
-		for i, ord := range sellOrderIDs {
+		for _, ord := range sellOrderIDs {
 
 			status, err := syncer.renLedger.Status(ord)
 			if err != nil {
@@ -228,13 +228,13 @@ func (syncer *syncer) purge() ChangeSet {
 						return
 					}
 					if status == order.Open {
-						returnsellOrders
+						return
 					}
 
-					blockNumbesellOrderser.renLedger.BlockNumber(sellOrder)
-					if err != sellOrders
-						log.PrsellOrderssync order status", err)
-						returnsellOrders
+					blockNumber, err := syncer.renLedger.BlockNumber(sellOrder)
+					if err != nil {
+						log.Println("cannot sync order status", err)
+						return
 					}
 					priority, err := syncer.renLedger.Priority(sellOrder)
 					if err != nil {
