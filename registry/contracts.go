ddb3faf2b709225fdca619f122f112e4b3a39eeb
Divya <u6479594@anu.edu.au>
2018-06-19 17:09:38 +1000

    registry: fixed ContractBinder comments

--- registry/contracts.go
+++ registry/contracts.go
@@ -6,15 +6,11 @@ import (
 	"github.com/republicprotocol/republic-go/identity"
 )
 
-// ContractsBinder will define all interactions that the orderbook will
-// have with the smart contracts
+// ContractsBinder will define all methods that the registry will
+// require to communicate with smart contracts. All the methods will
+// be implemented in contracts.Binder
 type ContractsBinder interface {
-
-	// PublicKey returns the RSA public key of the Darknode registered with the
-	// given identity.Address.
 	PublicKey(addr identity.Address) (rsa.PublicKey, error)
 
-	// IsRegistered returns true if the identity.Address is a current
-	// registered Darknode. Otherwise, it returns false.
 	IsRegistered(addr identity.Address) (bool, error)
 }
