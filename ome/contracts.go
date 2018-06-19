6a7d16e66eefbd78627d6708f2e348ffb8256d09
Divya <u6479594@anu.edu.au>
2018-06-19 17:09:04 +1000

    ome: fixed ContractBinder comments

--- ome/contracts.go
+++ ome/contracts.go
@@ -9,22 +9,17 @@ const StatusOpen = 1
 const StatusConfirmed = 2
 const StatusCanceled = 3
 
-// ContractsBinder will define all interactions that the orderbook will
-// have with the smart contracts
+// ContractsBinder will define all methods that the order matching
+// engine will require to communicate with smart contracts. All the
+// methods will be implemented in contracts.Binder
 type ContractsBinder interface {
-
-	// ConfirmOrder match on the Ren Ledger.
 	ConfirmOrder(buy order.ID, sell order.ID) error
 
-	// Depth will return depth of confirmation blocks
 	Depth(orderID order.ID) (uint, error)
 
-	// Status will return the status of the order
 	Status(orderID order.ID) (order.Status, error)
 
-	// Settle the order pair which gets confirmed by the RenLedger
 	Settle(buy order.Order, sell order.Order) error
 
-	// OrderMatch of an order, if any.
 	OrderMatch(order order.ID) (order.ID, error)
 }
