96fd83a0892e8b829b1044f89560ba284fdf35e3
Loong <loong@republicprotocol.com>
2018-06-20 17:33:08 +1000

    orderbook: store interface updated

--- orderbook/store.go
+++ orderbook/store.go
@@ -6,48 +6,61 @@ import (
 	"github.com/republicprotocol/republic-go/order"
 )
 
-// ErrOrderFragmentNotFound is return when attempting to load an order that
+// ErrOrderFragmentNotFound is returned when attempting to read an order that
 // cannot be found.
 var ErrOrderFragmentNotFound = errors.New("order fragment not found")
 
-// ErrOrderNotFound is return when attempting to load an order that cannot be
-// found.
+// ErrOrderNotFound is returned whened attempting to read an order that cannot
+// be found.
 var ErrOrderNotFound = errors.New("order not found")
+
+// ErrCursorOutOfRange is returned when an iterator cursor is used to read a
+// value outside the range of the iterator.
+var ErrCursorOutOfRange = errors.New("cursor out of range")
 
 // Storer for the orders and order fragments that are received by the
 // Orderbook.
 type Storer interface {
-	InsertOrderFragment(orderFragment order.Fragment) error
-	InsertOrder(order order.Order) error
-	OrderFragment(id order.ID) (order.Fragment, error)
+	PutOrder(order order.Order) error
+	DeleteOrder(id order.ID) error
 	Order(id order.ID) (order.Order, error)
-	RemoveOrderFragment(id order.ID) error
-	RemoveOrder(id order.ID) error
+	Orders() (OrderIterator, error)
+
+	PutOrderFragment(orderFragment order.Fragment) error
+	DeleteOrderFragment(id order.ID) error
+	OrderFragment(id order.ID) (order.Fragment, error)
+	OrderFragments() (OrderFragmentIterator, error)
+
+	PutBuyPointer(pointer Pointer) error
+	BuyPointer() (Pointer, error)
+
+	PutOrderPointer(pointer Pointer) error
+	OrderPointer() (Pointer, error)
 }
 
-// SyncPointer points to the last order.Order that was successfully
-// synchronized by the Syncer. It is primarily used to prevent re-syncing all
-// orders aftter a reboot.
-type SyncPointer = int
+// OrderIterator is used to iterate over an order.order collection.
+type OrderIterator interface {
 
-// SyncStorer exposes functionality for storing and loading synchronization
-// data that the Syncer uses to keep track of where it is in the
-// synchronization process.
-type SyncStorer interface {
+	// Next progresses the cursor. Returns true if the new cursor is still in
+	// the range of the order.Order collection, otherwise false.
+	Next() bool
 
-	// InsertBuyPointer into the Storer. The prevents the Syncer needing to
-	// re-sync all buy orders after a reboot.
-	InsertBuyPointer(SyncPointer) error
-
-	// InsertSellPointer into the Storer. The prevents the Syncer needing to
-	// re-sync all sell orders after a reboot.
-	InsertSellPointer(SyncPointer) error
-
-	// BuyPointer returns the SyncPointer stored in the Storer. It defaults to
-	// zero.
-	BuyPointer() (SyncPointer, error)
-
-	// SellPointer returns the SyncPointer stored in the Storer. It defaults to
-	// zero.
-	SellPointer() (SyncPointer, error)
+	// Cursor returns the order.Order at the current cursor location. Returns
+	// an error if the cursor is out of range.
+	Cursor() (order.Order, error)
 }
+
+// OrderFragmentIterator is used to iterate over an order.Fragment collection.
+type OrderFragmentIterator interface {
+
+	// Next progresses the cursor. Returns true if the new cursor is still in
+	// the range of the order.Fragment collection, otherwise false.
+	Next() bool
+
+	// Cursor returns the order.Fragment at the current cursor location.
+	// Returns an error if the cursor is out of range.
+	Cursor() (order.Fragment, error)
+}
+
+// Pointer points to the last order.Order that was successfully synchronised.
+type Pointer int
