972f59ccfaa8a7bf2f3f6ac550e6187082afb36c
Divya <u6479594@anu.edu.au>
2018-06-20 15:12:05 +1000

    contract: stubs for tests

---
+++ contract/binder_test.go
@@ -0,0 +1,22 @@
+package contract_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/republicprotocol/republic-go/contract"
+
+	"github.com/republicprotocol/republic-go/testutils"
+)
+
+var _ = Describe("Contract Binder", func() {
+
+	testutils.GanacheBeforeSuite(func() {
+		
+	})
+
+	Context("when ", func() {
+
+		It("should ", func() {
+		})
+	})
+})
