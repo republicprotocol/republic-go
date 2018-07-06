b27103ed59e276a74a4f3abe7ee4799b0dd9dc06
Loong <loong@republicprotocol.com>
2018-07-06 12:34:38 +1000

    cmd/sendOrder, contract/bindings: support for simultaneous epochs

--- cmd/sendOrder/sendOrder.go
+++ cmd/sendOrder/sendOrder.go
@@ -19,6 +19,7 @@ import (
 	"github.com/republicprotocol/republic-go/http"
 	"github.com/republicprotocol/republic-go/http/adapter"
 	"github.com/republicprotocol/republic-go/order"
+	"github.com/republicprotocol/republic-go/registry"
 )
 
 func main() {
@@ -58,65 +59,28 @@ func main() {
 		message := append([]byte("Republic Protocol: open: "), ord.ID[:]...)
 		signatureData := crypto.Keccak256([]byte(fmt.Sprintf("\x19Ethereum Signed Message:\n%d", len(message))), message)
 		signature, err := keystore.Sign(signatureData)
-
-		log.Printf("order = %v, from = %v", ord.ID, contractBinder.From().String())
-
 		if err != nil {
 			log.Fatalf("cannot sign order: %v", err)
 		}
 
+		log.Printf("order = %v, from = %v", ord.ID, contractBinder.From().String())
 		request := http.OpenOrderRequest{
-			Signature:            base64.StdEncoding.EncodeToString(signature),
-			OrderFragmentMapping: map[string][]adapter.OrderFragment{},
+			Signature:             base64.StdEncoding.EncodeToString(signature),
+			OrderFragmentMappings: [](map[string][]adapter.OrderFragment){},
 		}
 
+		// Current epoch
 		pods, err := contractBinder.Pods()
 		if err != nil {
 			log.Fatalf("cannot get pods from darkpool: %v", err)
 		}
-		for _, pod := range pods {
-			n := int64(len(pod.Darknodes))
-			k := int64(2 * (len(pod.Darknodes) + 1) / 3)
-			hash := base64.StdEncoding.EncodeToString(pod.Hash[:])
-			ordFragments, err := ord.Split(n, k)
-			if err != nil {
-				log.Fatalf("cannot split order to %v: %v", hash, err)
-			}
-			request.OrderFragmentMapping[hash] = []adapter.OrderFragment{}
-			for i, ordFragment := range ordFragments {
-				marshaledOrdFragment := adapter.OrderFragment{
-					Index: int64(i + 1),
-				}
-
-				pubKey, err := contractBinder.PublicKey(pod.Darknodes[i])
-				if err != nil {
-					log.Fatalf("cannot get public key of %v: %v", pod.Darknodes[i], err)
-				}
-
-				encryptedFragment, err := ordFragment.Encrypt(pubKey)
-				marshaledOrdFragment.ID = base64.StdEncoding.EncodeToString(encryptedFragment.ID[:])
-				marshaledOrdFragment.OrderID = base64.StdEncoding.EncodeToString(encryptedFragment.OrderID[:])
-				marshaledOrdFragment.OrderParity = encryptedFragment.OrderParity
-				marshaledOrdFragment.OrderSettlement = encryptedFragment.OrderSettlement
-				marshaledOrdFragment.OrderType = encryptedFragment.OrderType
-				marshaledOrdFragment.OrderExpiry = encryptedFragment.OrderExpiry.Unix()
-				marshaledOrdFragment.Tokens = base64.StdEncoding.EncodeToString(encryptedFragment.Tokens)
-				marshaledOrdFragment.Price = []string{
-					base64.StdEncoding.EncodeToString(encryptedFragment.Price.Co),
-					base64.StdEncoding.EncodeToString(encryptedFragment.Price.Exp),
-				}
-				marshaledOrdFragment.Volume = []string{
-					base64.StdEncoding.EncodeToString(encryptedFragment.Volume.Co),
-					base64.StdEncoding.EncodeToString(encryptedFragment.Volume.Exp),
-				}
-				marshaledOrdFragment.MinimumVolume = []string{
-					base64.StdEncoding.EncodeToString(encryptedFragment.MinimumVolume.Co),
-					base64.StdEncoding.EncodeToString(encryptedFragment.MinimumVolume.Exp),
-				}
-				marshaledOrdFragment.Nonce = base64.StdEncoding.EncodeToString(encryptedFragment.Nonce)
-				request.OrderFragmentMapping[hash] = append(request.OrderFragmentMapping[hash], marshaledOrdFragment)
-			}
+		request.OrderFragmentMappings = append(request.OrderFragmentMappings, generateOrderFragmentMapping(ord, pods))
+		// Previous epoch
+		pods, err = contractBinder.PreviousPods()
+		if err != nil {
+			log.Fatalf("cannot get previous pods from darkpool: %v", err)
 		}
+		request.OrderFragmentMappings = append(request.OrderFragmentMappings, generateOrderFragmentMapping(ord, pods))
 
 		data, err := json.MarshalIndent(request, "", "  ")
 		if err != nil {
@@ -189,4 +153,54 @@ func loadContractBinder(config contract.Config, keystore crypto.Keystore) (contr
 	auth := bind.NewKeyedTransactor(keystore.EcdsaKey.PrivateKey)
 
 	return contract.NewBinder(auth, conn)
+}
+
+func generateOrderFragmentMapping(ord order.Order, pods []registry.Pod) map[string]([]adapter.OrderFragment) {
+	orderFragmentMapping := map[string]([]adapter.OrderFragment){}
+
+	for _, pod := range pods {
+		n := int64(len(pod.Darknodes))
+		k := int64(2 * (len(pod.Darknodes) + 1) / 3)
+		hash := base64.StdEncoding.EncodeToString(pod.Hash[:])
+		ordFragments, err := ord.Split(n, k)
+		if err != nil {
+			log.Fatalf("cannot split order to %v: %v", hash, err)
+		}
+		orderFragmentMapping[hash] = []adapter.OrderFragment{}
+		for i, ordFragment := range ordFragments {
+			marshaledOrdFragment := adapter.OrderFragment{
+				Index: int64(i + 1),
+			}
+
+			pubKey, err := contractBinder.PublicKey(pod.Darknodes[i])
+			if err != nil {
+				log.Fatalf("cannot get public key of %v: %v", pod.Darknodes[i], err)
+			}
+
+			encryptedFragment, err := ordFragment.Encrypt(pubKey)
+			marshaledOrdFragment.ID = base64.StdEncoding.EncodeToString(encryptedFragment.ID[:])
+			marshaledOrdFragment.OrderID = base64.StdEncoding.EncodeToString(encryptedFragment.OrderID[:])
+			marshaledOrdFragment.OrderParity = encryptedFragment.OrderParity
+			marshaledOrdFragment.OrderSettlement = encryptedFragment.OrderSettlement
+			marshaledOrdFragment.OrderType = encryptedFragment.OrderType
+			marshaledOrdFragment.OrderExpiry = encryptedFragment.OrderExpiry.Unix()
+			marshaledOrdFragment.Tokens = base64.StdEncoding.EncodeToString(encryptedFragment.Tokens)
+			marshaledOrdFragment.Price = []string{
+				base64.StdEncoding.EncodeToString(encryptedFragment.Price.Co),
+				base64.StdEncoding.EncodeToString(encryptedFragment.Price.Exp),
+			}
+			marshaledOrdFragment.Volume = []string{
+				base64.StdEncoding.EncodeToString(encryptedFragment.Volume.Co),
+				base64.StdEncoding.EncodeToString(encryptedFragment.Volume.Exp),
+			}
+			marshaledOrdFragment.MinimumVolume = []string{
+				base64.StdEncoding.EncodeToString(encryptedFragment.MinimumVolume.Co),
+				base64.StdEncoding.EncodeToString(encryptedFragment.MinimumVolume.Exp),
+			}
+			marshaledOrdFragment.Nonce = base64.StdEncoding.EncodeToString(encryptedFragment.Nonce)
+			orderFragmentMapping[hash] = append(orderFragmentMapping[hash], marshaledOrdFragment)
+		}
+	}
+
+	return orderFragmentMapping
 }
