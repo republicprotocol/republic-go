4984d69cc758756f015f6873d5b49d177750b8ce
Vince Au <vinceau09@gmail.com>
2018-07-10 13:14:05 +1000

    leveldb: test pointer cloning

--- leveldb/orderbook_test.go
+++ leveldb/orderbook_test.go
@@ -124,6 +124,56 @@ var _ = Describe("Orderbook storage", func() {
 			})
 		})
 
+		Context("when cloning pointer tables", func() {
+			It("should have the same pointer as the original", func() {
+				orderbookPointerTable := NewOrderbookPointerTable(expiry)
+				err := orderbookPointerTable.PutPointer(42)
+				Expect(err).ShouldNot(HaveOccurred())
+
+				clonedTable, err := orderbookPointerTable.Clone()
+				Expect(err).ShouldNot(HaveOccurred())
+				op, err := orderbookPointerTable.Pointer()
+				Expect(err).ShouldNot(HaveOccurred())
+				cp, err := clonedTable.Pointer()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(op).Should(Equal(cp))
+			})
+
+			It("should not propagate changes from the original to the clone", func() {
+				orderbookPointerTable := NewOrderbookPointerTable(expiry)
+				err := orderbookPointerTable.PutPointer(42)
+				Expect(err).ShouldNot(HaveOccurred())
+				clonedTable, err := orderbookPointerTable.Clone()
+				Expect(err).ShouldNot(HaveOccurred())
+
+				err = orderbookPointerTable.PutPointer(10)
+				Expect(err).ShouldNot(HaveOccurred())
+				op, err := orderbookPointerTable.Pointer()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(op).Should(BeEquivalentTo(10))
+				cp, err := clonedTable.Pointer()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(cp).Should(BeEquivalentTo(42))
+			})
+
+			It("should not propagate changes from the clone to the original", func() {
+				orderbookPointerTable := NewOrderbookPointerTable(expiry)
+				err := orderbookPointerTable.PutPointer(42)
+				Expect(err).ShouldNot(HaveOccurred())
+				clonedTable, err := orderbookPointerTable.Clone()
+				Expect(err).ShouldNot(HaveOccurred())
+
+				err = clonedTable.PutPointer(10)
+				Expect(err).ShouldNot(HaveOccurred())
+				cp, err := clonedTable.Pointer()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(cp).Should(BeEquivalentTo(10))
+				op, err := orderbookPointerTable.Pointer()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(op).Should(BeEquivalentTo(42))
+			})
+		})
+
 	})
 
 })
