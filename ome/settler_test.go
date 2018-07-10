39c5177f78fd08006fa49f4b5284993141676446
Divya Mary <divyakoshy05@gmail.com>
2018-07-10 14:49:57 +1000

    ome: fixed settler_test

--- ome/settler_test.go
+++ ome/settler_test.go
@@ -3,12 +3,13 @@ package ome_test
 import (
 	"fmt"
 	"os"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/republicprotocol/republic-go/leveldb"
 	. "github.com/republicprotocol/republic-go/ome"
 
+	"github.com/republicprotocol/republic-go/leveldb"
 	"github.com/republicprotocol/republic-go/testutils"
 )
 
@@ -16,7 +17,7 @@ const NumberOfNodes = 24
 
 var _ = Describe("Settler", func() {
 	var (
-		storers   [NumberOfNodes]*leveldb.Store
+		storers   [NumberOfNodes]ComputationStorer
 		smpcers   [NumberOfNodes]*testutils.Smpc
 		contracts [NumberOfNodes]*omeBinder
 		settles   [NumberOfNodes]Settler
@@ -24,9 +25,9 @@ var _ = Describe("Settler", func() {
 
 	BeforeEach(func() {
 		for i := 0; i < NumberOfNodes; i++ {
-			storer, err := leveldb.NewStore(fmt.Sprintf("./data-%v.out", i))
+			storer, err := leveldb.NewStore(fmt.Sprintf("./data-%v.out", i), 72*time.Hour)
 			Ω(err).ShouldNot(HaveOccurred())
-			storers[i] = storer
+			storers[i] = storer.SomerComputationStore()
 			smpcers[i] = testutils.NewAlwaysMatchSmpc()
 			contracts[i] = newOmeBinder()
 			settles[i] = NewSettler(storers[i], smpcers[i], contracts[i])
@@ -41,15 +42,13 @@ var _ = Describe("Settler", func() {
 
 	Context("when a computation has been resolved to a match and been confirmed ", func() {
 		It("should be able to reconstruct the order and settle it.", func() {
-			buy, sell := testutils.RandomBuyOrder(), testutils.RandomSellOrder()
-			comp := NewComputation(buy.ID, sell.ID, [32]byte{})
-			buyShares, err := buy.Split(int64(NumberOfNodes), int64(2*(NumberOfNodes+1)/3))
-			Ω(err).ShouldNot(HaveOccurred())
-			sellShares, err := sell.Split(int64(NumberOfNodes), int64(2*(NumberOfNodes+1)/3))
-			Ω(err).ShouldNot(HaveOccurred())
+			buyFragments, err := testutils.RandomBuyOrderFragments(int64(NumberOfNodes), int64(2*(NumberOfNodes+1)/3))
+			Expect(err).ShouldNot(HaveOccurred())
+			sellFragments, err := testutils.RandomSellOrderFragments(int64(NumberOfNodes), int64(2*(NumberOfNodes+1)/3))
+			Expect(err).ShouldNot(HaveOccurred())
+			comp := NewComputation([32]byte{}, buyFragments[0], sellFragments[0], ComputationStateNil, true)
 			for i := 0; i < NumberOfNodes; i++ {
-				Ω(storers[i].PutOrderFragment(buyShares[i])).ShouldNot(HaveOccurred())
-				Ω(storers[i].PutOrderFragment(sellShares[i])).ShouldNot(HaveOccurred())
+				Ω(storers[i].PutComputation(comp)).ShouldNot(HaveOccurred())
 			}
 
 			for i := 0; i < NumberOfNodes; i++ {
