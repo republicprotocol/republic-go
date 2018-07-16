45507195edae4d0fcd8b9e9809b51f927e0db634
Divya Mary <divyakoshy05@gmail.com>
2018-07-16 12:50:51 +1000

    swarm: added MultiAddressStorer interface in swarmer

---
+++ swarm/store.go
@@ -0,0 +1,37 @@
+package swarm
+
+import (
+	"errors"
+
+	"github.com/republicprotocol/republic-go/identity"
+)
+
+// ErrCursorOutOfRange is returned when an iterator cursor is used to read a
+// value outside the range of the iterator.
+var ErrCursorOutOfRange = errors.New("cursor out of range")
+
+// MultiAddressStorer for the identity.MultiAddresses that are registered with
+// the dark node registry.
+type MultiAddressStorer interface {
+	PutMultiAddress(address identity.Address, multiaddress identity.MultiAddress, nonce uint64) (bool, error)
+	MultiAddress(address identity.Address) (identity.MultiAddress, error)
+	MultiAddresses() (MultiAddressIterator, error)
+}
+
+// MultiAddressIterator is used to iterate over an identity.MultiAddress collection.
+type MultiAddressIterator interface {
+
+	// Next progresses the cursor. Returns true if the new cursor is still in
+	// the range of the identity.MultiAddress collection, otherwise false.
+	Next() bool
+
+	// Cursor returns the identity.MultiAddress at the current cursor location.
+	// Returns an error if the cursor is out of range.
+	Cursor() (identity.MultiAddress, uint64, error)
+
+	// Collect all identity.MultiAddresses in the iterator into slices.
+	Collect() ([]identity.MultiAddress, []uint64, error)
+
+	// Release the resources allocated by the iterator.
+	Release()
+}
