0ff46fe20627d9d42ecdb1a3bfe8446d52856c40
Loong <loong@republicprotocol.com>
2018-07-23 12:01:23 +1000

    smpc: check context closure in handleStream

--- smpc/smpc.go
+++ smpc/smpc.go
@@ -207,10 +207,16 @@ func (smpc *smpcer) query(addr identity.Address) (identity.MultiAddress, error) 
 
 func (smpc *smpcer) handleStream(ctx context.Context, remoteAddr identity.Address, remoteStream stream.Stream) {
 	for {
+		// Check whether or not the stream has closed
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
 		message := Message{}
 		if err := remoteStream.Recv(&message); err != nil {
-			logger.Network(logger.LevelDebug, fmt.Sprintf("closing stream with %v: %v", remoteAddr, err))
-
+			log.Printf("[error] (network) cannot receive message from %v: %v", remoteAddr, err)
 			time.Sleep(time.Second)
 			continue
 		}
