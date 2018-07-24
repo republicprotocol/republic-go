a5a699ce64fef02b4ef798b4eeff81b1d40a8133
Divya Mary <divyakoshy05@gmail.com>
2018-07-24 15:59:33 +1000

    smpc: reduced rate limit for tests

--- smpc/smpc_test.go
+++ smpc/smpc_test.go
@@ -187,7 +187,7 @@ func generateMocknodes(n, α int) ([]*mockNode, []identity.Address, []swarm.Mult
 		if err != nil {
 			return nil, nil, nil, err
 		}
-		swarmService := grpc.NewSwarmService(swarm.NewServer(swarmer, stores[i], α), time.Second)
+		swarmService := grpc.NewSwarmService(swarm.NewServer(swarmer, stores[i], α), time.Microsecond)
 
 		streamer := grpc.NewStreamer(testutils.NewCrypter(), testutils.NewCrypter(), addr)
 		streamerService := grpc.NewStreamerService(testutils.NewCrypter(), testutils.NewCrypter(), streamer)
