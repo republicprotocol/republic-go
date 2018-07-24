08a921c3f434eb9b8790181a616ea8604ff3c754
Loong <loong@republicprotocol.com>
2018-07-24 16:05:26 +1000

    grpc: stripped per message logs

--- grpc/stream.go
+++ grpc/stream.go
@@ -65,9 +65,6 @@ func newConcurrentStream(secret [16]byte, grpcStream grpc.Stream) *concurrentStr
 
 // Send implements the stream.Stream interface.
 func (concurrentStream *concurrentStream) Send(message stream.Message) error {
-	log.Printf("[debug] sending message")
-	defer log.Printf("[debug] sent message")
-
 	concurrentStream.grpcSendMu.Lock()
 	defer concurrentStream.grpcSendMu.Unlock()
 
