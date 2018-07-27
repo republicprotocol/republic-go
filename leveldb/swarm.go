e0bad4a805dea3564e36316c99e1edc0e86b505d
Divya Mary <divyakoshy05@gmail.com>
2018-07-27 14:10:41 +1000

    swarm: logs

--- leveldb/swarm.go
+++ leveldb/swarm.go
@@ -96,7 +96,7 @@ func NewSwarmMultiAddressTable(db *leveldb.DB, expiry time.Duration) *SwarmMulti
 // PutMultiAddress implements the swarm.MultiAddressStorer interface.
 func (table *SwarmMultiAddressTable) PutMultiAddress(multiAddress identity.MultiAddress, nonce uint64) (bool, error) {
 	isNew := false
-	// log.Printf("storing %v", multiAddress.Address())
+	log.Printf("storing........ %v", multiAddress)
 	value := SwarmMultiAddressValue{
 		Nonce:        nonce,
 		MultiAddress: multiAddress,
