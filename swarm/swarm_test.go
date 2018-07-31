5e49f48d73e6f5f239f964feb81322c370967676
Divya Mary <divyakoshy05@gmail.com>
2018-08-01 09:39:03 +1000

    swarm: make bootstrap nodes honest

--- swarm/swarm_test.go
+++ swarm/swarm_test.go
@@ -43,7 +43,7 @@ var _ = Describe("Swarm", func() {
 				return nil, nil, serverHub, err
 			}
 			clientType := testutils.Honest
-			if !honest {
+			if !honest && i > numberOfBootstrapClients {
 				clientType = testutils.ClientTypes[rand.Intn(len(testutils.ClientTypes))]
 			}
 			client, store, err := testutils.NewMockSwarmClient(serverHub, &key, clientType)
