15fe2978e8cedfab3fa0686f3237f2eb9877fa21
Divya Mary <divyakoshy05@gmail.com>
2018-08-09 16:22:53 +1000

    smpc: testing higher delay

--- smpc/network.go
+++ smpc/network.go
@@ -196,7 +196,7 @@ func (network *network) SendWithDelay(networkID NetworkID, message Message) {
 
 		// Delay this goroutine based on the position of the address
 		pos := positions[addr] + message.Rotation(uint64(len(positions)))%uint64(len(positions))
-		time.Sleep(4 * time.Second * time.Duration(pos))
+		time.Sleep(10 * time.Second * time.Duration(pos))
 
 		sender := senders[addr]
 		if err := sender.Send(message); err != nil {
