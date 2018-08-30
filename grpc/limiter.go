c53932d7bab8185b483aa145acefd9f900536a30
tok-kkk <yunshi@republicprotocol.com>
2018-08-31 09:42:44 +1000

    grpc: add comments for the limiter and its methods

--- grpc/limiter.go
+++ grpc/limiter.go
@@ -8,8 +8,8 @@ import (
 )
 
 // RateLimiter wraps the time/rate.Limiter. It first limits the request
-// according to the ip address.Then do a global limiting on all the
-// request received.
+// according to the ip address.Then do a global limiting on the total
+// number of requests received.
 type RateLimiter struct {
 	mu     *sync.Mutex
 	limit  rate.Limit
@@ -29,6 +29,9 @@ func NewRateLimiter(limiter *rate.Limiter, limit float64, burst int) *RateLimite
 	}
 }
 
+// Allow reports whether the request from given address may happen at time now.
+// Use this method if you intend to drop / skip events that exceed the rate
+// limit. Otherwise use Reserve or Wait.
 func (limiter *RateLimiter) Allow(addr string) bool {
 	limiter.mu.Lock()
 	if _, ok := limiter.local[addr]; !ok {
@@ -44,6 +47,9 @@ func (limiter *RateLimiter) Allow(addr string) bool {
 	return limiter.global.Allow()
 }
 
+// Wait blocks until the limiter permits the request to happen. It returns an
+// error if it exceeds the Limiter's burst size, the Context is canceled, or
+// the expected wait time exceeds the Context's Deadline.
 func (limiter *RateLimiter) Wait(ctx context.Context, addr string) error {
 	limiter.mu.Lock()
 	if _, ok := limiter.local[addr]; !ok {
@@ -59,6 +65,9 @@ func (limiter *RateLimiter) Wait(ctx context.Context, addr string) error {
 	return limiter.global.Wait(ctx)
 }
 
+// Reserve returns a Reservation that indicates how long the caller must wait
+// before the request happens. The Limiter takes this Reservation into account
+// when allowing future events.
 func (limiter *RateLimiter) Reserve(addr string) *rate.Reservation {
 	limiter.mu.Lock()
 	if _, ok := limiter.local[addr]; !ok {
@@ -74,6 +83,7 @@ func (limiter *RateLimiter) Reserve(addr string) *rate.Reservation {
 	return limiter.global.Reserve()
 }
 
+// SetLimit sets a new limit for the limiter.
 func (limiter *RateLimiter) SetLimit(limit float64) {
 	limiter.mu.Lock()
 	defer limiter.mu.Unlock()
@@ -81,6 +91,7 @@ func (limiter *RateLimiter) SetLimit(limit float64) {
 	limiter.limit = rate.Limit(limit)
 }
 
+// SetBurst sets a burst size for the limiter.
 func (limiter *RateLimiter) SetBurst(burst int) {
 	limiter.mu.Lock()
 	defer limiter.mu.Unlock()
