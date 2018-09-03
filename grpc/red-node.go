871118ab3db7737bc5a8dbe5c4069dbe4d3cb9c1
Divya Mary <divyakoshy05@gmail.com>
2018-09-03 13:08:04 +1000

    grpc: moved rednode specific implementation to red-node.go

---
+++ grpc/red-node.go
@@ -0,0 +1,115 @@
+package grpc
+
+import (
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/republicprotocol/republic-go/identity"
+	"github.com/republicprotocol/republic-go/testutils"
+)
+
+// RedNodeBehaviour indicates the malicious behaviours the
+// red node will exhibit.
+type RedNodeBehaviour int
+
+// Values for a RedNodeBehaviour
+const (
+	InvalidRequests RedNodeBehaviour = iota
+	InvalidNonce
+	InvalidSignature
+	DropMultiAddresses
+	DropSignatures
+)
+
+// String returns a human-readable representation of RedNodeTypes.
+func (behaviours RedNodeBehaviour) String() string {
+	switch behaviours {
+	case InvalidRequests:
+		return "invalid requests"
+	case InvalidNonce:
+		return "invalid nonce"
+	case InvalidSignature:
+		return "invalid multi-address signature"
+	case DropMultiAddresses:
+		return "drop multi-addresses"
+	case DropSignatures:
+		return "drop multi-address signatures"
+	default:
+		return "unexpected behaviour"
+	}
+}
+
+// RedNodeSwarmerTypes contains an array of all possible red-node swarming
+// behaviours.
+var RedNodeSwarmerTypes = []RedNodeBehaviour{
+	InvalidRequests,
+	InvalidNonce,
+	InvalidSignature,
+	DropMultiAddresses,
+	DropSignatures,
+}
+
+func getTamperedMultiAddress(multiAddr identity.MultiAddress) MultiAddress {
+	redNodeType := RedNodeSwarmerTypes[rand.Intn(len(RedNodeSwarmerTypes))]
+
+	rand.Seed(time.Now().UnixNano())
+	multiAddress := MultiAddress{
+		Signature:         multiAddr.Signature,
+		MultiAddress:      multiAddr.String(),
+		MultiAddressNonce: multiAddr.Nonce,
+	}
+
+	switch redNodeType {
+	case InvalidRequests:
+		multiAddress.Signature = tamperSignature(multiAddr)
+		multiAddress.MultiAddressNonce = tamperNonce(multiAddr)
+		multiAddress.MultiAddress = tamperMultiAddress(multiAddr)
+	case InvalidNonce:
+		multiAddress.MultiAddressNonce = tamperNonce(multiAddr)
+	case InvalidSignature:
+		multiAddress.Signature = tamperSignature(multiAddr)
+	case DropMultiAddresses:
+		multiAddress.MultiAddress = ""
+	case DropSignatures:
+		multiAddress.Signature = []byte{}
+	default:
+	}
+
+	log.Printf("Red-node swarmer will exhibit behaviour: %v\n", redNodeType)
+	log.Printf("Red-node tampered multi-address %v to look like %v", multiAddr, multiAddress)
+	return multiAddress
+}
+
+func tamperSignature(multiAddr identity.MultiAddress) []byte {
+	r := rand.Intn(100)
+	if r < 50 {
+		randBytes := testutils.Random64Bytes()
+		return randBytes[:]
+	}
+	multiAddr.Signature[rand.Intn(64)] = byte(rand.Intn(100))
+	return multiAddr.Signature
+}
+
+func tamperMultiAddress(multiAddr identity.MultiAddress) string {
+	r := rand.Intn(100)
+	if r < 75 {
+		multiAddr, _ := testutils.RandomMultiAddress()
+		return multiAddr.String()
+	}
+	return multiAddr.String()
+}
+
+func tamperNonce(multiAddr identity.MultiAddress) uint64 {
+	r := rand.Intn(100)
+	if r < 33 {
+		return multiAddr.Nonce + uint64(r)
+	}
+	if r < 66 {
+		return multiAddr.Nonce - uint64(r)
+	}
+	if r < 99 {
+		return 0
+	}
+	return multiAddr.Nonce
+}
--- grpc/swarm.go
+++ grpc/swarm.go
@@ -3,7 +3,6 @@ package grpc
 import (
 	"errors"
 	"fmt"
-	"log"
 	"math/rand"
 	"net"
 	"sync"
@@ -12,50 +11,9 @@ import (
 	"github.com/republicprotocol/republic-go/identity"
 	"github.com/republicprotocol/republic-go/logger"
 	"github.com/republicprotocol/republic-go/swarm"
-	"github.com/republicprotocol/republic-go/testutils"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/peer"
 )
-
-// RedNodeBehaviour indicates the malicious behaviours the
-// red node will exhibit during swarming.
-type RedNodeBehaviour int
-
-// Values for a RedNodeBehaviour
-const (
-	InvalidRequests RedNodeBehaviour = iota
-	InvalidNonce
-	InvalidSignature
-	DropMultiAddresses
-	DropSignatures
-)
-
-// RedNodeTypes contains an array of all possible red-node behaviours.
-var RedNodeTypes = []RedNodeBehaviour{
-	InvalidRequests,
-	InvalidNonce,
-	InvalidSignature,
-	DropMultiAddresses,
-	DropSignatures,
-}
-
-// String returns a human-readable representation of RedNodeTypes.
-func (behaviours RedNodeBehaviour) String() string {
-	switch behaviours {
-	case InvalidRequests:
-		return "invalid requests"
-	case InvalidNonce:
-		return "invalid nonce"
-	case InvalidSignature:
-		return "invalid multi-address signature"
-	case DropMultiAddresses:
-		return "drop multi-addresses"
-	case DropSignatures:
-		return "drop multi-address signatures"
-	default:
-		return "unexpected behaviour"
-	}
-}
 
 // ErrRateLimitExceeded is returned when the same client sends more than one
 // request to the server within a specified rate limit.
@@ -207,7 +165,8 @@ func (service *SwarmService) Ping(ctx context.Context, request *PingRequest) (*P
 	}
 
 	// Atleast 75% of the time, the red node will not attempt to do anything
-	// with the ping request.
+	// with the ping request. The rest of the time, it will act tamper with
+	// the multi-address at the client side and send that to the other nodes.
 	if rand.Intn(100) < 75 {
 		return &PingResponse{}, nil
 	}
@@ -240,7 +199,8 @@ func (service *SwarmService) Pong(ctx context.Context, request *PongRequest) (*P
 	}
 
 	// Atleast 50% of the time, the red node will not attempt to do anything
-	// with the pong request.
+	// with the pong request. The rest of the time, the client will tamper with
+	// it's own multi-address and send that back to the requester.
 	if rand.Intn(100) < 50 {
 		return &PongResponse{}, nil
 	}
@@ -273,7 +233,8 @@ func (service *SwarmService) Query(ctx context.Context, request *QueryRequest) (
 	}
 
 	// Atleast 25% of the time, the red node will not attempt to do anything
-	// with the query request.
+	// with the query request. The rest of the time, it will tamper with the
+	// multi-addresses that were returned and sends this data as the response.
 	if rand.Intn(100) < 25 {
 		return &QueryResponse{}, nil
 	}
@@ -320,68 +281,4 @@ func (service *SwarmService) isRateLimited(ctx context.Context) error {
 	}
 	service.rateLimits[clientIP] = time.Now()
 	return nil
-}
-
-func getTamperedMultiAddress(multiAddr identity.MultiAddress) MultiAddress {
-	redNodeType := RedNodeTypes[rand.Intn(len(RedNodeTypes))]
-
-	rand.Seed(time.Now().UnixNano())
-	multiAddress := MultiAddress{
-		Signature:         multiAddr.Signature,
-		MultiAddress:      multiAddr.String(),
-		MultiAddressNonce: multiAddr.Nonce,
-	}
-
-	switch redNodeType {
-	case InvalidRequests:
-		multiAddress.Signature = tamperSignature(multiAddr)
-		multiAddress.MultiAddressNonce = tamperNonce(multiAddr)
-		multiAddress.MultiAddress = tamperMultiAddress(multiAddr)
-	case InvalidNonce:
-		multiAddress.MultiAddressNonce = tamperNonce(multiAddr)
-	case InvalidSignature:
-		multiAddress.Signature = tamperSignature(multiAddr)
-	case DropMultiAddresses:
-		multiAddress.MultiAddress = ""
-	case DropSignatures:
-		multiAddress.Signature = []byte{}
-	default:
-	}
-
-	log.Printf("Red-node swarmer will exhibit behaviour: %v\n", redNodeType)
-	log.Printf("Red-node tampered multi-address %v to look like %v", multiAddr, multiAddress)
-	return multiAddress
-}
-
-func tamperSignature(multiAddr identity.MultiAddress) []byte {
-	r := rand.Intn(100)
-	if r < 50 {
-		randBytes := testutils.Random64Bytes()
-		return randBytes[:]
-	}
-	multiAddr.Signature[rand.Intn(64)] = byte(rand.Intn(100))
-	return multiAddr.Signature
-}
-
-func tamperMultiAddress(multiAddr identity.MultiAddress) string {
-	r := rand.Intn(100)
-	if r < 75 {
-		multiAddr, _ := testutils.RandomMultiAddress()
-		return multiAddr.String()
-	}
-	return multiAddr.String()
-}
-
-func tamperNonce(multiAddr identity.MultiAddress) uint64 {
-	r := rand.Intn(100)
-	if r < 33 {
-		return multiAddr.Nonce + uint64(r)
-	}
-	if r < 66 {
-		return multiAddr.Nonce - uint64(r)
-	}
-	if r < 99 {
-		return 0
-	}
-	return multiAddr.Nonce
 }
