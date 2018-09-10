b59ba9c8735aa272b6aeba14bbfaa8735a84d154
tok-kkk <yunshi@republicprotocol.com>
2018-09-11 09:37:02 +1000

    ome: fix the log to print the correct volume

--- ome/settler.go
+++ ome/settler.go
@@ -128,7 +128,7 @@ func (settler *settler) settleOrderMatch(com Computation, buy, sell order.Order)
 	// submitting such orders. Note: minimum volume is set to 1 ETH.
 	settleVolume := volumeInEth(buy, sell)
 	if settleVolume < settler.minimumSettleVolume {
-		log.Printf("[info] (settle) cannot execute settlement buy = %v, sell = %v: volume = %f ETH too low", buy.ID, sell.ID, settleVolume)
+		log.Printf("[info] (settle) cannot execute settlement buy = %v, sell = %v: volume = %v ETH too low", buy.ID, sell.ID, settleVolume)
 		return
 	}
 
